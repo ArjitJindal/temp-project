d0d47e5ef8bf1ed38be6d4d5625822672c396242
Nadig <madhug.nadig@gmail.com>
2021-10-18 06:22:51 +0200

    chore: Minor refactor active rules

--- src/pages/rules/active-rules/data.d.ts
+++ src/pages/rules/active-rules/data.d.ts
@@ -1,32 +1,49 @@
-export type AdvancedOperation1 = {
-  key: string;
-  type: string;
+export const valueEnum = {
+  0: 'close',
+  1: 'running',
+  2: 'online',
+  3: 'error',
+};
+
+export const ProcessMap = {
+  close: 'normal',
+  running: 'active',
+  online: 'success',
+  error: 'exception',
+};
+
+export type TableListItem = {
+  key: number;
   name: string;
+  progress: number;
+  containers: number;
+  callNumber: number;
+  creator: string;
   status: string;
-  updatedAt: string;
+  createdAt: number;
   memo: string;
 };
 
-export type AdvancedOperation2 = {
-  key: string;
-  type: string;
-  name: string;
-  status: string;
-  updatedAt: string;
-  memo: string;
-};
+export const createTableList = () => {
+  const tableListDataSource: TableListItem[] = [];
 
-export type AdvancedOperation3 = {
-  key: string;
-  type: string;
-  name: string;
-  status: string;
-  updatedAt: string;
-  memo: string;
-};
+  const creators = ['付小小', '曲丽丽', '林东东', '陈帅帅', '兼某某'];
 
-export interface AdvancedProfileData {
-  advancedOperation1?: AdvancedOperation1[];
-  advancedOperation2?: AdvancedOperation2[];
-  advancedOperation3?: AdvancedOperation3[];
-}
+  for (let i = 0; i < 5; i += 1) {
+    tableListDataSource.push({
+      key: i,
+      name: `Rule ${i + 1}`,
+      containers: Math.floor(Math.random() * 20),
+      callNumber: Math.floor(Math.random() * 2000),
+      progress: Math.ceil(Math.random() * 100) + 1,
+      creator: creators[Math.floor(Math.random() * creators.length)],
+      status: valueEnum[Math.floor(Math.random() * 10) % 4],
+      createdAt: Date.now() - Math.floor(Math.random() * 100000),
+      memo:
+        i % 2 === 1
+          ? 'If this rule is hit, flag the user and create SAR report.'
+          : 'Black list the user if this rule is hit',
+    });
+  }
+  return tableListDataSource;
+};
--- src/pages/rules/active-rules/index.tsx
+++ src/pages/rules/active-rules/index.tsx
@@ -1,54 +1,9 @@
 import { Button, DatePicker, Space, Table } from 'antd';
 import type { ProColumns } from '@ant-design/pro-table';
 import ProTable from '@ant-design/pro-table';
+import { createTableList, ProcessMap, TableListItem } from './data.d';
 
 const { RangePicker } = DatePicker;
-
-const valueEnum = {
-  0: 'close',
-  1: 'running',
-  2: 'online',
-  3: 'error',
-};
-
-const ProcessMap = {
-  close: 'normal',
-  running: 'active',
-  online: 'success',
-  error: 'exception',
-};
-
-export type TableListItem = {
-  key: number;
-  name: string;
-  progress: number;
-  containers: number;
-  callNumber: number;
-  creator: string;
-  status: string;
-  createdAt: number;
-  memo: string;
-};
-const tableListDataSource: TableListItem[] = [];
-
-const creators = ['付小小', '曲丽丽', '林东东', '陈帅帅', '兼某某'];
-
-for (let i = 0; i < 5; i += 1) {
-  tableListDataSource.push({
-    key: i,
-    name: `Rule ${i + 1}`,
-    containers: Math.floor(Math.random() * 20),
-    callNumber: Math.floor(Math.random() * 2000),
-    progress: Math.ceil(Math.random() * 100) + 1,
-    creator: creators[Math.floor(Math.random() * creators.length)],
-    status: valueEnum[Math.floor(Math.random() * 10) % 4],
-    createdAt: Date.now() - Math.floor(Math.random() * 100000),
-    memo:
-      i % 2 === 1
-        ? 'If this rule is hit, flag the user and create SAR report.'
-        : 'Black list the user if this rule is hit',
-  });
-}
 
 const columns: ProColumns<TableListItem>[] = [
   {
@@ -158,7 +113,7 @@ export default () => {
           </Space>
         );
       }}
-      dataSource={tableListDataSource}
+      dataSource={createTableList()}
       scroll={{ x: 1300 }}
       options={false}
       search={false}
