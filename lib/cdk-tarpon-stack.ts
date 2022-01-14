0afdaa0b0e497a8cd72d8248cf5badcfac72441a
Chia-Lun Wu <chia@flagright.com>
2022-01-14 10:19:28 +0100

    Migrate SAM template to CDK
    Migrate sam yaml template to CDK in Typescript.
    Remove Open API integration (TODO: FDT-31).
    (issue: https://github.com/aws/aws-cdk/issues/1461)
    
    Build and Test
    1. `npm run build` (in rules-engine-lambda)
    2. `cdk synth`
    3. `sam local invoke "PostRulesEngineFunction" --event events/event.json -t cdk.out/tarpon.template.json`
    
    Deploy
    - `cdk deploy`
    
    Idea: We should come up with an internal CLI tool then we don't
    need to memorize multiple commands - `npm`, `cdk`, `sam`.

---
+++ lib/cdk-tarpon-stack.ts
@@ -0,0 +1,74 @@
+import { AttributeType, Table } from '@aws-cdk/aws-dynamodb'
+import { Code, Function, Runtime, Tracing } from '@aws-cdk/aws-lambda'
+import * as cdk from '@aws-cdk/core'
+import { CfnOutput, Duration } from '@aws-cdk/core'
+import { LambdaIntegration, LambdaRestApi } from '@aws-cdk/aws-apigateway'
+
+export class CdkTarponStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props)
+
+    /**
+     * DynamoDB
+     */
+    const transactionTable = new Table(this, 'Transactions', {
+      tableName: 'Transactions',
+      // tenantID for primary item
+      partitionKey: { name: 'PartitionKeyID', type: AttributeType.STRING },
+      // transactionID for primary item
+      sortKey: { name: 'SortKeyID', type: AttributeType.STRING },
+      readCapacity: 1,
+      writeCapacity: 1,
+    })
+
+    /**
+     * Lambda Functions
+     */
+    const postRulesEngineFunction = new Function(
+      this,
+      'PostRulesEngineFunction',
+      {
+        functionName: 'PostRulesEngineFunction',
+        runtime: Runtime.NODEJS_14_X,
+        handler: 'dist/app.lambdaHandler',
+        code: Code.fromAsset('rules-engine-lambda'),
+        tracing: Tracing.DISABLED,
+        timeout: Duration.seconds(10),
+      }
+    )
+    transactionTable.grantReadWriteData(postRulesEngineFunction)
+
+    /**
+     * API Gateway
+     */
+
+    // TODO: CDK+OpenAPI integration (issue: https://github.com/aws/aws-cdk/issues/1461)
+    const api = new LambdaRestApi(this, 'TarponAPI', {
+      handler: postRulesEngineFunction, // TODO: create default handler,
+      proxy: false,
+    })
+    const transactions = api.root.addResource('transaction')
+    transactions.addMethod(
+      'POST',
+      new LambdaIntegration(postRulesEngineFunction)
+    )
+
+    /**
+     * Outputs
+     */
+    new CfnOutput(
+      this,
+      'API Gateway endpoint URL for Prod stage for Rules Engine function',
+      {
+        value: api.urlForPath('/'),
+      }
+    )
+    new CfnOutput(this, 'Post Rules Engine Function Name', {
+      value: postRulesEngineFunction.functionName,
+    })
+
+    new CfnOutput(this, 'Transaction Table', {
+      value: transactionTable.tableName,
+    })
+  }
+}
