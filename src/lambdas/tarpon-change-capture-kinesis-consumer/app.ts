d38d3a6662c5aaf726f694675b5f0311d5e0162f
Nadig <madhug.nadig@gmail.com>
2022-02-21 11:38:35 +0100

    add comment

--- src/lambdas/tarpon-change-capture-kinesis-consumer/app.ts
+++ src/lambdas/tarpon-change-capture-kinesis-consumer/app.ts
@@ -1,6 +1,7 @@
 import { KinesisStreamEvent } from 'aws-lambda'
 
 export const tarponChangeCaptureHandler = async (event: KinesisStreamEvent) => {
+  // Implementation pending
   console.log('Kinesis Event')
   console.log(event)
 }
