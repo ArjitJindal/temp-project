70c60b7282fa904e77da1d67ede3c39d0c2b4822
Chia-Lun Wu <chia@flagright.com>
2022-02-23 21:32:09 +0100

    chore: Auto-generate OpenAPI SDK
    Run `npm run openapi:fetch` to fetch the latest spec
    Run `npm run openapi:build` to generate the API SDK and models.
    
    Files under `src/apis` are auto-generated by `openapi-generator`.
    
    Also remove unused umi openapi files.

---
+++ src/apis/models/index.ts
@@ -0,0 +1,7 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './ListImportRequest';
+export * from './PresignedUrlResponse';
+export * from './RuleInstance';
+export * from './TransactionImportRequest';
+export * from './TransactionImportResponse';
