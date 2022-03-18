c9a9a75f52e0f03ae9bfeeab13e71b08529441c5
Chia-Lun Wu <chia@flagright.com>
2022-03-18 13:37:26 +0100

    feat: enable connecting to local api
    Add local config for connecting to local api.

---
+++ config/config.local.ts
@@ -0,0 +1,11 @@
+// https://umijs.org/config/
+import { defineConfig } from 'umi';
+
+export default defineConfig({
+  define: {
+    API_BASE_PATH: 'http://localhost:3000',
+    AUTH0_AUDIENCE: 'https://dev.api.flagright.com/',
+    AUTH0_DOMAIN: 'dev-flagright.eu.auth0.com',
+    AUTH0_CLIENT_ID: 'uGGbVNumU7d57NswPLD5UaTwvf17tc7y',
+  },
+});
