cd3d1d30222b4fd84e68c6d96404d2b7d30141a7
Nadig <madhug.nadig@gmail.com>
2022-03-28 09:53:54 +0200

    chore: revert api.ts

--- src/api.ts
+++ src/api.ts
@@ -26,20 +26,20 @@ class AuthorizationMiddleware implements Middleware {
 }
 
 export function useApi(): FlagrightApi {
-  const { getAccessTokenWithPopup } = useAuth0();
+  const { getAccessTokenSilently } = useAuth0();
   const api = useMemo(() => {
     const configParams: ConfigurationParameters = {
       basePath: API_BASE_PATH,
       middleware: [
-        /*new AuthorizationMiddleware(() =>
-          getAccessTokenWithPopup({
+        new AuthorizationMiddleware(() =>
+          getAccessTokenSilently({
             audience: AUTH0_AUDIENCE,
           }),
-        ),*/
+        ),
       ],
     };
     const apiConfig = new Configuration(configParams);
     return new FlagrightApi(apiConfig);
-  }, [getAccessTokenWithPopup]);
+  }, [getAccessTokenSilently]);
   return api;
 }
