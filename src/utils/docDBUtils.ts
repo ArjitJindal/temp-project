3e5360c2c9f969afe50348f8dfc86f4d60148c71
Madhu G Nadig <madhugnadig@Madhus-MacBook-Pro.fritz.box>
2022-04-18 16:57:43 +0200

    Use new atlas connection string

--- src/utils/docDBUtils.ts
+++ src/utils/docDBUtils.ts
@@ -6,11 +6,10 @@ const AWS = require('aws-sdk')
 interface DBCredentials {
   username: string
   password: string
+  host:string
 }
 
 const secretsmanager = new AWS.SecretsManager()
-const DB_HOST = process.env.DB_HOST as string
-const DB_PORT = process.env.DB_PORT as string
 const SM_SECRET_ARN = process.env.SM_SECRET_ARN as string
 
 let cacheClient: MongoClient
@@ -28,11 +27,9 @@ export async function connectToDB() {
   const credentials = await getCredentials()
   const DB_USERNAME = credentials['username']
   const DB_PASSWORD = encodeURIComponent(credentials['password'])
-  const DB_URL = `mongodb://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}`
+  const DB_HOST = credentials['host']
+  const DB_URL = `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}`
   cacheClient = await MongoClient.connect(DB_URL as string, {
-    ssl: true,
-    sslValidate: true,
-    sslCA: `${__dirname}/rds-combined-ca-bundle.pem`,
     retryWrites: false,
     socketTimeoutMS: 5000000,
   })
