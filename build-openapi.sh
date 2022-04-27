da68e60d12573a06f69332358fbd0ce95f08a995
Nikolai Mavrenkov <koluch@koluch.ru>
2022-04-27 21:34:19 +0300

    chore: Add openapi-generator-cli to dev-deps, so we don't have to install it manually. Also, now `openapi-generator-cli` command supposed to be used on macOS too

--- build-openapi.sh
+++ build-openapi.sh
@@ -2,11 +2,7 @@ set -e
 
 rm -rf src/apis
 
-if [ "$(uname)" = "Darwin" ]; then
-    openapi-generator generate -i config/openapi.yaml -g typescript -o src/apis --additional-properties=modelPropertyNaming=original
-elif [ "$(expr substr $(uname -s) 1 5)" = "Linux" ]; then
-    openapi-generator-cli generate -i config/openapi.yaml -g typescript -o src/apis --additional-properties=modelPropertyNaming=original
-fi
+openapi-generator-cli generate -i config/openapi.yaml -g typescript -o src/apis --additional-properties=modelPropertyNaming=original
 
 if [ "$(uname)" = "Darwin" ]; then
     sed -i '' "s/\* as URLParse/URLParse/g" src/apis/http/http.ts
