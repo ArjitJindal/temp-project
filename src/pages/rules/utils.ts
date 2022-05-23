802dbdb094ef66f2099ef18fd75c6c3f4abed957
Chia-Lun Wu <chia@flagright.com>
2022-05-23 16:23:07 +0800

    fix: add SUSPEND rule action (#72)

--- src/pages/rules/utils.ts
+++ src/pages/rules/utils.ts
@@ -2,6 +2,7 @@ import { RuleAction } from '@/apis/models/RuleAction';
 
 export const RULE_ACTION_OPTIONS: { label: string; value: RuleAction }[] = [
   { label: 'Flag', value: 'FLAG' },
+  { label: 'Suspend', value: 'SUSPEND' },
   { label: 'Block', value: 'BLOCK' },
   { label: 'Whitelist', value: 'WHITELIST' },
 ];
