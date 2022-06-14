0e8b0eecee34126b9a77ffe99ae280e54091fdb1
Chia-Lun Wu <chia@flagright.com>
2022-06-14 15:05:31 +0200

    Update SITE_CERTIFICATE_ARN for prod (#116)
    chore: update SITE_CERTIFICATE_ARN for prod
    
    Change from the certificate for eu-1.console.flagright.com to
    the one for console.flagright.com.

--- lib/configs/config-prod.ts
+++ lib/configs/config-prod.ts
@@ -10,5 +10,5 @@ export const config: Config = {
   env: { account: '870721492449', region: 'eu-central-1' },
   SITE_DOMAIN: 'console.flagright.com',
   SITE_CERTIFICATE_ARN:
-    'arn:aws:acm:us-east-1:870721492449:certificate/0c876a57-83af-4c80-965a-c3c6575659ed',
+    'arn:aws:acm:us-east-1:870721492449:certificate/cc8f49e3-2fa0-44bb-bee9-38f393b5b014',
 };
