ce6a05bd5f6c69ae83ef68777618adb09a9e0606
kaukushikmar <65467554+kaukushikmar@users.noreply.github.com>
2022-07-27 01:18:30 +0530

    FDT-10870: Update Footer in product to say Flagright Data Technologies Inc. (#191)

--- src/components/AppWrapper/Footer/index.tsx
+++ src/components/AppWrapper/Footer/index.tsx
@@ -5,10 +5,9 @@ import { useI18n } from '@/locales';
 export default function Footer() {
   const i18l = useI18n();
   const defaultMessage = i18l('app.copyright.produced', {
-    defaultMessage: 'Flagright Data Technologies',
+    defaultMessage: 'Flagright Data Technologies Inc.',
   });
 
   const currentYear = new Date().getFullYear();
-
   return <DefaultFooter copyright={`${currentYear} ${defaultMessage}`} className={style.footer} />;
 }
