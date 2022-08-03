bab72e19f36865b92985b0c3060dc3d7eb7a1b5c
Chia-Lun Wu <chia@flagright.com>
2022-08-03 16:49:29 +0200

    Change CodeBuild project back to MEDIUM compute type (#321)
    The assumption of fdcb646 is wrong. We need more memory.

--- lib/cdk-tarpon-pipeline-stack.ts
+++ lib/cdk-tarpon-pipeline-stack.ts
@@ -5,6 +5,7 @@ import * as iam from 'aws-cdk-lib/aws-iam'
 import * as codebuild from 'aws-cdk-lib/aws-codebuild'
 
 import { Construct } from 'constructs'
+import { ComputeType } from 'aws-cdk-lib/aws-codebuild'
 import { config as deployConfig } from './configs/config-deployment'
 import { config as devConfig } from './configs/config-dev'
 import { config as sandboxConfig } from './configs/config-sandbox'
@@ -60,6 +61,7 @@ export class CdkTarponPipelineStack extends cdk.Stack {
       }),
       environment: {
         buildImage: codebuild.LinuxBuildImage.STANDARD_6_0,
+        computeType: ComputeType.MEDIUM,
       },
     })
     const getDeployCodeBuildProject = (
