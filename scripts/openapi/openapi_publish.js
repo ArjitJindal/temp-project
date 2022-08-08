214740faa1d738a1f28a1899609ac8bf43ff1e8b
Nikolai Mavrenkov <koluch@koluch.ru>
2022-08-08 14:58:02 +0200

    Improvement: protect main branch from pushing to main with special environment variable (#327)
    * Improvement: protect main branch from pushing to main with special environment variable
    
    * Improvement: protect main branch from pushing to main with special environment variable

--- scripts/openapi/openapi_publish.js
+++ scripts/openapi/openapi_publish.js
@@ -60,7 +60,7 @@ async function main() {
 
   await prepareSchemas(OUTPUT_DIR)
 
-  let { BRANCH_NAME, PUBLIC_PROJECT_TOKEN, INTERNAL_PROJECT_TOKEN } = process.env
+  let { BRANCH_NAME, CONFIRM_PUSH_MAIN, PUBLIC_PROJECT_TOKEN, INTERNAL_PROJECT_TOKEN } = process.env
 
   if (!BRANCH_NAME) {
     BRANCH_NAME = exec("git symbolic-ref --short -q HEAD 2>/dev/null | tr / -").toString().trim()
@@ -70,6 +70,13 @@ async function main() {
     logger.error("ERROR: Please set the BRANCH_NAME environment variables");
     process.exit(1)
   }
+
+  logger.log("BRANCH_NAME", BRANCH_NAME)
+  if (BRANCH_NAME === 'main' && CONFIRM_PUSH_MAIN !== 'true') {
+    logger.error("ERROR: To push to main branch, please, also provide CONFIRM_PUSH_MAIN=true environment variable");
+    process.exit(1)
+  }
+
   if (!PUBLIC_PROJECT_TOKEN) {
     logger.error("ERROR: Please set the PUBLIC_PROJECT_TOKEN environment variables");
     process.exit(1)
@@ -78,7 +85,6 @@ async function main() {
     logger.error("ERROR: Please set the INTERNAL_PROJECT_TOKEN environment variables");
     process.exit(1)
   }
-  logger.info("BRANCH_NAME", BRANCH_NAME)
 
   exec(`./node_modules/.bin/stoplight push --ci-token ${PUBLIC_PROJECT_TOKEN} --directory ${OUTPUT_DIR}/public --branch ${BRANCH_NAME}`)
   exec(`./node_modules/.bin/stoplight push --ci-token ${INTERNAL_PROJECT_TOKEN} --directory ${OUTPUT_DIR}/internal --branch ${BRANCH_NAME}`)
