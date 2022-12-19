d03d8e653248e21ff3fc45750779b7eba856d772
Madhu G Nadig <madhug.nadig@gmail.com>
2022-12-19 17:05:00 +0100

    Count all users on DRS chart (#756)
    * Count all users on DRS chart
    
    * Remove script

---
+++ scripts/migrations/pre-deployment/2022.12.19T11.46.30.migration-flagright-populate-drs.ts
@@ -0,0 +1,54 @@
+import { MigrationFn } from 'umzug'
+import { StackConstants } from '@cdk/constants'
+import { getConfig } from '../utils/config'
+import { AccountsConfig } from '@/lambdas/console-api-account/app'
+import {
+  Tenant,
+  AccountsService,
+} from '@/lambdas/console-api-account/services/accounts-service'
+import { getMongoDbClient, USERS_COLLECTION } from '@/utils/mongoDBUtils'
+import { getDynamoDbClient } from '@/utils/dynamodb'
+import { DashboardStatsRepository } from '@/lambdas/console-api-dashboard/repositories/dashboard-stats-repository'
+import { updateInitialRiskScores } from '@/services/risk-scoring'
+import { Business } from '@/@types/openapi-public/Business'
+import { User } from '@/@types/openapi-public/User'
+
+async function migrateTenant(tenant: Tenant | null, timestamp: number) {
+  if (!tenant) {
+    console.info(`Tenant not found`)
+    return
+  }
+  console.info(`Starting to migrate tenant ${tenant.name} (ID: ${tenant.id})`)
+  const dynamodb = await getDynamoDbClient()
+  const mongoDb = await getMongoDbClient(StackConstants.MONGO_DB_DATABASE_NAME)
+  const usersCollectionName = USERS_COLLECTION(tenant.id)
+  const db = mongoDb.db()
+
+  const usersBeforeTimestamp = await db
+    .collection<User | Business>(usersCollectionName)
+    .find({ createdAt: { $lte: new Date(timestamp) } })
+    .toArray()
+
+  for await (const user of usersBeforeTimestamp) {
+    await updateInitialRiskScores(tenant.id, dynamodb, user)
+  }
+
+  const dashboardStatsRepository = new DashboardStatsRepository(tenant.id, {
+    mongoDb,
+  })
+
+  await dashboardStatsRepository.refreshUserStats()
+}
+
+export const up: MigrationFn = async () => {
+  const config = getConfig()
+  const accountsService = new AccountsService(
+    config.application as AccountsConfig
+  )
+  const flagrightTenant = await accountsService.getTenantById('flagright')
+  await migrateTenant(flagrightTenant, 1671446984000)
+}
+
+export const down: MigrationFn = async () => {
+  // skip
+}
