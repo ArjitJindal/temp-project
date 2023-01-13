66e57d27ef1ffd667e471d27eaa56bee59f8f940
Chia-Lun Wu <chia@flagright.com>
2023-01-13 11:25:27 +0100

    Fix running migration (#800)

--- lib/cdk-tarpon-pipeline-stack.ts
+++ lib/cdk-tarpon-pipeline-stack.ts
@@ -169,6 +169,7 @@ export class CdkTarponPipelineStack extends cdk.Stack {
               commands: [
                 'npm install @tsconfig/node16 ts-node typescript',
                 `export SM_SECRET_ARN=${config.application.ATLAS_CREDENTIALS_SECRET_ARN}`,
+                `export AUTH0_MANAGEMENT_CREDENTIALS_SECRET_ARN=${config.application.AUTH0_MANAGEMENT_CREDENTIALS_SECRET_ARN}`,
                 `export ENV=${env}`,
                 `export AWS_REGION=${config.env.region}`,
                 ...assumeRuleCommands,
