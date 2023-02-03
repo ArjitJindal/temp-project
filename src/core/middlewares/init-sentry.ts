7882190df572893d8071ea81f2dd195870f2247b
Chia-Lun Wu <chia@flagright.com>
2023-02-03 16:30:39 +0800

    Disable sentry performance tracing (#849)
    Currently we only use sentry for error alerts. For performance tracing we're
    using aws X-Ray and CloudWatch metrics to monitor. The current sentry tracing
    eats up our most transactions quota per month. Thus disable it.

--- src/core/middlewares/init-sentry.ts
+++ src/core/middlewares/init-sentry.ts
@@ -28,7 +28,7 @@ export const initSentry =
 
     Sentry.AWSLambda.init({
       dsn: 'https://ecefa05b5cfb4b5998ccc8d4907012c8@o1295082.ingest.sentry.io/6567808', // I think we can hardcode this for now since it is same for all lambdas
-      tracesSampleRate: 0.02,
+      tracesSampleRate: 0,
       environment: process.env.ENV || 'local',
       release: process.env.RELEASE_VERSION,
       integrations: [
