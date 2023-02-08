19c438f22676d391cd557eb3818d3ac91a44488a
Tim Coulson <tim.r.coulson@googlemail.com>
2023-02-08 07:16:11 +0100

    Tim's dev environment (#689)
    add cert ARN for Tim's dev environment
    
    Co-authored-by: Madhu G Nadig <madhug.nadig@gmail.com>

--- lib/configs/config-dev-user.ts
+++ lib/configs/config-dev-user.ts
@@ -10,6 +10,7 @@ const siteArns: { [key: string]: string } = {
   chialunwu: 'arn:aws:acm:us-east-1:911899431626:certificate/638527d1-45c8-4c37-80cf-f8f99aa38f19',
   koluch: 'arn:aws:acm:us-east-1:911899431626:certificate/eb82e34c-a29c-47f1-95ef-9637f9a641e1',
   nadig: 'arn:aws:acm:us-east-1:911899431626:certificate/a80581ad-6e27-47f2-b804-aa0329ea1003',
+  timrcoulson: 'arn:aws:acm:us-east-1:911899431626:certificate/4311800d-8038-4ee1-b01f-072c6ad8dd23',
 };
 
 export const config: Config = {
