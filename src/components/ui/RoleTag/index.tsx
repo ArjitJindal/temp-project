e9a5df10aa190dc6f1037ee5da259d9633400a10
Tim Coulson <tim.r.coulson@googlemail.com>
2023-02-08 12:47:38 +0100

    Fix main: Change AccountRole => AccountRoleName (#698)
    Change AccountRole => AccountRoleName

--- src/components/ui/RoleTag/index.tsx
+++ src/components/ui/RoleTag/index.tsx
@@ -1,20 +1,25 @@
 import { Tag } from 'antd';
 import React from 'react';
-import { AccountRole } from '@/apis';
+import { AccountRoleName } from '@/apis';
 import COLORS, { ColorSet } from '@/components/ui/colors';
 import { neverReturn } from '@/utils/lang';
 
 interface Props {
-  role: AccountRole;
+  role: AccountRoleName;
 }
 
-function getColor(role: AccountRole): ColorSet {
+function getColor(role: AccountRoleName): ColorSet {
   switch (role) {
     case 'root':
       return COLORS.red;
     case 'admin':
       return COLORS.orange;
     case 'user':
+    case 'analyst':
+    case 'approver':
+    case 'auditor':
+    case 'developer':
+    case 'qa_analyst':
       return COLORS.leafGreen;
   }
   return neverReturn(role, COLORS.brandBlue);
