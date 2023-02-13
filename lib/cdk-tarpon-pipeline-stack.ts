173c1c80f6b10c3a768afeacd8bf83e284b6bdd6
Chia-Lun Wu <chia@flagright.com>
2023-02-13 14:46:50 +0800

    Remove skipping tests (#884)
    * Remove skipping tests
    
    Follow-up of 5802c18
    
    * Fix deploy

--- lib/cdk-tarpon-pipeline-stack.ts
+++ lib/cdk-tarpon-pipeline-stack.ts
@@ -26,9 +26,13 @@ const GENERATED_DIRS = [
   'dist',
   'node_modules',
   'src/@types/openapi-internal',
+  'src/@types/openapi-internal-custom',
   'src/@types/openapi-public',
+  'src/@types/openapi-public-custom',
   'src/@types/openapi-public-management',
+  'src/@types/openapi-public-management-custom',
   'src/@types/openapi-public-device-data',
+  'src/@types/openapi-public-device-data-custom',
 ]
 
 function getReleaseVersion(version: string) {
