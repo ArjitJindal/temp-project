696a548951ddb9d20448cc352378630849cb94ed
Tim Coulson <tim@flagright.com>
2023-02-15 16:32:21 +0100

    Add transactions to cases (#896)
    * Add transactions to cases
    
    * Update scripts/migrations/post-deployment/2023.02.15T14.39.04.add-transactions-to-cases.ts
    
    Co-authored-by: Chia-Lun Wu <chia@flagright.com>
    
    ---------
    
    Co-authored-by: Madhu G Nadig <madhug.nadig@gmail.com>
    Co-authored-by: Chia-Lun Wu <chia@flagright.com>

---
+++ scripts/migrations/post-deployment/2023.02.15T14.39.04.add-transactions-to-cases.ts
@@ -0,0 +1,49 @@
+import { StackConstants } from '@cdk/constants'
+import { migrateAllTenants } from '../utils/tenant'
+import {
+  CASES_COLLECTION,
+  getMongoDbClient,
+  TRANSACTIONS_COLLECTION,
+} from '@/utils/mongoDBUtils'
+import { Tenant } from '@/services/accounts'
+import { Case } from '@/@types/openapi-internal/Case'
+import { Transaction } from '@/@types/openapi-internal/Transaction'
+import { CaseTransaction } from '@/@types/openapi-internal/CaseTransaction'
+
+export async function addTransactionsToCases(tenant: Tenant) {
+  const mongodb = await getMongoDbClient(StackConstants.MONGO_DB_DATABASE_NAME)
+
+  const db = mongodb.db()
+  const casesCollections = db.collection<Case>(CASES_COLLECTION(tenant.id))
+  const transactionsCollections = db.collection<Transaction>(
+    TRANSACTIONS_COLLECTION(tenant.id)
+  )
+
+  const cases = await casesCollections.find({
+    $or: [
+      { caseTransactions: { $exists: false } },
+      { 'caseTransactions.0': { $exists: false } },
+    ],
+    caseTransactionsIds: { $exists: true },
+  })
+  for await (const caseItem of cases) {
+    const caseTransactionsCursor = await transactionsCollections.find({
+      transactionId: { $in: caseItem.caseTransactionsIds },
+    })
+    const caseTransactions = await caseTransactionsCursor.toArray()
+    await casesCollections.updateOne(caseItem, {
+      $set: {
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        caseTransactions: caseTransactions as Array<CaseTransaction>,
+      },
+    })
+  }
+}
+
+export const up = async () => {
+  await migrateAllTenants(addTransactionsToCases)
+}
+export const down = async () => {
+  // Put your migration code for rolling back here. If not applicable, skip it.
+}
