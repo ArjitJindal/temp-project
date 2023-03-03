44f9105b4a93955e1071cc128763ac86588978f8
Tim Coulson <tim@flagright.com>
2023-03-03 10:24:32 +0100

    Remigrate user auth roles (#983)
    * Remigrate user auth roles
    
    * Update 2023.03.03T09.30.37.authroles.ts

---
+++ scripts/migrations/post-deployment/2023.03.03T09.30.37.authroles.ts
@@ -0,0 +1,35 @@
+import { migrateAllTenants } from '../utils/tenant'
+import { AccountsService, Tenant } from '@/services/accounts'
+import { RoleService } from '@/services/roles'
+
+async function migrateTenant(tenant: Tenant, auth0Domain: string) {
+  const accountsService = new AccountsService({
+    auth0Domain,
+  })
+  const rolesService = new RoleService({
+    auth0Domain,
+  })
+  const users = await accountsService.getTenantAccounts(tenant)
+  await Promise.all(
+    users
+      .map((u) => {
+        if (u.role == 'user') {
+          console.log(`setting ${u.email} with ${u.role} as analyst`)
+          return rolesService.setRole(tenant.id, u.id, 'analyst')
+        }
+        if (u.role == 'admin') {
+          console.log(`setting ${u.email} with ${u.role} as admin`)
+          return rolesService.setRole(tenant.id, u.id, 'admin')
+        }
+        return null
+      })
+      .filter((p) => p)
+  )
+}
+
+export const up = async () => {
+  await migrateAllTenants(migrateTenant)
+}
+export const down = async () => {
+  // skip
+}
