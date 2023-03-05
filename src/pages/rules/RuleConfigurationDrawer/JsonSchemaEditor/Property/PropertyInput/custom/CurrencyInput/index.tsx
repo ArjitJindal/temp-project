9682285008a1055261654079bb3edf03f204d1d7
zentho <102931585+zentho@users.noreply.github.com>
2023-03-05 08:47:39 -0800

    Add search to currency input (#781)
    Add currency select search property from Ant docs

--- src/pages/rules/RuleConfigurationDrawer/JsonSchemaEditor/Property/PropertyInput/custom/CurrencyInput/index.tsx
+++ src/pages/rules/RuleConfigurationDrawer/JsonSchemaEditor/Property/PropertyInput/custom/CurrencyInput/index.tsx
@@ -7,5 +7,12 @@ interface Props extends InputProps<Currency> {}
 
 export default function CurrencyInput(props: Props) {
   const { ...rest } = props;
-  return <Select options={CURRENCIES_SELECT_OPTIONS} placeholder="Select currency" {...rest} />;
+  return (
+    <Select
+      options={CURRENCIES_SELECT_OPTIONS}
+      placeholder="Select currency"
+      showSearch={true}
+      {...rest}
+    />
+  );
 }
