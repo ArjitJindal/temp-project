4219f6c8eaebf7734f6213227ef3b9c321f8df50
Chia-Lun Wu <chia@flagright.com>
2023-04-24 16:56:01 +0200

    Try to fix EMFILE error - 2 (#1220)
    ref: https://github.com/aws/aws-sdk-js-v3/issues/3019

--- src/core/middlewares/lambda-api-middlewares.ts
+++ src/core/middlewares/lambda-api-middlewares.ts
@@ -1,3 +1,4 @@
+import * as sharedIniFileLoader from '@aws-sdk/shared-ini-file-loader'
 import { compose } from './compose'
 import { contextProvider } from './context-provider'
 import { featureProtected } from './feature-protected'
@@ -8,6 +9,18 @@ import { initSentry } from './init-sentry'
 import { metricsMiddleware } from './metrics-middleware'
 import { Feature } from '@/@types/openapi-internal/Feature'
 import { rbacMiddleware } from '@/core/middlewares/rbac'
+
+// Workaround to get around EMFILE issue in lambda
+// ref: https://github.com/aws/aws-sdk-js-v3/issues/3019#issuecomment-966900587
+if (process.env.ENV !== 'local') {
+  Object.assign(sharedIniFileLoader, {
+    loadSharedConfigFiles:
+      async (): Promise<sharedIniFileLoader.SharedConfigFiles> => ({
+        configFile: {},
+        credentialsFile: {},
+      }),
+  })
+}
 
 export const lambdaApi = (options?: { requiredFeatures?: Feature[] }) => {
   const middlewares = [
