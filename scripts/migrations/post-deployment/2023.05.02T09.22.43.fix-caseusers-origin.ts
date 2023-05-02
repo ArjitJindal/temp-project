e4b05f472afbb3028eb447e3e7203b62fef670b0
Chia-Lun Wu <chia@flagright.com>
2023-05-02 12:24:41 +0200

    Remove invalid caseUsers.origin (#1256)
    Same cause as https://github.com/flagright/tarpon/pull/1240 that we process
    a "user" without userId and accidentally updates all cases without `caseUsers.origin`
    because of https://github.com/flagright/tarpon/blob/497ef4c9f6f29ba7c054522f972fc17e598879e6/src/services/rules-engine/repositories/case-repository.ts#L1573.

---
+++ scripts/migrations/post-deployment/2023.05.02T09.22.43.fix-caseusers-origin.ts
@@ -0,0 +1,24 @@
+import { migrateAllTenants } from '../utils/tenant'
+import { CASES_COLLECTION, getMongoDbClient } from '@/utils/mongoDBUtils'
+import { Tenant } from '@/services/accounts'
+import { Case } from '@/@types/openapi-internal/Case'
+
+async function migrateTenant(tenant: Tenant) {
+  const db = (await getMongoDbClient()).db()
+  const casesCollections = db.collection<Case>(CASES_COLLECTION(tenant.id))
+  await casesCollections.updateMany(
+    {
+      'caseUsers.origin.SortKeyID': { $exists: true },
+      'caseUsers.origin.userId': { $eq: null },
+      'caseUsers.destination.userId': { $ne: null },
+    },
+    { $set: { 'caseUsers.origin': null } }
+  )
+}
+
+export const up = async () => {
+  await migrateAllTenants(migrateTenant)
+}
+export const down = async () => {
+  // skip
+}
