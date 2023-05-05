aaa0d0f0bc7ad2e7e61ac2e501bedea1a263cff6
Aman Dugar <55328714+amandugar@users.noreply.github.com>
2023-05-05 16:30:13 +0530

    Fix JS Stack Trace Memory Limit exceeded in Pipeline (#1273)

--- lib/cdk-tarpon-pipeline-stack.ts
+++ lib/cdk-tarpon-pipeline-stack.ts
@@ -167,6 +167,7 @@ export class CdkTarponPipelineStack extends cdk.Stack {
         `ASSUME_ROLE_ARN="arn:aws:iam::${config.env.account}:role/CodePipelineDeployRole"`,
         `TEMP_ROLE=$(aws sts assume-role --role-arn $ASSUME_ROLE_ARN --role-session-name deploy-${config.region})`,
         'export TEMP_ROLE',
+        'export NODE_OPTIONS=--max-old-space-size=4096',
         'export AWS_ACCESS_KEY_ID=$(echo "${TEMP_ROLE}" | jq -r ".Credentials.AccessKeyId")',
         'export AWS_SECRET_ACCESS_KEY=$(echo "${TEMP_ROLE}" | jq -r ".Credentials.SecretAccessKey")',
         'export AWS_SESSION_TOKEN=$(echo "${TEMP_ROLE}" | jq -r ".Credentials.SessionToken")',
