723a5fe3638235c03105ee68bc98fe1085d8470b
Aman Dugar <55328714+amandugar@users.noreply.github.com>
2023-05-18 23:11:52 +0530

    Count Of Remaining Users in KRS Backfilling Script (#1337)
    Fix

--- scripts/migrations/post-deployment/2023.05.17T21.30.11.fill-users-with-no-drs-score.ts
+++ scripts/migrations/post-deployment/2023.05.17T21.30.11.fill-users-with-no-drs-score.ts
@@ -3,12 +3,29 @@ import { tenantHasFeature } from '@/core/middlewares/tenant-has-feature'
 import { Tenant } from '@/services/accounts'
 import { RiskScoringService } from '@/services/risk-scoring'
 import { getDynamoDbClient } from '@/utils/dynamodb'
-import { getMongoDbClient } from '@/utils/mongoDBUtils'
+import { USERS_COLLECTION, getMongoDbClient } from '@/utils/mongoDBUtils'
+
+const getCountOfUsersWithNoRiskScore = async (tenantId: string) => {
+  const mongoDb = await getMongoDbClient()
+  const db = mongoDb.db()
+  const usersCollection = db.collection(USERS_COLLECTION(tenantId))
+  return await usersCollection.countDocuments({
+    krsScore: { $exists: false },
+  })
+}
 
 async function migrateTenant(tenant: Tenant) {
   if (!(await tenantHasFeature(tenant.id, 'PULSE'))) {
     return
   }
+  const countOfUsersWithNoRiskScore = await getCountOfUsersWithNoRiskScore(
+    tenant.id
+  )
+
+  console.log(
+    `Tenant ${tenant.id} has ${countOfUsersWithNoRiskScore} users with no risk score`
+  )
+
   const dynamoDb = getDynamoDbClient()
   const mongoDb = await getMongoDbClient()
   const riskScoringService = new RiskScoringService(tenant.id, {
