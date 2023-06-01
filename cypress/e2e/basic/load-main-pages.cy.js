9ed74c6582dfbf2f4f1868e0d40386af49cb295a
Sumit Shinde <shindesumit268@gmail.com>
2023-06-01 13:44:33 +0530

    Tests for entities added (#971)
    * Tests for entities added
    
    * Tests added
    
    * config restored
    
    * In progress
    
    * Tags updated
    
    * Tests updated
    
    * Tests updated
    
    ---------
    
    Co-authored-by: Madhu G Nadig <madhug.nadig@gmail.com>

--- cypress/e2e/basic/load-main-pages.cy.js
+++ cypress/e2e/basic/load-main-pages.cy.js
@@ -24,4 +24,62 @@ describe('navigate sidebar', () => {
     cy.get(TABLE_SELECTOR, { timeout: 3000 });
     cy.get('h2', { timeout: 3000 }).contains('My Rules');
   });
+
+  it('should load entity details', () => {
+    cy.visit('/transactions/list', { timeout: 20000 });
+    cy.get(TABLE_SELECTOR).should('exist');
+    cy.get(TABLE_SELECTOR, { timeout: 20000 })
+      .should('be.visible')
+      .then(($table) => {
+        expect($table).to.exist;
+      })
+      .first()
+      .should('have.length.gt', 0)
+      .find('a[data-cy="transaction-id"]', { timeout: 10000 })
+      .eq(0)
+      .click();
+    cy.contains('h3', 'Origin (Sender) and Destination (Receiver) Details').should('be.visible');
+
+    cy.visit('/case-management/cases', { timeout: 8000 });
+    cy.get(TABLE_SELECTOR).should('exist');
+    cy.get(TABLE_SELECTOR, { timeout: 20000 })
+      .should('be.visible')
+      .then(($table) => {
+        expect($table).to.exist;
+      })
+      .first()
+      .should('have.length.gt', 0)
+      .find('a[data-cy="case-id"]', { timeout: 10000 })
+      .eq(0)
+      .click();
+    cy.contains('h3', 'Alerts').should('be.visible');
+
+    cy.visit('/users/list/consumer/all', { timeout: 8000 });
+    cy.get(TABLE_SELECTOR).should('exist');
+    cy.get(TABLE_SELECTOR, { timeout: 20000 })
+      .should('be.visible')
+      .then(($table) => {
+        expect($table).to.exist;
+      })
+      .first()
+      .should('have.length.gt', 0)
+      .find('a[data-cy="consumer-user-id"]', { timeout: 10000 })
+      .eq(0)
+      .click();
+    cy.contains('h3', 'User Details').should('be.visible');
+
+    cy.visit('/users/list/business/all', { timeout: 8000 });
+    cy.get(TABLE_SELECTOR).should('exist');
+    cy.get(TABLE_SELECTOR, { timeout: 20000 })
+      .should('be.visible')
+      .then(($table) => {
+        expect($table).to.exist;
+      })
+      .first()
+      .should('have.length.gt', 0)
+      .find('a[data-cy="business-user-id"]', { timeout: 10000 })
+      .eq(0)
+      .click();
+    cy.contains('h3', 'User Details').should('be.visible');
+  });
 });
