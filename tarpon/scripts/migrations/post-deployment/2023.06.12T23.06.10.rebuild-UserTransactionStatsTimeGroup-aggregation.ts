e12abadf7997162cd5689833bb36329b16e0da9a
Chia-Lun Wu <chia@flagright.com>
2023-06-15 23:37:43 +0200

    Keep migration progress for 2023.06.12T23.06.10.rebuild-UserTransactionStatsTimeGroup-aggregation.ts (#1513)
    It failed midway due to `MongoServerSelectionError: connect ECONNREFUSED 13.234.245.83:27017`.
    Most likely caused by cursor timeout.
    Refactor to keep the migration progress then we can retry until success.
    Also increase the cursor `maxTimeMS`.

--- tarpon/scripts/migrations/post-deployment/2023.06.12T23.06.10.rebuild-UserTransactionStatsTimeGroup-aggregation.ts
+++ tarpon/scripts/migrations/post-deployment/2023.06.12T23.06.10.rebuild-UserTransactionStatsTimeGroup-aggregation.ts
@@ -1,26 +1,50 @@
 import _ from 'lodash'
 import { migrateAllTenants } from '../utils/tenant'
+import {
+  getMigrationLastCompletedTimestamp,
+  updateMigrationLastCompletedTimestamp,
+} from '../utils/migration-progress'
+import { migrateEntities } from '../utils/mongodb'
 import { Tenant } from '@/services/accounts'
-import { getMongoDbClient } from '@/utils/mongoDBUtils'
+import { USERS_COLLECTION, getMongoDbClient } from '@/utils/mongoDBUtils'
 import { GlobalRuleAggregationRebuildBatchJob } from '@/@types/batch-job'
 import { sendBatchJobCommand } from '@/services/batch-job'
-import { UserRepository } from '@/services/users/repositories/user-repository'
+import { InternalUser } from '@/@types/openapi-internal/InternalUser'
 
 async function migrateTenant(tenant: Tenant) {
   const mongoDb = await getMongoDbClient()
-  const userRepository = new UserRepository(tenant.id, { mongoDb })
+  const db = mongoDb.db()
+  const usersCollection = db.collection<InternalUser>(
+    USERS_COLLECTION(tenant.id)
+  )
+  const migrationKey = `2023.06.12T23.06.10.rebuild-aggregation-${tenant.id}`
+  const lastCompletedTimestamp = await getMigrationLastCompletedTimestamp(
+    migrationKey
+  )
+  const cursor = usersCollection
+    .find({
+      createdTimestamp: { $gt: lastCompletedTimestamp ?? 0 },
+    })
+    .sort({ createdTimestamp: 1 })
+    .maxTimeMS(60 * 60 * 1000) // 1 hour
 
-  for await (const user of await userRepository.getAllUserIdsCursor()) {
-    const batchJob: GlobalRuleAggregationRebuildBatchJob = {
-      type: 'GLOBAL_RULE_AGGREGATION_REBUILD',
-      tenantId: tenant.id,
-      parameters: {
-        userId: user.userId,
-        aggregatorName: 'UserTransactionStatsTimeGroup',
-      },
+  await migrateEntities<InternalUser>(cursor, async (usersBatch) => {
+    for (const user of usersBatch) {
+      const batchJob: GlobalRuleAggregationRebuildBatchJob = {
+        type: 'GLOBAL_RULE_AGGREGATION_REBUILD',
+        tenantId: tenant.id,
+        parameters: {
+          userId: user.userId,
+          aggregatorName: 'UserTransactionStatsTimeGroup',
+        },
+      }
+      await sendBatchJobCommand(tenant.id, batchJob)
     }
-    await sendBatchJobCommand(tenant.id, batchJob)
-  }
+    await updateMigrationLastCompletedTimestamp(
+      migrationKey,
+      _.last(usersBatch)?.createdTimestamp ?? 0
+    )
+  })
 }
 
 export const up = async () => {
