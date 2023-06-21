0396daedf8d2bfe6e475c29594ab1bc0b2430e0b
Tim Coulson <tim@flagright.com>
2023-06-21 09:49:31 +0200

    Add a content security policy to cloudfront (#1540)
    Add a content security policy

--- phytoplankton-console/lib/cdk-phytoplankton-stack.ts
+++ phytoplankton-console/lib/cdk-phytoplankton-stack.ts
@@ -10,9 +10,26 @@ import { CnameRecord, HostedZone } from 'aws-cdk-lib/aws-route53';
 import { userAlias } from './configs/config-dev-user';
 import type { Config } from './configs/config';
 
+function contentSecurityPolicy(domain: string) {
+  return `default-src 'self';
+script-src 'self';
+style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
+object-src 'none';
+base-uri 'self';
+connect-src 'self' https://api-js.mixpanel.com https://*.${domain} https://ipinfo.io https://*.ingest.sentry.io;
+font-src 'self' https://fonts.gstatic.com;
+frame-src 'self' https://login.console.${domain};
+img-src 'self' https://s.gravatar.com https://i2.wp.com https://cdnjs.cloudflare.com https://platform.slack-edge.com;
+manifest-src 'self';
+media-src 'self';
+worker-src blob:;`.replace(/(\r\n|\n|\r)/gm, ' ');
+}
+
 export class CdkPhytoplanktonStack extends cdk.Stack {
   constructor(scope: Construct, id: string, config: Config) {
     super(scope, id, { env: config.env });
+    const prefix = process.env.ENV === 'dev:user' ? `${userAlias()}.` : '';
+    const domainName = config.SITE_DOMAIN.replace(prefix, '').replace('console.', '');
 
     const cloudfrontOAI = new cloudfront.OriginAccessIdentity(this, 'cloudfront-OAI', {
       comment: `OAI for ${config.SITE_DOMAIN}`,
@@ -44,7 +61,10 @@ export class CdkPhytoplanktonStack extends cdk.Stack {
 
     const viewerCertificate = cloudfront.ViewerCertificate.fromAcmCertificate(
       acm.Certificate.fromCertificateArn(this, 'SiteCertificate', config.SITE_CERTIFICATE_ARN),
-      { aliases: [config.SITE_DOMAIN], securityPolicy: cloudfront.SecurityPolicyProtocol.TLS_V1_2_2021 },
+      {
+        aliases: [config.SITE_DOMAIN],
+        securityPolicy: cloudfront.SecurityPolicyProtocol.TLS_V1_2_2021,
+      },
     );
 
     const extraBehaviours =
@@ -59,6 +79,7 @@ export class CdkPhytoplanktonStack extends cdk.Stack {
     // CloudFront distribution
     const distribution = new cloudfront.CloudFrontWebDistribution(this, 'SiteDistribution', {
       priceClass: config.CLOUDFRONT_PRICE_CLASS,
+
       viewerCertificate,
       errorConfigurations: [
         {
@@ -92,6 +113,28 @@ export class CdkPhytoplanktonStack extends cdk.Stack {
       ],
     });
 
+    // Hack taken from https://stackoverflow.com/questions/72306740/how-to-enable-managed-response-header-policy-of-securityheaders-with-cloudfrontw
+    const cfnDistribution = distribution.node.defaultChild as cloudfront.CfnDistribution;
+    const responseHeaderCfn = new cdk.CfnResource(this, 'ResponseHeadersPolicyCfn', {
+      type: 'AWS::CloudFront::ResponseHeadersPolicy',
+      properties: {
+        ResponseHeadersPolicyConfig: {
+          Name: `ContentSecurity${userAlias()}`,
+          SecurityHeadersConfig: {
+            ContentSecurityPolicy: {
+              ContentSecurityPolicy: contentSecurityPolicy(domainName),
+              Override: true,
+            },
+          },
+        },
+      },
+    });
+    cfnDistribution.addDependsOn(responseHeaderCfn);
+    cfnDistribution.addPropertyOverride(
+      'DistributionConfig.DefaultCacheBehavior.ResponseHeadersPolicyId',
+      responseHeaderCfn.ref,
+    );
+
     // Deploy site contents to S3 bucket
     new s3deploy.BucketDeployment(this, 'DeployWithInvalidation', {
       sources: [s3deploy.Source.asset('./dist')],
@@ -106,10 +149,8 @@ export class CdkPhytoplanktonStack extends cdk.Stack {
     new CfnOutput(this, 'DistributionDomainName', { value: distribution.distributionDomainName });
 
     if (config.stage === 'dev') {
-      const prefix = process.env.ENV === 'dev:user' ? `${userAlias()}.` : '';
-
       const hostedZone = HostedZone.fromLookup(this, `zone`, {
-        domainName: config.SITE_DOMAIN.replace(prefix, '').replace('console.', ''),
+        domainName,
         privateZone: false,
       });
       new CnameRecord(this, `cname`, {
