df5b52b552ed792f675021af6736133e42e2a399
Jayant Patil <72199264+patiljayant@users.noreply.github.com>
2023-06-22 11:28:11 +0530

    added consumer segment field and enhanced the ui (#1558)
    * added consumer segment field and enhanced the ui
    
    * small fixes
    
    ---------
    
    Co-authored-by: Madhu G Nadig <madhug.nadig@gmail.com>

--- phytoplankton-console/src/pages/users-item/UserDetails/ConsumerUserDetails/UserDetails/UsersInfoCard/index.tsx
+++ phytoplankton-console/src/pages/users-item/UserDetails/ConsumerUserDetails/UserDetails/UsersInfoCard/index.tsx
@@ -5,6 +5,7 @@ import { dayjs, DEFAULT_DATE_TIME_FORMAT } from '@/utils/dayjs';
 import { InternalConsumerUser } from '@/apis';
 import TimerLineIcon from '@/components/ui/icons/Remix/system/timer-line.react.svg';
 import Calender2LineIcon from '@/components/ui/icons/Remix/business/calendar-2-line.react.svg';
+import BookmarkLineIcon from '@/components/ui/icons/Remix/business/bookmark-3-line.react.svg';
 import User3LineIcon from '@/components/ui/icons/Remix/user/user-3-line.react.svg';
 import EarthLineIcon from '@/components/ui/icons/Remix/map/earth-line.react.svg';
 import Home4LineIcon from '@/components/ui/icons/Remix/buildings/home-4-line.react.svg';
@@ -52,9 +53,14 @@ export default function UsersInfoCard(props: Props) {
           {dayjs(user.createdTimestamp).format(DEFAULT_DATE_TIME_FORMAT)}
         </Form.Layout.Label>
       </div>
-      <div>
+      <div className={s.inner}>
         <Form.Layout.Label icon={<StoreLineIcon />} title={'Acquisition Channel'}>
           {user.acquisitionChannel ?? '-'}
+        </Form.Layout.Label>
+      </div>
+      <div className={s.inner}>
+        <Form.Layout.Label icon={<BookmarkLineIcon />} title={'Consumer Segment'}>
+          {user.userSegment ?? '-'}
         </Form.Layout.Label>
       </div>
       <div className={s.inner}>
@@ -69,13 +75,6 @@ export default function UsersInfoCard(props: Props) {
             '-'
           )}
         </Form.Layout.Label>
-      </div>
-      <div>
-        <div>
-          <Form.Layout.Label icon={<StoreLineIcon />} title={'Acquisition Channel'}>
-            {user.acquisitionChannel ?? '-'}
-          </Form.Layout.Label>
-        </div>
       </div>
       <div className={s.tag}>
         <Form.Layout.Label icon={<DeleteBackLineIcon />} title={'Tags'}>
