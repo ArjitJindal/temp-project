ba162d9cf3cda218ffa2fc9aaa5ef4b9de92443f
Aman Dugar <55328714+amandugar@users.noreply.github.com>
2023-07-14 16:35:52 +0530

    Fix Sar Test (#1745)
    Fix

--- phytoplankton-console/cypress/e2e/sar/sar-generate.cy.ts
+++ phytoplankton-console/cypress/e2e/sar/sar-generate.cy.ts
@@ -5,15 +5,30 @@ describe('SAR Generate', () => {
 
   it('should generate SAR', () => {
     cy.visit('/case-management/cases', { timeout: 15000 });
-    cy.get('button[data-cy="segmented-control-all"]', { timeout: 15000 }).click();
-    cy.get('a[data-cy="case-id"]', { timeout: 15000 }).eq(0).click();
+    cy.visit(
+      '/case-management/cases?sort=-lastStatusChange.timestamp&showCases=ALL_ALERTS&alertStatus=CLOSED',
+    );
+    cy.get('body').then((body) => {
+      if (body.find('input[data-cy="row-table-checkbox"]').length > 0) {
+        cy.get('input[data-cy="header-table-checkbox"]', { timeout: 15000 }).click();
+        cy.get('button[data-cy="update-status-button"]', { timeout: 15000 }).eq(1).click();
+        cy.get('button[data-cy="modal-ok-ⓘ-confirm-action"]').eq(0).click();
+      }
+    });
+    cy.visit(
+      '/case-management/cases?sort=-lastStatusChange.timestamp&showCases=ALL&caseStatus=OPEN',
+    );
+    cy.get('a[data-cy="case-id"]', { timeout: 15000 })
+      .should('be.visible')
+      .first()
+      .click({ force: true });
     cy.get('div[aria-controls="rc-tabs-0-panel-alerts"]', { timeout: 15000 }).click();
     cy.get('button[data-cy="expand-icon"]').eq(0).click();
     cy.get('div[id="rc-tabs-1-panel-transactions"] input[data-cy="row-table-checkbox"]', {
       timeout: 15000,
     })
-      .eq(0)
-      .click();
+      .first()
+      .click({ force: true });
     cy.get('button[data-cy="sar-button"]').click();
     cy.get('label[data-cy="sar-country-select"] input').click();
     cy.get('div[title="Kenya"]').click();
