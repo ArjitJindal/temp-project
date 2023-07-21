07e2ff6e57665ebd635cd49f399b4cf16a054cfa
Tim Coulson <tim@flagright.com>
2023-07-21 13:37:29 +0200

    Local env for dev init (#1800)

--- tarpon/src/core/xray/index.ts
+++ tarpon/src/core/xray/index.ts
@@ -1,11 +1,12 @@
 import { Subsegment } from 'aws-xray-sdk-core'
 import { logger } from '@/core/logger'
+import { envIsNot } from '@/utils/env'
 
 // NOTE: Allowed special chars: _, ., :, /, %, &, #, =, +, \, -, @
 // ref: https://docs.aws.amazon.com/xray/latest/devguide/xray-api-segmentdocuments.html
 const ALLOWED_SPECIAL_CHAR_REGEX = /[`,~!#$^*()|?;'"<>{}[\]/]/g
 
-const xrayDisabled = process.env.ENV === 'local' || !!process.env.MIGRATION_TYPE
+const xrayDisabled = envIsNot('dev', 'sandbox', 'prod')
 
 let xrayInitialized = false
 
