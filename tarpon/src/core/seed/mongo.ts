9d48f1bc0a45968c28c2d559fbe0262b4bb7da55
Aman Dugar <55328714+amandugar@users.noreply.github.com>
2023-08-06 20:06:20 +0530

    Added Fix in Seeder (#1929)

--- tarpon/src/core/seed/mongo.ts
+++ tarpon/src/core/seed/mongo.ts
@@ -217,9 +217,13 @@ const getStatusChangesObject = (
 
 const getRandomAssigneeObject = (accounts: Account[]): Assignment => {
   const randomAccountIndex = getRandomIntInclusive(0, accounts.length - 1)
-
+  logger.info(
+    `Assigning to ${accounts[randomAccountIndex]?.id}, JSON: ${JSON.stringify(
+      accounts[randomAccountIndex]
+    )}`
+  )
   return {
-    assigneeUserId: accounts[randomAccountIndex].id,
+    assigneeUserId: accounts[randomAccountIndex]?.id,
     timestamp: Date.now(),
   }
 }
