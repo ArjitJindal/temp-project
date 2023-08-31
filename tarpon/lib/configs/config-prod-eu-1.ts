b9e12e35f993a8cbe73c377facf33fb739814ebd
Madhu G Nadig <madhug.nadig@gmail.com>
2023-08-31 08:53:00 +0200

    Increase mem to 4096

--- tarpon/lib/configs/config-prod-eu-1.ts
+++ tarpon/lib/configs/config-prod-eu-1.ts
@@ -34,7 +34,7 @@ export const config: Config = {
     },
     TRANSACTION_LAMBDA: {
       PROVISIONED_CONCURRENCY: 30,
-      MEMORY_SIZE: 2048,
+      MEMORY_SIZE: 4096,
     },
     TRANSACTIONS_VIEW_LAMBDA: {
       PROVISIONED_CONCURRENCY: 5,
