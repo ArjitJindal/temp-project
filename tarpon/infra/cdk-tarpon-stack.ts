b7ffccea3ee95a1c6a6a4bce5ce3244b29819cc3
Chia-Lun Wu <chia@flagright.com>
2023-09-13 14:42:35 +0200

    Use the default max receive count for the transaction aggregation lambda (#2207)
    1000 -> 30

--- tarpon/infra/cdk-tarpon-stack.ts
+++ tarpon/infra/cdk-tarpon-stack.ts
@@ -148,7 +148,6 @@ export class CdkTarponStack extends cdk.Stack {
       SQSQueues.TRANSACTION_AGGREGATION_QUEUE_NAME,
       {
         visibilityTimeout: CONSUMER_SQS_VISIBILITY_TIMEOUT,
-        maxReceiveCount: MAX_SQS_RECEIVE_COUNT,
         fifo: true,
         retentionPeriod: Duration.days(7),
       }
