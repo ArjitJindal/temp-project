48eb720d2fe053e72b16ea3c510cbff9a295c5dd
Aman Dugar <55328714+amandugar@users.noreply.github.com>
2023-09-13 16:50:31 +0530

    Delete Migrations Older Then 15 Aug (#2204)

--- tarpon/scripts/migrations/post-deployment/2023.05.17T09.19.33.payment-method-to-payment-methods-array.ts
+++
@@ -1,8 +0,0 @@
-import { deleteUnusedRuleFilter } from '../utils/rule'
-
-export const up = async () => {
-  await deleteUnusedRuleFilter(['paymentMethod'])
-}
-export const down = async () => {
-  // skip
-}
--- tarpon/scripts/migrations/post-deployment/2023.08.01T11.11.28.remove-unused-rule-filter.ts
+++
@@ -1,8 +0,0 @@
-import { deleteUnusedRuleFilter } from '../utils/rule'
-
-export const up = async () => {
-  await deleteUnusedRuleFilter(['productType'])
-}
-export const down = async () => {
-  // skip
-}
