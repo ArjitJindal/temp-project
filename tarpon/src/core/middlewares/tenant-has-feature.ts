f5e783bef6beaada958a90ed244d121d42445642
Tim Coulson <tim@flagright.com>
2023-10-04 08:52:35 +0200

    Revert "Refractor Tenant Has Feature (#2221)"
    This reverts commit 95b483b4e1fefa9ae87c6e964cb1ddbd88a9f9b6.

--- tarpon/src/core/middlewares/tenant-has-feature.ts
+++ tarpon/src/core/middlewares/tenant-has-feature.ts
@@ -1,12 +1,8 @@
-import { isEmpty } from 'lodash'
 import { TenantRepository } from '@/services/tenants/repositories/tenant-repository'
+
 import { Feature } from '@/@types/openapi-internal/Feature'
 import { getDynamoDbClient } from '@/utils/dynamodb'
-import {
-  getContext,
-  getTestEnabledFeatures,
-  updateTenantFeatures,
-} from '@/core/utils/context'
+import { getTestEnabledFeatures } from '@/core/utils/context'
 
 // Function for finaind tenant specific feature - to be used in global systems without context like Kinesis Consumers
 // For lambdas in console API or public API, use `useFeature` from context instead
@@ -17,20 +13,20 @@ export async function tenantHasFeature(
   const tenantRepository = new TenantRepository(tenantId, {
     dynamoDb: getDynamoDbClient(),
   })
-
-  const contextFeatures = getContext()?.features
-
-  const features = isEmpty(contextFeatures)
-    ? (await tenantRepository.getTenantSettings(['features']))?.features
-    : contextFeatures
-
-  if (isEmpty(contextFeatures) && features) {
-    updateTenantFeatures(features)
-  }
-
+  const features = (await tenantRepository.getTenantSettings(['features']))
+    ?.features
   return (
     features?.includes(feature) ||
     getTestEnabledFeatures()?.includes(feature) ||
     false
   )
+}
+
+export async function getTenantFeatures(tenantId: string): Promise<Feature[]> {
+  const tenantRepository = new TenantRepository(tenantId, {
+    dynamoDb: getDynamoDbClient(),
+  })
+  const features = (await tenantRepository.getTenantSettings(['features']))
+    ?.features
+  return features || []
 }
--- tarpon/src/core/utils/context.ts
+++ tarpon/src/core/utils/context.ts
@@ -166,13 +166,6 @@ export function updateLogMetadata(addedMetadata: { [key: string]: any }) {
   }
 }
 
-export function updateTenantFeatures(features: Feature[]) {
-  const context = asyncLocalStorage.getStore()
-  if (context) {
-    context.features = features
-  }
-}
-
 export function publishMetric(
   metric: Metric,
   value: number,
