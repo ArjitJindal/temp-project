565ca2583914d70cd6305ff12c1606bb293ae7cc
Jayant Patil <72199264+patiljayant@users.noreply.github.com>
2023-10-05 18:34:15 +0530

    Show country code and BIC code in bank details (#2403)
    * initial commit
    
    * addec conuntry code and bic code to bank details dynamically
    
    * minor changes reverted
    
    * added scroll
    
    * added scroll
    
    * dont need to specify column count
    
    * minor changes
    
    * minor changes
    
    * minor changes

--- phytoplankton-console/src/pages/users-item/UserDetails/BusinessUserDetails/UserDetails/BankDetails/index.tsx
+++ phytoplankton-console/src/pages/users-item/UserDetails/BusinessUserDetails/UserDetails/BankDetails/index.tsx
@@ -4,7 +4,8 @@ import { IBANDetails, GenericBankAccountDetails, InternalBusinessUser } from '@/
 import * as Card from '@/components/ui/Card';
 import AccountPinCircleLineIcon from '@/components/ui/icons/Remix/user/account-pin-circle-line.react.svg';
 import AccountBoxLineIcon from '@/components/ui/icons/Remix/user/account-box-line.react.svg';
-import { PropertyColumns } from '@/pages/users-item/UserDetails/PropertyColumns';
+import FlagLineIcon from '@/components/ui/icons/Remix/business/flag-line.react.svg';
+import FileListIcon from '@/components/ui/icons/Remix/document/file-list-line.react.svg';
 
 interface Props {
   user: InternalBusinessUser;
@@ -12,30 +13,39 @@ interface Props {
 
 export function BankDetails(prop: Props) {
   const { user } = prop;
+  const showBicCodeColumn =
+    user.savedPaymentDetails?.filter(
+      (paymentDetail) => paymentDetail.method === 'IBAN' && paymentDetail.BIC !== undefined,
+    ).length !== 0;
   return (
     <Card.Column>
       <Card.Row className={s.header}>
         <Card.Subtitle className={s.title} icon={<AccountPinCircleLineIcon />} title="Bank name" />
-        <Card.Subtitle
-          className={s.title}
-          icon={<AccountBoxLineIcon />}
-          title="Bank account number"
-        />
+        <Card.Subtitle className={s.title} icon={<AccountBoxLineIcon />} title="Account number" />
+        <Card.Subtitle className={s.title} icon={<FlagLineIcon />} title="Country code" />
+        {showBicCodeColumn && (
+          <Card.Subtitle className={s.title} icon={<FileListIcon />} title="BIC code" />
+        )}
       </Card.Row>
-      <Card.Section className={s.root}>
-        <PropertyColumns>
-          {(user.savedPaymentDetails ?? [])
-            .filter((details): details is IBANDetails | GenericBankAccountDetails => {
-              return details.method === 'IBAN' || details.method === 'GENERIC_BANK_ACCOUNT';
-            })
-            .map((details, i) => (
-              <React.Fragment key={i}>
-                <div>{details.bankName}</div>
-                <div>{details.method === 'IBAN' ? details.IBAN : details.accountNumber}</div>
-              </React.Fragment>
-            ))}
-        </PropertyColumns>
-      </Card.Section>
+      <div className={s.root}>
+        {(user.savedPaymentDetails ?? [])
+          .filter((details): details is IBANDetails | GenericBankAccountDetails => {
+            return details.method === 'IBAN' || details.method === 'GENERIC_BANK_ACCOUNT';
+          })
+          .map((details, i) => {
+            const isIBAN = details.method === 'IBAN';
+            return (
+              <div className={s.row} key={i}>
+                <div className={s.cell}>{details.bankName ?? '-'}</div>
+                <div className={s.cell}>{isIBAN ? details.IBAN : details.accountNumber}</div>
+                <div className={s.cell}>{isIBAN ? details.country ?? '-' : '-'}</div>
+                {showBicCodeColumn && (
+                  <div className={s.cell}>{isIBAN ? details.BIC ?? '-' : '-'}</div>
+                )}
+              </div>
+            );
+          })}
+      </div>
     </Card.Column>
   );
 }
