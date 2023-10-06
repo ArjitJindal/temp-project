05376247e3a516e59dc249fc859a03a5d3cb8ff0
Aman Dugar <55328714+amandugar@users.noreply.github.com>
2023-10-06 20:51:27 +0530

    Mock Currency API (#2430)

--- tarpon/jest-setup.ts
+++ tarpon/jest-setup.ts
@@ -1,6 +1,7 @@
 import { exec, execSync } from 'child_process'
 import { TEST_DYNAMODB_TABLE_NAMES } from './src/test-utils/dynamodb-test-utils'
-
+import MOCKED_CURRENCY_EXCHANGE_RATES from './test-resources/mocked-currency-exchange-rates.json'
+import * as CurrencyUtils from './src/utils/currency-utils'
 process.env.ENV = 'local'
 process.env.DYNAMODB_URI = 'http://localhost:7999'
 if (!process.env.EXEC_SOURCE) {
@@ -15,6 +16,54 @@ jest.mock('@lib/constants', () => ({
     TRANSIENT_DYNAMODB_TABLE_NAME: TEST_DYNAMODB_TABLE_NAMES[3],
   },
 }))
+
+jest.mock('@/utils/currency-utils', () => {
+  const originalModule = jest.requireActual('@/utils/currency-utils')
+  return {
+    ...originalModule,
+    getCurrencyExchangeRate: jest.fn().mockImplementation((...args) => {
+      const [sourceCurrency, targetCurrency] = args
+
+      if (sourceCurrency === targetCurrency) {
+        return 1
+      }
+
+      return MOCKED_CURRENCY_EXCHANGE_RATES[sourceCurrency][targetCurrency]
+    }),
+    getTargetCurrencyAmount: jest.fn().mockImplementation((...args) => {
+      const [transactionAmountDefails, targetCurrency] = args
+      const sourceCurrency = transactionAmountDefails.transactionCurrency
+
+      if (sourceCurrency === targetCurrency) {
+        return transactionAmountDefails
+      }
+
+      const rate =
+        MOCKED_CURRENCY_EXCHANGE_RATES?.[sourceCurrency]?.[targetCurrency]
+
+      if (rate == null) {
+        throw new Error(
+          `Mocked getCurrencyExchangeRate(${sourceCurrency}, ${targetCurrency}) is not implemented`
+        )
+      }
+
+      return {
+        transactionAmount: transactionAmountDefails.transactionAmount * rate,
+        transactionCurrency: targetCurrency,
+      }
+    }),
+  }
+})
+
+jest
+  .spyOn(CurrencyUtils, 'getCurrencyExchangeRate')
+  .mockImplementation(async (...args) => {
+    const [sourceCurrency, targetCurrency] = args
+    console.log(
+      `Mocked getCurrencyExchangeRate(${sourceCurrency}, ${targetCurrency})`
+    )
+    return MOCKED_CURRENCY_EXCHANGE_RATES[sourceCurrency][targetCurrency]
+  })
 
 module.exports = async function () {
   if (process.env.EXEC_SOURCE !== 'CI') {
