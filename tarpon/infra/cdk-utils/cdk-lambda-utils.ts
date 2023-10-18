61938dbf7ce1f198740cb9f473ef511788d9ccf5
Aman Dugar <55328714+amandugar@users.noreply.github.com>
2023-10-18 23:54:12 +0530

    Reduce Number of Transactions (#2517)

--- tarpon/infra/cdk-utils/cdk-lambda-utils.ts
+++ tarpon/infra/cdk-utils/cdk-lambda-utils.ts
@@ -91,7 +91,7 @@ export function createFunction(
         RELEASE_VERSION: process.env.RELEASE_VERSION as string,
         LAMBDA_CODE_PATH: LAMBDAS[name].codePath,
         QA_SUBDOMAIN: process.env.QA_SUBDOMAIN as string,
-        SEED_TRANSACTIONS_COUNT: '10000',
+        SEED_TRANSACTIONS_COUNT: '4000',
       },
     },
     functionName: name,
