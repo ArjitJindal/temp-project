6d23842f09190b65ed328f7d25a16fb3bf9992b8
Ankit Rastogi <ankitrastogi31200@gmail.com>
2023-10-27 15:58:28 +0530

    name updated (#2580)

--- phytoplankton-console/cypress/e2e/cases/caseid-filter-test.cy.ts
+++ phytoplankton-console/cypress/e2e/cases/caseid-filter-test.cy.ts
@@ -13,7 +13,7 @@ describe('Filter according to case id', () => {
       .then((caseid) => {
         testCaseId = caseid.text().substring(0, 3);
         expect(testCaseId.length).to.be.greaterThan(0);
-        cy.get('[data-cy="Case ID-button"]', { timeout: 15000 }).eq(0).click();
+        cy.get('[data-cy="case-id-button"]', { timeout: 15000 }).eq(0).click();
         cy.get('.ant-popover-inner-content input', { timeout: 15000 }).type(testCaseId);
         cy.url().should('include', 'caseId');
         cy.get('[data-cy="table-body"]').within(() => {
