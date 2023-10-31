28c72fe638442855cd2e1ad5982c7be8daa3cefa
Ankit Rastogi <ankitrastogi31200@gmail.com>
2023-10-31 18:13:04 +0530

    fixed the test (#2607)

--- phytoplankton-console/cypress/e2e/cases/caseid-filter-test.cy.ts
+++ phytoplankton-console/cypress/e2e/cases/caseid-filter-test.cy.ts
@@ -13,8 +13,12 @@ describe('Filter according to case id', () => {
       .then((caseid) => {
         testCaseId = caseid.text().substring(0, 3);
         expect(testCaseId.length).to.be.greaterThan(0);
-        cy.get('[data-cy="case-id-button"]', { timeout: 15000 }).eq(0).click();
-        cy.get('.ant-popover-inner-content input', { timeout: 15000 }).type(testCaseId);
+        cy.get('[data-cy="rules-filter"]')
+          .filter(':contains("Case ID")')
+          .eq(0)
+          .should('exist')
+          .click();
+        cy.get('.ant-popover-inner-content input', { timeout: 15000 }).eq(0).type(testCaseId);
         cy.url().should('include', 'caseId');
         cy.get('[data-cy="table-body"]').within(() => {
           cy.get('[data-cy="caseId"]').each(($element) => {
