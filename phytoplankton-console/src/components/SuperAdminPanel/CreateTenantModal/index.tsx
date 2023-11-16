c2302578678d7ba3d24c24b74876d728594dfa76
Aman Dugar <55328714+amandugar@users.noreply.github.com>
2023-11-16 15:48:04 +0530

    Update Help while Creating Tenant (#2734)
    Fix Help while Creating Tenant

--- phytoplankton-console/src/components/SuperAdminPanel/CreateTenantModal/index.tsx
+++ phytoplankton-console/src/components/SuperAdminPanel/CreateTenantModal/index.tsx
@@ -203,7 +203,7 @@ export const CreateTenantModal = (props: Props) => {
       },
       auth0Domain: {
         'ui:help':
-          'If the tenant does not belong to our white-label customer, please add "sandbox-flagright.eu.auth0.com" as the Auth0 Domain. However, if the tenant belongs to our white-label customer, please add the Auth0 domain for their specific bureau, for example, "bureau-flagright.eu.auth0.com".',
+          'If the tenant does not belong to a whitelabel customer, please add "sandbox-flagright.eu.auth0.com" as the Auth0 Domain. However, if the tenant belongs to a whitelabel customer, please add the Auth0 domain for their specific instance, such as "customerName-flagright.eu.auth0.com". Ask for the specific URL on Slack, #sales-engineering channel.',
       },
       emailsOfAdmins: {
         'ui:help': 'The emails of the admins of the tenant',
