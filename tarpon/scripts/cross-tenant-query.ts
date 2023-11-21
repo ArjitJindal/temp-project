6802da261d9e56c71db336ef065f336ea4e836c4
Chia-Lun Wu <chia@flagright.com>
2023-11-21 10:00:29 +0100

    Extend 'cross-tenant-query' with predefined queries (#2742)
    e.g
    npm run cross-tenant-query:prod -- --query rule-stats

--- tarpon/scripts/cross-tenant-query.ts
+++ tarpon/scripts/cross-tenant-query.ts
@@ -1,15 +1,89 @@
+/**
+ * Usage:
+ * 1. Implement `runReadOnlyQueryForTenant` and run `npm run cross-tenant-query:dev`
+ * 2. Run `npm run cross-tenant-query:dev -- --query rule-stats`
+ */
+
 import { exit } from 'process'
-import commandLineArgs from 'command-line-args'
+import { program } from 'commander'
 import { render } from 'prettyjson'
 import { Db } from 'mongodb'
-import { isEmpty } from 'lodash'
+import { isEmpty, mergeWith, sortBy, startCase } from 'lodash'
 import { DynamoDBDocumentClient } from '@aws-sdk/lib-dynamodb'
 import { getConfig, loadConfigEnv } from './migrations/utils/config'
 import { TenantService } from '@/services/tenants'
 import { getMongoDbClientDb } from '@/utils/mongodb-utils'
 import { Env, PRODUCTION_ENVS } from '@/utils/env'
 import { getDynamoDbClient } from '@/utils/dynamodb'
+import { RuleInstanceRepository } from '@/services/rules-engine/repositories/rule-instance-repository'
+import {
+  RULES_LIBRARY,
+  getRuleByRuleId,
+} from '@/services/rules-engine/transaction-rules/library'
+import {
+  TRANSACTION_FILTERS,
+  TRANSACTION_HISTORICAL_FILTERS,
+  USER_FILTERS,
+} from '@/services/rules-engine/filters'
 
+/**
+ * Rule Stats
+ */
+type CountStats = { [key: string]: number }
+let globalRuleStats: CountStats = Object.fromEntries(
+  RULES_LIBRARY.map((r) => [r.id, 0])
+)
+let globalFilterStats: CountStats = Object.fromEntries(
+  Object.keys({
+    ...TRANSACTION_FILTERS,
+    ...TRANSACTION_HISTORICAL_FILTERS,
+    ...USER_FILTERS,
+  }).map((k) => [k, 0])
+)
+async function tenantRuleStats(
+  dynamoDb: DynamoDBDocumentClient,
+  tenantId: string
+): Promise<{ ruleStats: CountStats; filterStats: CountStats }> {
+  const ruleInstanceRepository = new RuleInstanceRepository(tenantId, {
+    dynamoDb,
+  })
+  const ruleInstances = await ruleInstanceRepository.getAllRuleInstances()
+  const tenantRuleStats: CountStats = {}
+  const tenantFilterStats: CountStats = {}
+  ruleInstances.forEach(
+    (r) => (tenantRuleStats[r.ruleId] = (tenantRuleStats[r.ruleId] ?? 0) + 1)
+  )
+  ruleInstances
+    .flatMap((r) => Object.keys(r.filters || {}))
+    .forEach((k) => (tenantFilterStats[k] = (tenantFilterStats[k] ?? 0) + 1))
+
+  return {
+    ruleStats: tenantRuleStats,
+    filterStats: tenantFilterStats,
+  }
+}
+function printRuleStats(ruleStats: CountStats, filterStats: CountStats) {
+  const sortedRuleStats = Object.fromEntries(
+    sortBy(Object.entries(ruleStats), (entry) => -entry[1]).map((entry) => [
+      `${entry[0]} (${getRuleByRuleId(entry[0])?.name})`,
+      entry[1],
+    ])
+  )
+  const sortedFileterStats = Object.fromEntries(
+    sortBy(Object.entries(filterStats), (entry) => -entry[1]).map((entry) => [
+      `${startCase(entry[0])} (${entry[0]})`,
+      entry[1],
+    ])
+  )
+  console.info('\nRules usage:')
+  console.info(isEmpty(sortedRuleStats) ? 'N/A/' : render(sortedRuleStats))
+  console.info('\nFilters usage:')
+  console.info(isEmpty(sortedFileterStats) ? 'N/A' : render(sortedFileterStats))
+}
+
+/**
+ * Custom query
+ */
 async function runReadOnlyQueryForTenant(
   _mongoDb: Db,
   _dynamoDb: DynamoDBDocumentClient,
@@ -28,9 +102,14 @@ async function runReadOnlyQueryForTenant(
   return { Hello: 'World' }
 }
 
-const optionDefinitions = [{ name: 'env', type: String }]
-const options = commandLineArgs(optionDefinitions)
-if (!['dev', 'sandbox', 'prod'].includes(options.env)) {
+program
+  .requiredOption('--env <string>', 'dev | sandbox | prod')
+  .option('--query <string>', 'rule-stats')
+  .parse()
+
+const { env, query } = program.opts()
+
+if (!['dev', 'sandbox', 'prod'].includes(env)) {
   console.error(`Allowed --env options: dev, sandbox prod`)
   exit(1)
 }
@@ -46,30 +125,58 @@ async function runReadOnlyQueryForEnv(env: Env) {
     config.region
   )
 
-  for (const tenant of tenantInfos) {
-    const result = await runReadOnlyQueryForTenant(
-      mongoDb,
-      dynamoDb,
-      tenant.tenant.id
-    )
-    if (!isEmpty(result)) {
-      console.info(
-        `\nTenant: ${tenant.tenant.name} (ID: ${tenant.tenant.id}) (region: ${tenant.tenant.region})`
+  if (query === 'rule-stats') {
+    for (const tenant of tenantInfos) {
+      const result = await tenantRuleStats(dynamoDb, tenant.tenant.id)
+      globalRuleStats = mergeWith(
+        globalRuleStats,
+        result.ruleStats,
+        (a, b) => (a ?? 0) + (b ?? 0)
       )
-      console.log(render(result))
+      globalFilterStats = mergeWith(
+        globalFilterStats,
+        result.filterStats,
+        (a, b) => (a ?? 0) + (b ?? 0)
+      )
+
+      if (!isEmpty(result.ruleStats) || !isEmpty(result.filterStats)) {
+        console.info(
+          `\nTenant: ${tenant.tenant.name} (ID: ${tenant.tenant.id}) (region: ${tenant.tenant.region})`
+        )
+        printRuleStats(result.ruleStats, result.filterStats)
+      }
+    }
+  } else {
+    for (const tenant of tenantInfos) {
+      const result = await runReadOnlyQueryForTenant(
+        mongoDb,
+        dynamoDb,
+        tenant.tenant.id
+      )
+      if (!isEmpty(result)) {
+        console.info(
+          `\nTenant: ${tenant.tenant.name} (ID: ${tenant.tenant.id}) (region: ${tenant.tenant.region})`
+        )
+        console.log(render(result))
+      }
     }
   }
 }
 
 async function main() {
-  if (options.env === 'dev') {
+  if (env === 'dev') {
     await runReadOnlyQueryForEnv('dev')
-  } else if (options.env === 'sandbox') {
+  } else if (env === 'sandbox') {
     await runReadOnlyQueryForEnv('sandbox')
-  } else if (options.env === 'prod') {
+  } else if (env === 'prod') {
     for (const env of PRODUCTION_ENVS) {
       await runReadOnlyQueryForEnv(env)
     }
+  }
+
+  if (query === 'rule-stats') {
+    console.info('\n================ All Tenants ================')
+    printRuleStats(globalRuleStats, globalFilterStats)
   }
 }
 
