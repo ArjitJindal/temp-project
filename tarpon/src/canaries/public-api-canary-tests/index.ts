20ee8caa8efe063c6d842b9f7eb93bf5e6383016
Aman Dugar <55328714+amandugar@users.noreply.github.com>
2023-11-22 16:20:26 +0530

    Add remaining transaction tests of canaries (#2753)
    * Add remaining transaction tests for Canaries
    
    * Increase Interval to 5 minutes
    
    * Increase Interval to 5 minutes
    
    * Changes

--- tarpon/src/canaries/public-api-canary-tests/index.ts
+++ tarpon/src/canaries/public-api-canary-tests/index.ts
@@ -141,6 +141,7 @@ const getTestTransactionSuccess = async () => {
   }
 
   await Promise.all([
+    // Transaction tests
     executeHttpStep<Transaction, TransactionMonitoringResult>(
       'Create transaction',
       '/transactions',
@@ -251,6 +252,136 @@ const getTestTransactionSuccess = async () => {
         },
       },
       { incorrectDomain: true }
+    ),
+    executeHttpStep<Transaction, ValidationError>(
+      'Incorrect timestamp',
+      '/transactions',
+      {
+        ...transactionPayload,
+        timestamp: 1,
+      },
+      {
+        statusCode: 400,
+        statusMessage: 'Bad Request',
+        dataCallback: (_, data, reject) => {
+          if (
+            data.validationErrors !==
+            '[numeric instance is lower than the required minimum (minimum: 1262300400000, found: 1)]'
+          ) {
+            reject('Validation error does not match')
+          }
+
+          if (data.message !== 'Invalid request body') {
+            reject('Message does not match')
+          }
+        },
+      }
+    ),
+    executeHttpStep<Transaction, ValidationError>(
+      'Invalid transaction method',
+      '/transactions',
+      {
+        ...transactionPayload,
+        originPaymentDetails: {
+          method: 'INVALID' as any,
+        },
+      },
+      {
+        statusCode: 400,
+        statusMessage: 'Bad Request',
+        dataCallback: (_, data, reject) => {
+          if (
+            data.validationErrors !==
+            '[instance failed to match exactly one schema (matched 0 out of 9)]'
+          ) {
+            reject('Validation error does not match')
+          }
+
+          if (data.message !== 'Invalid request body') {
+            reject('Message does not match')
+          }
+        },
+      }
+    ),
+    executeHttpStep<Transaction, ValidationError>(
+      'Invalid country code',
+      '/transactions',
+      {
+        ...transactionPayload,
+        destinationAmountDetails: {
+          transactionAmount: 100,
+          country: 'AB' as any,
+          transactionCurrency: 'EUR',
+        },
+      },
+      {
+        statusCode: 400,
+        statusMessage: 'Bad Request',
+        dataCallback: (_, data, reject) => {
+          if (
+            data.validationErrors !==
+            '[instance value ("AB") not found in enum (possible values: ["AF","AL","DZ","AS","AD","AO","AI","AQ","AG","AR","AM","AW","AU","AT","AZ","BS","BH","BD","BB","BY","BE","BZ","BJ","BM","BT","BO","BQ","BA","BW","BV","BR","IO","BN","BG","BF","BI","CV","KH","CM","CA","KY","CF","TD","CL","CN","CX","CC","CO","KM","CD","CG","CK","CR","HR","CU","CW","CY","CZ","CI","DK","DJ","DM","DO","EC","EG","SV","GQ","ER","EE","SZ","ET","FK","FO","FJ","FI","FR","GF","PF","TF","GA","GM","GE","DE","GH","GI","GR","GL","GD","GP","GU","GT","GG","GN","GW","GY","HT","HM","VA","HN","HK","HU","IS","IN","ID","IR","IQ","IE","IM","IL","IT","JM","JP","JE","JO","KZ","KE","KI","KP","KR","KW","KG","LA","LV","LB","LS","LR","LY","LI","LT","LU","MO","MG","MW","MY","MV","ML","MT","MH","MQ","MR","MU","YT","MX","FM","MD","MC","MN","ME","MS","MA","MZ","MM","NA","NR","NP","NL","NC","NZ","NI","NE","NG","NU","NF","MK","MP","NO","OM","PK","PW","PS","PA","PG","PY","PE","PH","PN","PL","PT","PR","QA","RO","RU","RW","RE","BL","SH","KN","LC","MF","PM","VC","WS","SM","ST","SA","SN","RS","SC","SL","SG","SX","SK","SI","SB","SO","ZA","GS","SS","ES","LK","SD","SR","SJ","SE","CH","SY","TW","TJ","TZ","TH","TL","TG","TK","TO","TT","TN","TM","TC","TV","TR","UG","UA","AE","GB","UM","US","UY","UZ","VU","VE","VN","VG","VI","WF","EH","YE","ZM","ZW","AX","N/A"])]'
+          ) {
+            reject('Validation error does not match')
+          }
+
+          if (data.message !== 'Invalid request body') {
+            reject('Message does not match')
+          }
+        },
+      }
+    ),
+    executeHttpStep<Transaction, ValidationError>(
+      'Amount Provided as string instead of number',
+      '/transactions',
+      {
+        ...transactionPayload,
+        destinationAmountDetails: {
+          transactionAmount: '100' as any,
+          country: 'NL',
+          transactionCurrency: 'EUR',
+        },
+      },
+      {
+        statusCode: 400,
+        statusMessage: 'Bad Request',
+        dataCallback: (_, data, reject) => {
+          if (
+            data.validationErrors !==
+            '[instance type (string) does not match any allowed primitive type (allowed: ["integer","number"])]'
+          ) {
+            reject('Validation error does not match')
+          }
+
+          if (data.message !== 'Invalid request body') {
+            reject('Message does not match')
+          }
+        },
+      }
+    ),
+    executeHttpStep<Transaction, ValidationError>(
+      'Transaction id is provided as boolean instead of string',
+      '/transactions',
+      {
+        ...transactionPayload,
+        transactionId: true as any,
+      },
+      {
+        statusCode: 400,
+        statusMessage: 'Bad Request',
+        dataCallback: (_, data, reject) => {
+          if (
+            data.validationErrors !==
+            '[instance type (boolean) does not match any allowed primitive type (allowed: ["string"])]'
+          ) {
+            reject('Validation error does not match')
+          }
+
+          if (data.message !== 'Invalid request body') {
+            reject('Message does not match')
+          }
+        },
+      }
     ),
   ])
 
