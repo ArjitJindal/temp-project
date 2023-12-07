4bab49178d941cba8f281ff28aad35fbe5df5bd1
Rathin R <148132321+rath1nr@users.noreply.github.com>
2023-12-07 15:09:07 +0530

    FR-3771 Test Investigative Copilot Modal is opening (#2796)
    * FR-3771 Test Investigative Copilot Modal is opening
    
    * Changes made in an already existing test
    
    * Changes made in an already existing test

--- phytoplankton-console/cypress/e2e/copilot/copilot-test.cy.ts
+++ phytoplankton-console/cypress/e2e/copilot/copilot-test.cy.ts
@@ -1,3 +1,4 @@
+import promisify from 'cypress-promise';
 describe('Investigative Copilot', () => {
   beforeEach(() => {
     cy.loginByForm();
@@ -10,12 +11,18 @@ describe('Investigative Copilot', () => {
       force: true,
     });
 
+    // Check if the mode is opening
+    const text = await promisify(cy.get('.ant-modal-title').first().should('exist').invoke('text'));
+    cy.intercept('GET', '**/questions/**').as('query');
+    expect(text).to.equal('AI Forensics');
+
     // See if the natural language query works
     cy.get('[data-cy="investigation-input"]').type('Show me transactions for the last 180 days');
     cy.get('[data-cy="ask-ai-button"]').first().click();
     cy.get('.ant-modal-root table tr').then((tableRows) => {
       expect(tableRows.length).to.be.greaterThan(1);
     });
+    cy.wait('@query', { timeout: 15000 }).its('response.statusCode').should('be.oneOf', [200, 304]);
 
     // Check autocomplete works
     cy.get('[data-cy="investigation-input"]').type('Aler');
@@ -23,6 +30,7 @@ describe('Investigative Copilot', () => {
       expect(button).to.contain('Alerts');
       button.click();
     });
+    cy.wait('@query', { timeout: 15000 }).its('response.statusCode').should('be.oneOf', [200, 304]);
 
     // Go through each question type
     ['TRS score', 'Entity linking', 'User details', 'Transactions by rule action'].forEach(
@@ -31,11 +39,15 @@ describe('Investigative Copilot', () => {
         cy.get('[data-cy="investigation-suggestion-button"]').contains(text).click();
       },
     );
+    cy.wait('@query', { timeout: 15000 }).its('response.statusCode').should('be.oneOf', [200, 304]);
+    cy.get('.ant-modal-title').parents('.ant-modal-header').find('button').click();
 
     // Navigate back to the same alert
     cy.reload();
     cy.get('[data-cy="investigate-button"]').first().click({
       force: true,
     });
+
+    cy.wait('@query', { timeout: 15000 }).its('response.statusCode').should('be.oneOf', [200, 304]);
   });
 });
