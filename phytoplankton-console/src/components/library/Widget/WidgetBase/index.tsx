ArjitJindal/temp-project

commit 2e2ca5b5d99ef0d5f93f14a94976db0663478e57

Optimise and refactor dashboard user charts (#2802)
* Use affected time range when updating user stats

* Refactor transaction stats dashboard aggregation pipeline

* Refactor user stats dashboard aggregation

- Split into consumer user collection and business user collection
- Attribute counts are grouped by time-only (same as transaction-stats).
  A single record will look like
```
{
  "_id": "2023-04-20T14",
  "krsRiskLevel_MEDIUM": 1,
  "drsRiskLevel_MEDIUM": 1,
  "kycStatus_FAILED": 1
  "updatedAt": 1700833293222,
}
```
- Use the user stats collection for kyc status distribution as well
- Support time filter for kyc status distribution chart

* Refactor dashboard user distribution widgets

Used the shared `DistributionChartWidget` component for the following 3 widgets
- users distribution by risk levels
- users breakdown by risk levels
- users distribution by KYC status

to have consistent time filter and pdf download output.

* add migrations

* fix

* fix

* update

* fix test

* fix test

phytoplankton-console/src/components/library/Widget/WidgetBase/index.tsx
@@ -5,7 +5,13 @@ import s from './index.module.less';
 
 type Props = Pick<WidgetProps, 'children' | 'width'>;
 
-export default function WidgetBase(props: Props) {
+function WidgetBase(props: Props, ref: React.Ref<HTMLInputElement>) {
   const { children, width = 'FULL' } = props;
-  return <div className={cn(s.root, s[`width-${width}`])}>{children}</div>;
+  return (
+    <div ref={ref} className={cn(s.root, s[`width-${width}`])}>
+      {children}
+    </div>
+  );
 }
+
+export default React.forwardRef(WidgetBase);

phytoplankton-console/src/components/library/Widget/index.tsx
@@ -10,7 +10,7 @@ import DownloadAsPDF from '@/components/SanctionsTable/SearchResultDetailsModal/
 
 const DEFAULT_FIXED_HEIGHT = 400;
 
-export default function Widget(props: WidgetProps) {
+function Widget(props: WidgetProps, ref: React.Ref<HTMLInputElement>) {
   const { title, extraControls, onDownload, children, width, resizing = 'AUTO' } = props;
   const controls = [
     ...(extraControls ?? []),
@@ -18,7 +18,7 @@ export default function Widget(props: WidgetProps) {
   ];
 
   return (
-    <WidgetBase width={width}>
+    <WidgetBase ref={ref} width={width}>
       <div
         className={cn(s.root, resizing === 'FIXED' && s.fixedHeight)}
         style={{
@@ -48,6 +48,8 @@ export default function Widget(props: WidgetProps) {
     </WidgetBase>
   );
 }
+
+export default React.forwardRef(Widget);
 
 export function DownloadButton(props: {
   onDownload: () => Promise<{

phytoplankton-console/src/pages/dashboard/analysis/index.tsx
@@ -1,10 +1,14 @@
 import { useMemo, useState } from 'react';
 import { useLocalStorageState } from 'ahooks';
+import { kebabCase } from 'lodash';
 import TransactionsChartWidget from './components/TransactionsChartWidget';
 import PaymentMethodDistributionWidget from './components/Transactions/PaymentMethodDistributionWidget';
 import RuleHitCard from './components/RulesHitCard';
 import TopUsersHitCard from './components/TopUsersHitCard';
-import RiskLevelDistributionCard from './components/RiskLevelDistributionCard';
+import {
+  RiskLevelDistributionCard,
+  RiskLevelBreakdownCard,
+} from './components/RiskLevelDistributionCard';
 import TeamPerformanceCard from './components/TeamPerformanceCard';
 import OverviewCard from './components/OverviewCard';
 import RulePrioritySplitCard from './components/RulePrioritySplitCard';
@@ -29,7 +33,7 @@ import WidgetGrid from '@/components/library/WidgetGrid';
 import Widget from '@/components/library/Widget';
 import Label from '@/components/library/Label';
 import { notEmpty } from '@/utils/array';
-import RiskLevelBreakdownWidget from '@/pages/dashboard/analysis/components/RiskLevelBreakdownWidget';
+import { WidgetProps } from '@/components/library/Widget/types';
 
 type KeyValues =
   | 'OVERVIEW'
@@ -105,10 +109,6 @@ interface Widgets {
   TEAM_MANAGEMENT: WidgetGroup;
 }
 
-interface WidgetProps {
-  id: string;
-}
-
 const WIDGETS: Widgets = {
   OVERVIEW: [
     {
@@ -122,8 +122,8 @@ const WIDGETS: Widgets = {
     {
       groupTitle: 'Consumer users',
       id: 'CONSUMER_USERS_DISTRIBUTION_BY_RISK_LEVEL',
-      title: 'Consumer users distribution by risk levels',
-      component: RiskLevelBreakdownWidget,
+      title: 'Users distribution by risk levels',
+      component: RiskLevelBreakdownCard,
       width: 'FULL' as const,
       userType: 'CONSUMER',
       requiredFeatures: ['RISK_SCORING'],
@@ -131,7 +131,7 @@ const WIDGETS: Widgets = {
     {
       groupTitle: 'Consumer users',
       id: 'CONSUMER_USERS_BREAKDOWN_BY_RISK_LEVEL',
-      title: 'Consumer users breakdown by risk levels',
+      title: 'Users breakdown by risk levels',
       component: RiskLevelDistributionCard,
       width: 'HALF' as const,
       userType: 'CONSUMER',
@@ -141,7 +141,7 @@ const WIDGETS: Widgets = {
     {
       groupTitle: 'Consumer users',
       id: 'TOP_CONSUMER_USERS_BY_RULE_HITS',
-      title: 'Top consumer users by rule hits',
+      title: 'Top users by rule hits',
       component: TopUsersHitCard,
       width: 'HALF' as const,
       userType: 'CONSUMER',
@@ -149,7 +149,7 @@ const WIDGETS: Widgets = {
     {
       groupTitle: 'Consumer users',
       id: 'CONSUMER_USERS_DISTRIBUTION_BY_KYC_STATUS',
-      title: 'Consumer users distribution by KYC status',
+      title: 'Users distribution by KYC status',
       component: KYCStatusDistributionCard,
       width: 'HALF' as const,
       userType: 'CONSUMER',
@@ -157,7 +157,7 @@ const WIDGETS: Widgets = {
     {
       groupTitle: 'Consumer users',
       id: 'CONSUMER_USERS_DISTRIBUTION_BY_USER_STATUS',
-      title: 'Consumer users distribution by user status',
+      title: 'Users distribution by user status',
       component: UserStatusDistributionCard,
       width: 'HALF' as const,
       userType: 'CONSUMER',
@@ -167,8 +167,8 @@ const WIDGETS: Widgets = {
     {
       groupTitle: 'Business users',
       id: 'BUSINESS_USERS_DISTRIBUTION_BY_RISK_LEVEL',
-      title: 'Business users distribution by risk levels',
-      component: RiskLevelBreakdownWidget,
+      title: 'Users distribution by risk levels',
+      component: RiskLevelBreakdownCard,
       width: 'FULL' as const,
       userType: 'BUSINESS' as const,
       requiredFeatures: ['RISK_SCORING'],
@@ -176,7 +176,7 @@ const WIDGETS: Widgets = {
     {
       groupTitle: 'Business users',
       id: 'BUSINESS_USERS_BREAKDOWN_BY_RISK_LEVEL',
-      title: 'Business users breakdown by risk levels',
+      title: 'Users breakdown by risk levels',
       component: RiskLevelDistributionCard,
       width: 'HALF' as const,
       userType: 'BUSINESS',
@@ -185,7 +185,7 @@ const WIDGETS: Widgets = {
     {
       groupTitle: 'Business users',
       id: 'TOP_BUSINESS_USERS_BY_RULE_HITS',
-      title: 'Top business users by rule hits',
+      title: 'Top users by rule hits',
       component: TopUsersHitCard,
       width: 'HALF' as const,
       userType: 'BUSINESS',
@@ -193,7 +193,7 @@ const WIDGETS: Widgets = {
     {
       groupTitle: 'Business users',
       id: 'BUSINESS_USERS_DISTRIBUTION_BY_KYC_STATUS',
-      title: 'Business users distribution by KYC status',
+      title: 'Users distribution by KYC status',
       component: KYCStatusDistributionCard,
       width: 'HALF' as const,
       userType: 'BUSINESS',
@@ -201,7 +201,7 @@ const WIDGETS: Widgets = {
     {
       groupTitle: 'Business users',
       id: 'BUSINESS_USERS_DISTRIBUTION_BY_USER_STATUS',
-      title: 'Business users distribution by user status',
+      title: 'Users distribution by user status',
       component: UserStatusDistributionCard,
       width: 'HALF' as const,
       userType: 'BUSINESS',
@@ -243,7 +243,7 @@ const WIDGETS: Widgets = {
       groupTitle: 'Rules',
       id: 'TOP_RULE_HITS_BY_COUNT',
       title: 'Top rule hits by count',
-      component: Widget,
+      component: Widget as (props) => JSX.Element,
       children: <RuleHitCard />,
       resizing: 'AUTO' as const,
     },
@@ -327,6 +327,7 @@ function Analysis() {
       .map((widget) => {
         const widgetProps: WidgetProps = {
           ...widget,
+          downloadFilenamePrefix: kebabCase(`${widget.userType ?? ''} ${widget.title}`.trim()),
         };
 
         return {

phytoplankton-console/src/pages/dashboard/analysis/components/KYCStatusDistributionCard/index.tsx
@@ -1,12 +1,6 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-import { MutableRefObject, useRef } from 'react';
-import Column, { ColumnData } from '../charts/Column';
-import { exportDataForBarGraphs } from '../../utils/export-data-build-util';
-import { useApi } from '@/api';
-import { useQuery } from '@/utils/queries/hooks';
-import { USERS_KYC_STATUS_STATS } from '@/utils/queries/keys';
-import AsyncResourceRenderer from '@/components/common/AsyncResourceRenderer';
-import NoData from '@/pages/case-management-item/CaseDetails/InsightsCard/components/NoData';
+import { useState } from 'react';
+import DistributionChartWidget from '../widgets/DistributionChartWidget';
 import {
   COLORS_V2_ANALYTICS_CHARTS_07,
   COLORS_V2_ANALYTICS_CHARTS_03,
@@ -14,10 +8,12 @@ import {
   COLORS_V2_ANALYTICS_CHARTS_02,
   COLORS_V2_ANALYTICS_CHARTS_05,
 } from '@/components/ui/colors';
-import { KYCStatus } from '@/apis';
-import { KYC_STATUSES as definedOrder } from '@/utils/api/users';
-import Widget from '@/components/library/Widget';
 import { WidgetProps } from '@/components/library/Widget/types';
+import { useQuery } from '@/utils/queries/hooks';
+import { USERS_STATS } from '@/utils/queries/keys';
+import { useApi } from '@/api';
+import { DashboardStatsUsersStats, KYCStatus } from '@/apis';
+import { KYC_STATUSES } from '@/utils/api/users';
 import { dayjs } from '@/utils/dayjs';
 
 const KYC_STATUS_COLORS = {
@@ -33,80 +29,32 @@ interface Props extends WidgetProps {
 }
 
 export default function KYCStatusDistributionCard(props: Props) {
-  const { userType = 'CONSUMER' } = props;
-  const api = useApi();
-  const queryResult = useQuery(USERS_KYC_STATUS_STATS(userType), async () => {
-    const response = await api.getDashboardStatsKycStatusDistribution({ userType });
-    const lookUp: Record<string, { kycStatus: KYCStatus; count: number; percentage: number }> = {};
-    response.data.forEach((item) => {
-      const percentage =
-        item?.percentage === undefined ? '0' : parseFloat(item?.percentage).toFixed(2);
-      const obj = {
-        kycStatus: item?.kycStatus,
-        count: item?.count,
-        percentage: parseFloat(percentage),
-      };
-      lookUp[item?.kycStatus] = obj;
-    });
-    return {
-      total: response.total,
-      items: definedOrder.map((val) => lookUp[val]),
-    };
+  const { userType, ...restProps } = props;
+  const [timeRange, setTimeRange] = useState({
+    startTimestamp: dayjs().subtract(1, 'year').valueOf(),
+    endTimestamp: dayjs().valueOf(),
   });
-  const pdfRef = useRef() as MutableRefObject<HTMLInputElement>;
+  const params = {
+    userType: userType ?? 'CONSUMER',
+    startTimestamp: timeRange.startTimestamp,
+    endTimestamp: timeRange.endTimestamp,
+  };
+  const api = useApi();
+  const queryResult = useQuery(USERS_STATS(params), async () => {
+    return await api.getDashboardStatsUsersByTime(params);
+  });
   return (
-    <AsyncResourceRenderer resource={queryResult.data}>
-      {(response) => {
-        if (response.total === 0) {
-          return <NoData />;
-        }
-        const data: ColumnData<string, number, string> = response.items.map((item) => {
-          return {
-            xValue: item?.kycStatus,
-            yValue: item?.percentage ?? 0,
-            series: item?.kycStatus,
-          };
-        });
-        let COLORS = {};
-        response.items.map((item) => {
-          COLORS = {
-            ...COLORS,
-            [item?.kycStatus]: KYC_STATUS_COLORS[item?.kycStatus] as string,
-          };
-        });
-        return (
-          <div ref={pdfRef}>
-            <Widget
-              resizing="AUTO"
-              {...props}
-              onDownload={(): Promise<{
-                fileName: string;
-                data: string;
-                pdfRef: MutableRefObject<HTMLInputElement>;
-              }> => {
-                return new Promise((resolve, _reject) => {
-                  const fileData = {
-                    fileName: `${userType.toLowerCase()}-users-distribution-by-kyc-status-${dayjs().format(
-                      'YYYY_MM_DD',
-                    )}`,
-                    data: exportDataForBarGraphs(data, 'KYC status', 'Percentage'),
-                    pdfRef,
-                  };
-                  resolve(fileData);
-                });
-              }}
-            >
-              <Column
-                data={data}
-                colors={COLORS}
-                height={400}
-                hideLegend={true}
-                rotateLabel={false}
-              />
-            </Widget>
-          </div>
-        );
-      }}
-    </AsyncResourceRenderer>
+    <DistributionChartWidget<DashboardStatsUsersStats, KYCStatus>
+      groups={[
+        { name: 'kycStatus', attributeName: 'KYC status', attributeDataPrefix: 'kycStatus' },
+      ]}
+      groupBy="VALUE"
+      valueColors={KYC_STATUS_COLORS}
+      values={KYC_STATUSES}
+      queryResult={queryResult}
+      timeRange={timeRange}
+      onTimeRangeChange={setTimeRange}
+      {...restProps}
+    />
   );
 }

phytoplankton-console/src/pages/dashboard/analysis/components/RiskLevelBreakdownWidget/index.tsx
@@ -1,141 +1,0 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-import React, { useState } from 'react';
-import { RangeValue } from 'rc-picker/es/interface';
-import { useLocalStorageState } from 'ahooks';
-import Column, { ColumnData } from '../charts/Column';
-import s from './styles.module.less';
-import RiskTypeSelector, { RiskTypeSelectorValue } from './RiskTypeSelector';
-import { useApi } from '@/api';
-import { RISK_LEVELS } from '@/utils/risk-levels';
-import { useQuery } from '@/utils/queries/hooks';
-import { USERS_STATS } from '@/utils/queries/keys';
-import AsyncResourceRenderer from '@/components/common/AsyncResourceRenderer';
-import NoData from '@/pages/case-management-item/CaseDetails/InsightsCard/components/NoData';
-import { getRiskLevelLabel, useSettings } from '@/components/AppWrapper/Providers/SettingsProvider';
-import { map } from '@/utils/asyncResource';
-import { RiskLevel } from '@/apis';
-import {
-  COLORS_V2_PRIMARY_SHADES_BLUE_100,
-  COLORS_V2_PRIMARY_SHADES_BLUE_300,
-  COLORS_V2_PRIMARY_SHADES_BLUE_50,
-  COLORS_V2_PRIMARY_SHADES_BLUE_600,
-  COLORS_V2_PRIMARY_TINTS_BLUE_900,
-} from '@/components/ui/colors';
-import { Dayjs, dayjs } from '@/utils/dayjs';
-import Widget from '@/components/library/Widget';
-import { WidgetProps } from '@/components/library/Widget/types';
-import DatePicker from '@/components/ui/DatePicker';
-import ContainerRectMeasure from '@/components/utils/ContainerRectMeasure';
-import { formatDate } from '@/pages/dashboard/analysis/utils/date-utils';
-
-const DEFAULT_DATE_RANGE: [Dayjs, Dayjs] = [dayjs().subtract(1, 'year'), dayjs()];
-
-const RISK_LEVEL_COLORS = {
-  VERY_LOW: COLORS_V2_PRIMARY_SHADES_BLUE_50,
-  LOW: COLORS_V2_PRIMARY_SHADES_BLUE_100,
-  MEDIUM: COLORS_V2_PRIMARY_SHADES_BLUE_300,
-  HIGH: COLORS_V2_PRIMARY_SHADES_BLUE_600,
-  VERY_HIGH: COLORS_V2_PRIMARY_TINTS_BLUE_900,
-};
-
-interface Props extends WidgetProps {
-  userType?: 'BUSINESS' | 'CONSUMER';
-}
-
-export default function RiskLevelBreakdownWidget(props: Props) {
-  const { userType = 'CONSUMER', ...restProps } = props;
-  const api = useApi();
-  const settings = useSettings();
-  const [selectedRiskType, setSelectedRiskType] = useLocalStorageState<RiskTypeSelectorValue>(
-    `dashboard-${userType}-risk-type-active-tab`,
-    'CRA',
-  );
-
-  const [dateRange, setDateRange] = useState<RangeValue<Dayjs>>([
-    dayjs().subtract(1, 'year'),
-    dayjs(),
-  ]);
-
-  let startTimestamp = dayjs().subtract(1, 'year').valueOf();
-  let endTimestamp = Date.now();
-  const [start, end] = dateRange ?? [];
-  if (start != null && end != null) {
-    startTimestamp = start.startOf('day').valueOf();
-    endTimestamp = end.endOf('day').valueOf();
-  }
-
-  const params = {
-    userType: userType,
-    startTimestamp,
-    endTimestamp,
-  };
-  const queryResult = useQuery(USERS_STATS(params), async () => {
-    const response = await api.getDashboardStatsUsersByTime(params);
-    return response;
-  });
-
-  const preparedDataRes = map(queryResult.data, (data): ColumnData<string, number, RiskLevel> => {
-    return data.flatMap((dataItem): ColumnData<string, number, RiskLevel> => {
-      return RISK_LEVELS.map((riskLevel) => {
-        return {
-          xValue: dataItem._id ?? '-',
-          yValue:
-            dataItem[
-              selectedRiskType === 'KRS' ? `krsRiskLevel_${riskLevel}` : `drsRiskLevel_${riskLevel}`
-            ] ?? 0,
-          series: riskLevel,
-        };
-      });
-    });
-  });
-
-  return (
-    <Widget
-      resizing="FIXED"
-      {...restProps}
-      extraControls={[
-        <DatePicker.RangePicker
-          value={dateRange}
-          onChange={(e) => {
-            setDateRange(e ?? DEFAULT_DATE_RANGE);
-          }}
-        />,
-      ]}
-    >
-      <AsyncResourceRenderer resource={preparedDataRes}>
-        {(data) => {
-          if (data.length === 0) {
-            return <NoData />;
-          }
-          return (
-            <div className={s.root}>
-              <RiskTypeSelector
-                selectedSection={selectedRiskType}
-                setSelectedSection={setSelectedRiskType}
-              />
-              <div className={s.chartContainer}>
-                <ContainerRectMeasure className={s.chartContainer2}>
-                  {(size) => (
-                    <Column<RiskLevel, string>
-                      data={data}
-                      colors={RISK_LEVEL_COLORS}
-                      rotateLabel={false}
-                      hideLegend={false}
-                      height={size.height}
-                      formatSeries={(series) => {
-                        return getRiskLevelLabel(series, settings);
-                      }}
-                      formatX={(xValue) => {
-                        return formatDate(xValue);
-                      }}
-                    />
-                  )}
-                </ContainerRectMeasure>
-              </div>
-            </div>
-          );
-        }}
-      </AsyncResourceRenderer>
-    </Widget>
-  );
-}

commit 9ad42152eff07c70565e6213f9c16c30c66f5314

Fix dashboard widget cypress tests (#2849)
broken by 2e2ca5b.

phytoplankton-console/src/components/library/Widget/WidgetBase/index.tsx
@@ -3,12 +3,12 @@ import React from 'react';
 import { WidgetProps } from '../types';
 import s from './index.module.less';
 
-type Props = Pick<WidgetProps, 'children' | 'width'>;
+type Props = Pick<WidgetProps, 'children' | 'width' | 'id'>;
 
 function WidgetBase(props: Props, ref: React.Ref<HTMLInputElement>) {
-  const { children, width = 'FULL' } = props;
+  const { children, width = 'FULL', id } = props;
   return (
-    <div ref={ref} className={cn(s.root, s[`width-${width}`])}>
+    <div ref={ref} className={cn(s.root, s[`width-${width}`])} data-cy={id}>
       {children}
     </div>
   );

phytoplankton-console/src/components/library/Widget/index.tsx
@@ -11,20 +11,19 @@ import DownloadAsPDF from '@/components/SanctionsTable/SearchResultDetailsModal/
 const DEFAULT_FIXED_HEIGHT = 400;
 
 function Widget(props: WidgetProps, ref: React.Ref<HTMLInputElement>) {
-  const { title, extraControls, onDownload, children, width, resizing = 'AUTO' } = props;
+  const { id, title, extraControls, onDownload, children, width, resizing = 'AUTO' } = props;
   const controls = [
     ...(extraControls ?? []),
     ...(onDownload ? [<DownloadButton onDownload={onDownload} />] : []),
   ];
 
   return (
-    <WidgetBase ref={ref} width={width}>
+    <WidgetBase ref={ref} width={width} id={id}>
       <div
         className={cn(s.root, resizing === 'FIXED' && s.fixedHeight)}
         style={{
           minHeight: resizing === 'AUTO' ? undefined : DEFAULT_FIXED_HEIGHT,
         }}
-        data-cy={title}
       >
         <div className={s.header}>
           {title && <div className={s.title}>{title}</div>}

phytoplankton-console/src/pages/dashboard/analysis/index.tsx
@@ -416,7 +416,7 @@ function Analysis() {
                         });
                       }}
                       extraLeftLabelMargin
-                      testName={widget.title}
+                      testName={widget.id}
                     />
                   </Label>
                 );

phytoplankton-console/cypress/e2e/dashboard/widget-selector-load-test.cy.ts
@@ -6,28 +6,28 @@ describe('Dashboard Integration Test', () => {
   it('should configure the dashboard with selected widgets', async () => {
     cy.visit('/');
     const WIDGETS = {
-      Overview: 'dashboard_stats/overview',
-      'Consumer users distribution by risk levels': 'dashboard_stats/users/**',
-      'Consumer users breakdown by risk levels': 'dashboard_stats/users/**',
-      'Top consumer users by rule hits': 'dashboard_stats/users/**',
-      'Consumer users distribution by KYC status': 'dashboard_stats/hits_per_user**',
-      'Consumer users distribution by user status': 'dashboard_stats/kyc-status-distribution/**',
-      'Business users distribution by risk levels': 'consumer/**',
-      'Business users breakdown by risk levels': 'dashboard_stats/users/**',
-      'Top business users by rule hits': 'dashboard_stats/hits_per_user**',
-      'Business users distribution by KYC status': 'dashboard_stats/kyc-status-distribution**',
-      'Business users distribution by user status': 'business/**',
-      'Transactions breakdown by rule action': 'dashboard_stats/transactions/**',
-      'Distribution by payment method': 'dashboard_stats/transactions/**',
-      'Distribution by transaction type': 'dashboard_stats/transactions/**',
-      'Transactions breakdown by TRS': 'dashboard_stats/transactions/**',
-      'Top rule hits by count': 'dashboard_stats/rule_hit**',
-      'Distribution by rule priority': 'rule_instances',
-      'Distribution by rule action': 'rule_instances',
-      'Distribution by closing reason': 'dashboard_stats/closing_reason_distribution**',
-      'Distribution by open alert priority': 'dashboard_stats/alert_priority_distribution**',
-      'Distribution by status': 'dashboard_stats/alert_and_case_status_distribution**',
-      'Team overview': 'dashboard_stats/team**',
+      OVERVIEW: 'dashboard_stats/overview',
+      CONSUMER_USERS_DISTRIBUTION_BY_RISK_LEVEL: 'dashboard_stats/users/**',
+      CONSUMER_USERS_BREAKDOWN_BY_RISK_LEVEL: 'dashboard_stats/users/**',
+      TOP_CONSUMER_USERS_BY_RULE_HITS: 'dashboard_stats/users/**',
+      CONSUMER_USERS_DISTRIBUTION_BY_KYC_STATUS: 'dashboard_stats/hits_per_user**',
+      CONSUMER_USERS_DISTRIBUTION_BY_USER_STATUS: 'dashboard_stats/kyc-status-distribution/**',
+      BUSINESS_USERS_DISTRIBUTION_BY_RISK_LEVEL: 'consumer/**',
+      BUSINESS_USERS_BREAKDOWN_BY_RISK_LEVEL: 'dashboard_stats/users/**',
+      TOP_BUSINESS_USERS_BY_RULE_HITS: 'dashboard_stats/hits_per_user**',
+      BUSINESS_USERS_DISTRIBUTION_BY_KYC_STATUS: 'dashboard_stats/kyc-status-distribution**',
+      BUSINESS_USERS_DISTRIBUTION_BY_USER_STATUS: 'business/**',
+      TRANSACTIONS_BREAKDOWN_BY_RULE_ACTION: 'dashboard_stats/transactions/**',
+      DISTRIBUTION_BY_PAYMENT_METHOD: 'dashboard_stats/transactions/**',
+      DISTRIBUTION_BY_TRANSACTION_TYPE: 'dashboard_stats/transactions/**',
+      TRANSACTIONS_BREAKDOWN_BY_TRS: 'dashboard_stats/transactions/**',
+      TOP_RULE_HITS_BY_COUNT: 'dashboard_stats/rule_hit**',
+      DISTRIBUTION_BY_RULE_PRIORITY: 'rule_instances',
+      DISTRIBUTION_BY_RULE_ACTION: 'rule_instances',
+      DISTRIBUTION_BY_CLOSING_REASON: 'dashboard_stats/closing_reason_distribution**',
+      DISTRIBUTION_BY_ALERT_PRIORITY: 'dashboard_stats/alert_priority_distribution**',
+      DISTRIBUTION_BY_CASE_AND_ALERT_STATUS: 'dashboard_stats/alert_and_case_status_distribution**',
+      TEAM_OVERVIEW: 'dashboard_stats/team**',
     };
 
     const endpoints = [

phytoplankton-console/cypress/e2e/dashboard/widget-selector-test.cy.ts
@@ -5,27 +5,28 @@ describe('Check if the widget selector works as expected', () => {
 
   it('should check the visibility of selected widgets', () => {
     const WIDGETS = [
-      'Consumer users distribution by risk levels',
-      'Consumer users breakdown by risk levels',
-      'Top consumer users by rule hits',
-      'Consumer users distribution by KYC status',
-      'Consumer users distribution by user status',
-      'Business users distribution by risk levels',
-      'Business users breakdown by risk levels',
-      'Top business users by rule hits',
-      'Business users distribution by KYC status',
-      'Business users distribution by user status',
-      'Transactions breakdown by rule action',
-      'Distribution by payment method',
-      'Distribution by transaction type',
-      'Transactions breakdown by TRS',
-      'Top rule hits by count',
-      'Distribution by rule priority',
-      'Distribution by rule action',
-      'Distribution by closing reason',
-      'Distribution by open alert priority',
-      'Distribution by status',
-      'Team overview',
+      'OVERVIEW',
+      'CONSUMER_USERS_DISTRIBUTION_BY_RISK_LEVEL',
+      'CONSUMER_USERS_BREAKDOWN_BY_RISK_LEVEL',
+      'TOP_CONSUMER_USERS_BY_RULE_HITS',
+      'CONSUMER_USERS_DISTRIBUTION_BY_KYC_STATUS',
+      'CONSUMER_USERS_DISTRIBUTION_BY_USER_STATUS',
+      'BUSINESS_USERS_DISTRIBUTION_BY_RISK_LEVEL',
+      'BUSINESS_USERS_BREAKDOWN_BY_RISK_LEVEL',
+      'TOP_BUSINESS_USERS_BY_RULE_HITS',
+      'BUSINESS_USERS_DISTRIBUTION_BY_KYC_STATUS',
+      'BUSINESS_USERS_DISTRIBUTION_BY_USER_STATUS',
+      'TRANSACTIONS_BREAKDOWN_BY_RULE_ACTION',
+      'DISTRIBUTION_BY_PAYMENT_METHOD',
+      'DISTRIBUTION_BY_TRANSACTION_TYPE',
+      'TRANSACTIONS_BREAKDOWN_BY_TRS',
+      'TOP_RULE_HITS_BY_COUNT',
+      'DISTRIBUTION_BY_RULE_PRIORITY',
+      'DISTRIBUTION_BY_RULE_ACTION',
+      'DISTRIBUTION_BY_CLOSING_REASON',
+      'DISTRIBUTION_BY_ALERT_PRIORITY',
+      'DISTRIBUTION_BY_CASE_AND_ALERT_STATUS',
+      'TEAM_OVERVIEW',
     ];
 
     cy.visit('/');