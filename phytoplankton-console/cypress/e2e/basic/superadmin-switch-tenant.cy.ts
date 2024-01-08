e3189c2e92dfb04eba1da57a7479c2670e0e248d
Jayant Patil <72199264+patiljayant@users.noreply.github.com>
2024-01-08 09:23:17 +0530

    improved superadin switch e2e test (#2995)

--- phytoplankton-console/cypress/e2e/basic/superadmin-switch-tenant.cy.ts
+++ phytoplankton-console/cypress/e2e/basic/superadmin-switch-tenant.cy.ts
@@ -12,7 +12,12 @@ describe('Switching from one tenant to another', () => {
 
     // Switch to 'FlagrightPostman'
     cy.get("button[data-cy='superadmin-panel-button']").click({ force: true });
-    cy.get('.ant-modal .ant-select').first().should('be.visible').click();
+    cy.get('.ant-modal .ant-select')
+      .first()
+      .contains('Cypress Tenant')
+      .should('be.visible')
+      .should('not.be.disabled')
+      .click();
     cy.get(`div[data-cy='FlagrightPostman']`).last().click();
 
     // Wait for tenant switch
@@ -23,7 +28,12 @@ describe('Switching from one tenant to another', () => {
 
     // Switch to 'Cypress Tenant'
     cy.get("button[data-cy='superadmin-panel-button']").click();
-    cy.get('.ant-modal .ant-select').first().should('be.visible').click();
+    cy.get('.ant-modal .ant-select')
+      .first()
+      .contains('FlagrightPostman')
+      .should('be.visible')
+      .should('not.be.disabled')
+      .click();
     cy.get(`div[data-cy='Cypress Tenant']`).last().click();
 
     // Wait for tenant switch
