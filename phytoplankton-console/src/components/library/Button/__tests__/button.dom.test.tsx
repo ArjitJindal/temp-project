56ad32ffeae2385b68527ec9ca629acbfcab14ce
Jayant Patil <72199264+patiljayant@users.noreply.github.com>
2024-01-09 11:14:01 +0530

    DOM tests for buttons (#2994)
    Co-authored-by: Madhu G Nadig <madhug.nadig@gmail.com>

---
+++ phytoplankton-console/src/components/library/Button/__tests__/button.dom.test.tsx
@@ -0,0 +1,62 @@
+import { test, describe, expect } from '@jest/globals';
+import { render, screen, userEvent } from 'testing-library-wrapper';
+import Button, { ButtonSize, ButtonType } from '..';
+
+const BUTTON_TYPES: ButtonType[] = ['PRIMARY', 'SECONDARY', 'TETRIARY', 'TEXT'];
+const BUTTON_SIZES: ButtonSize[] = ['SMALL', 'MEDIUM', 'LARGE'];
+
+describe('Button Component', () => {
+  test.each(BUTTON_TYPES)('renders correctly for type %s', (type) => {
+    render(<Button type={type}>Test</Button>);
+    const button = getInput('Test');
+    const classNames = button?.className.split(/\s+/);
+    const hasTypeClass = classNames?.some((className) => className.includes(`type-${type}`));
+    expect(hasTypeClass).toBe(true);
+  });
+
+  test.each(BUTTON_SIZES)('renders correctly for size %s', (size) => {
+    render(<Button size={size}>Test</Button>);
+    const button = getInput('Test');
+    const classNames = button?.className.split(/\s+/);
+    const hasSizeClass = classNames?.some((className) => className.includes(`size-${size}`));
+    expect(hasSizeClass).toBe(true);
+  });
+
+  test('handles click events', async () => {
+    const handleClick = jest.fn();
+    render(<Button onClick={handleClick}>Click Me</Button>);
+    const button = getInput('Click Me');
+    await userEvent.click(button);
+    expect(handleClick).toHaveBeenCalledTimes(1);
+  });
+
+  test('is disabled when isDisabled is true', () => {
+    render(<Button isDisabled={true}>Disabled</Button>);
+    const button = getInput('Disabled');
+    expect(button).toBeDisabled();
+  });
+
+  test.each(BUTTON_TYPES)('renders correctly for danger type %s', (type) => {
+    render(
+      <Button type={type} isDanger={true}>
+        Test
+      </Button>,
+    );
+    const button = getInput('Test');
+    const classNames = button?.className.split(/\s+/);
+    const hasDangerClass = classNames?.some((className) =>
+      className.includes(`type-${type}-danger`),
+    );
+    expect(hasDangerClass).toBe(true);
+  });
+
+  test('is disabled when isLoading is true', () => {
+    render(<Button isLoading={true}>Loading</Button>);
+    const button = getInput('Loading');
+    expect(button).toBeDisabled();
+  });
+});
+
+function getInput(text: string) {
+  return screen.getByText(text);
+}
