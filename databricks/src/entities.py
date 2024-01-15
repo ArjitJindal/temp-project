87c6d1033caea909e07ad470866029b6e1728003
Tim Coulson <tim@flagright.com>
2024-01-15 16:35:40 +0100

    Merge user and business schemas (#3053)

--- databricks/src/entities.py
+++ databricks/src/entities.py
@@ -2,6 +2,7 @@
 import json
 import os
 import sys
+from pyspark.sql.types import StructType
 
 sys.path.append(os.path.abspath("/Workspace/Shared/main/src"))
 
@@ -9,6 +10,10 @@ from openapi_client.models.transaction import (  # pylint: disable=import-error
     Transaction,
 )
 from openapi_client.models.user import User  # pylint: disable=import-error
+from openapi_client.models.business import Business  # pylint: disable=import-error
+
+def merge_schemas(*schemas):
+    StructType(list({obj.name: obj for l in schemas for obj in l}.values()))
 
 entities = [
     {
@@ -19,7 +24,7 @@ entities = [
     },
     {
         "table": "users",
-        "schema": User,
+        "schema": merge_schemas(User, Business),
         "partition_key": "user#primary",
         "id_column": "userId",
     },
