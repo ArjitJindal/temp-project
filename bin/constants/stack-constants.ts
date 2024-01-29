ab40366ef36ff2bc2837651aced42ef756ec3d08
Jayant Patil <72199264+patiljayant@users.noreply.github.com>
2024-01-29 11:28:39 +0530

    Updated ARN for pipeline (#3151)
    updated ARN for pipeling

--- bin/constants/stack-constants.ts
+++ bin/constants/stack-constants.ts
@@ -2,9 +2,9 @@ export const STACK_CONSTANTS = {
   SENTRY_AUTH_TOKEN:
     'arn:aws:secretsmanager:eu-central-1:073830519512:secret:sentryCreds-NQB0S7:authToken',
   CYPRESS_SUPER_ADMIN:
-    'arn:aws:secretsmanager:eu-central-1:073830519512:secret:rbacCypressCreds-YAe7eJ:super_admin',
+    'arn:aws:secretsmanager:eu-central-1:073830519512:secret:rbacCypressCreds-yWaetJ:super_admin',
   CYPRESS_CUSTOM_ROLE:
-    'arn:aws:secretsmanager:eu-central-1:073830519512:secret:rbacCypressCreds-YAe7eJ:custom_role',
+    'arn:aws:secretsmanager:eu-central-1:073830519512:secret:rbacCypressCreds-yWaetJ:custom_role',
   CYPRESS_ADMIN:
-    'arn:aws:secretsmanager:eu-central-1:073830519512:secret:rbacCypressCreds-YAe7eJ:admin',
+    'arn:aws:secretsmanager:eu-central-1:073830519512:secret:rbacCypressCreds-yWaetJ:admin',
 }
