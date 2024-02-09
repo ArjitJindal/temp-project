9f694797746c710ec10801991d6be842c01c066f
Jayant Patil <72199264+patiljayant@users.noreply.github.com>
2024-02-09 12:26:53 +0530

    V8: Conditions are cleared when pressing Enter in new variable form (#3240)
    * preserve conditions on enter
    
    * moved logic to child

--- phytoplankton-console/src/components/ui/LogicBuilder/index.tsx
+++ phytoplankton-console/src/components/ui/LogicBuilder/index.tsx
@@ -1,6 +1,6 @@
 import { Query, Builder, BuilderProps, Config } from '@react-awesome-query-builder/ui';
 import '@react-awesome-query-builder/ui/css/styles.css';
-import React from 'react';
+import React, { useEffect } from 'react';
 import cn from 'clsx';
 import { Operators } from '@react-awesome-query-builder/core';
 import s from './index.module.less';
@@ -22,6 +22,19 @@ export interface Props {
 
 export default function LogicBuilder(props: Props) {
   const { value, onChange, config, hideConjunctions = false } = props;
+
+  useEffect(() => {
+    const handleKeyPress = (event) => {
+      if (event.key === 'Enter') {
+        event.preventDefault();
+      }
+    };
+    window.addEventListener('keydown', handleKeyPress);
+    return () => {
+      window.removeEventListener('keydown', handleKeyPress);
+    };
+  }, []);
+
   return (
     <div className={cn(s.root, hideConjunctions && s.hideConjunctions)}>
       <Query {...config} value={value} onChange={onChange} renderBuilder={renderBuilder} />
