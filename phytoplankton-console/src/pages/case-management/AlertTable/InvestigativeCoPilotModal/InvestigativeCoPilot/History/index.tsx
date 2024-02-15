bc3e8c54e987c180948e900d3ffa4ef4490813a9
Jayant Patil <72199264+patiljayant@users.noreply.github.com>
2024-02-15 12:02:58 +0530

    Use SVG empty image in Forensics (#3277)
    used svg instead of png

--- phytoplankton-console/src/pages/case-management/AlertTable/InvestigativeCoPilotModal/InvestigativeCoPilot/History/index.tsx
+++ phytoplankton-console/src/pages/case-management/AlertTable/InvestigativeCoPilotModal/InvestigativeCoPilot/History/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { QuestionResponse } from '../types';
 import s from './index.module.less';
-import EmptyImg from './empty.png';
+import EmptyIcon from './empty.react.svg';
 import HistoryItem from '@/pages/case-management/AlertTable/InvestigativeCoPilotModal/InvestigativeCoPilot/History/HistoryItem';
 
 interface Props {
@@ -15,7 +15,8 @@ export default function History(props: Props) {
     <div className={s.root}>
       {items.length === 0 && (
         <div className={s.empty}>
-          <img src={EmptyImg} role="presentation" alt="Empty history image" />
+          {/* <img src={EmptyImg} role="presentation" alt="Empty history image" /> */}
+          <EmptyIcon height={120} width={120} />
           {'Start your investigation by searching for required data from below'}
         </div>
       )}
