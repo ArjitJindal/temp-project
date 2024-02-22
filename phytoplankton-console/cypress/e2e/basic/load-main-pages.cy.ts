2217dda2242cdd112fbfc7b383b57e5b0ddae448
Jayant Patil <72199264+patiljayant@users.noreply.github.com>
2024-02-22 16:57:52 +0530

    Fix for failing load pages e2e test (#3328)

--- phytoplankton-console/cypress/e2e/basic/load-main-pages.cy.ts
+++ phytoplankton-console/cypress/e2e/basic/load-main-pages.cy.ts
@@ -3,12 +3,18 @@ describe('navigate sidebar', () => {
     cy.loginByRole('super_admin');
   });
 
+  const navigateToRulesPage = (url: string, title: string) => {
+    cy.visit(`/rules/${url}`);
+    cy.get(`div[aria-selected='true'][class='ant-tabs-tab-btn']`).eq(0).contains(title);
+    cy.get('[data-test="table"]');
+  };
+
   it('should load main pages', () => {
     cy.navigateToPage('/case-management/cases', 'Case management');
     cy.navigateToPage('/transactions/list', 'Transactions');
     cy.navigateToPage('/users/list/business/all', 'Users');
-    cy.navigateToPage('/rules/rules-library', 'Rules library');
-    cy.navigateToPage('/rules/my-rules', 'My rules');
+    navigateToRulesPage('my-rules', 'My rules');
+    navigateToRulesPage('rules-library', 'Library');
   });
 
   it('should load entity details', () => {
