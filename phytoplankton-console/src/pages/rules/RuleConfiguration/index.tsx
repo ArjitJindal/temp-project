d76ec81364334acd6d8d23cbaf33908b13295542
Nikolai Mavrenkov <koluch@koluch.ru>
2024-02-29 13:08:04 +0100

    #FR-4281: V8: Separate Create Rule Feature flag with Rule Config (#3348)
    #FR-4281: Add separate feature flag to use V8 with library rules

--- phytoplankton-console/src/pages/rules/RuleConfiguration/index.tsx
+++ phytoplankton-console/src/pages/rules/RuleConfiguration/index.tsx
@@ -5,9 +5,10 @@ import {
 } from 'src/pages/rules/RuleConfiguration/RuleConfigurationV2/RuleConfigurationSimulation';
 import RuleConfigurationV2, { Props as V2Props } from './RuleConfigurationV2';
 import RuleConfigurationV8, { Props as V8Props } from './RuleConfigurationV8';
-import { useIsV8RuleInstance } from '@/pages/rules/utils';
+import { useIsV8RuleInstance, useIsV8Rule } from '@/pages/rules/utils';
 import { useHasPermissions } from '@/utils/user-utils';
 import { RuleInstance } from '@/apis';
+import { useFeatureEnabled } from '@/components/AppWrapper/Providers/SettingsProvider';
 
 type Props = V8Props &
   V2Props &
@@ -27,34 +28,40 @@ export default function RuleConfiguration(props: Props) {
     };
   }
 
-  const isV8 = useIsV8RuleInstance({
+  const isV8LibraryEnabled = useFeatureEnabled('RULES_ENGINE_V8_LIBRARY');
+  const isRuleV8 = useIsV8Rule(props.rule);
+  const isInstanceV8 = useIsV8RuleInstance({
     ruleId: props.rule?.id,
     type: props.rule?.type ?? 'TRANSACTION',
     logic: props.rule?.defaultLogic,
     logicAggregationVariables: props.rule?.defaultLogicAggregationVariables,
   } as RuleInstance);
 
-  if (isV8 || props.rule == null) {
+  if (isInstanceV8 || props.rule == null) {
     return <RuleConfigurationV8 {...props} ruleInstance={ruleInstance} readOnly={readOnly} />;
   }
-  return props.isSimulation ? (
-    <RuleConfigurationSimulation
-      {...props}
-      ruleInstance={{
-        ruleId: props.rule.id,
-        parameters: props.rule.defaultParameters,
-        riskLevelParameters: props.rule.defaultRiskLevelParameters,
-        action: props.rule.defaultAction,
-        riskLevelActions: props.rule.defaultRiskLevelActions,
-        nature: props.rule.defaultNature,
-        casePriority: props.rule.defaultCasePriority,
-        filters: props.rule.defaultFilters,
-        labels: [],
-        checksFor: props.rule.checksFor,
-        type: props.rule.type,
-      }}
-    />
-  ) : (
-    <RuleConfigurationV2 {...props} ruleInstance={ruleInstance} readOnly={readOnly} />
-  );
+  if (props.isSimulation) {
+    return (
+      <RuleConfigurationSimulation
+        {...props}
+        ruleInstance={{
+          ruleId: props.rule.id,
+          parameters: props.rule.defaultParameters,
+          riskLevelParameters: props.rule.defaultRiskLevelParameters,
+          action: props.rule.defaultAction,
+          riskLevelActions: props.rule.defaultRiskLevelActions,
+          nature: props.rule.defaultNature,
+          casePriority: props.rule.defaultCasePriority,
+          filters: props.rule.defaultFilters,
+          labels: [],
+          checksFor: props.rule.checksFor,
+          type: props.rule.type,
+        }}
+      />
+    );
+  } else if (isRuleV8 && isV8LibraryEnabled) {
+    return <RuleConfigurationV8 {...props} readOnly={readOnly} />;
+  } else {
+    return <RuleConfigurationV2 {...props} ruleInstance={ruleInstance} readOnly={readOnly} />;
+  }
 }
