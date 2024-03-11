fd17dc9bc744218a84b2c11b71f194a267da37d4
Jayant Patil <72199264+patiljayant@users.noreply.github.com>
2024-03-11 16:21:33 +0530

    Retries google spreadsheet API (#3413)
    * Retry for spreadsheet
    
    * minor change
    
    * changed log statement
    
    * update in max delay
    
    * switched back to exponential backoff

--- tarpon/src/services/metrics/sheets-api-usage-metrics-service.ts
+++ tarpon/src/services/metrics/sheets-api-usage-metrics-service.ts
@@ -19,16 +19,16 @@ import { TenantBasic } from '@/services/accounts'
 import { getSecretByName } from '@/utils/secrets-manager'
 import { mergeObjects } from '@/utils/object'
 import { traceable } from '@/core/xray'
-
-const RETRY_STATUS_CODES = [500, 502, 503, 504, 408, 429, 524]
+import { logger } from '@/core/logger'
 
 const RETRY_OPTIONS: BackoffOptions = {
   startingDelay: 15 * 1000, // ms
   maxDelay: 120 * 1000, // ms
   jitter: 'full',
-  retry: (e: any, _attemptNumber: number) => {
-    if (e.statusCode && RETRY_STATUS_CODES.includes(e.statusCode)) return true
-    return false
+  numOfAttempts: 10,
+  retry: (e, i) => {
+    logger.warn(`Retry number ${i} for error: ${e.message}`)
+    return true
   },
 }
 
