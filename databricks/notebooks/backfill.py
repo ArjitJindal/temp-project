6d842925d1a7fe9f788f46d613224dcb44d19c93
Tim Coulson <tim@flagright.com>
2024-03-15 12:03:43 +0100

    Final fix for backfill script

--- databricks/notebooks/backfill.py
+++ databricks/notebooks/backfill.py
@@ -1,10 +1,12 @@
 # Databricks notebook source
-# MAGIC %pip install /Workspace/Shared/src-0.1.0-py3-none-any.whl
 # MAGIC %pip install databricks-sdk --upgrade
+# MAGIC %pip install /Workspace/Shared/src-0.1.0-py3-none-any.whl
+
+# COMMAND ----------
+dbutils.library.restartPython()
 
 # COMMAND ----------
 
-dbutils.library.restartPython()
 from databricks.sdk import WorkspaceClient
 
 import pymongo
