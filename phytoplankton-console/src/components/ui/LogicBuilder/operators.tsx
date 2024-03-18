f240befabafdbeaf304be165985a1fbc073983b8
Chia-Lun Wu <chia@flagright.com>
2024-03-18 23:45:30 +0100

    Fix between operator (#3500)
    * Fix between operator
    
    `jsonLogic` with non-string type will cause unexpected behaviour
    (condition cannot be displayed after reload).
    Revert the changes in b7ff4e5. We'll create custom operator for
    time variable in FR-4505.

--- phytoplankton-console/src/components/ui/LogicBuilder/operators.tsx
+++ phytoplankton-console/src/components/ui/LogicBuilder/operators.tsx
@@ -2,41 +2,6 @@ import { BasicConfig, Config, CoreOperators, Operator } from '@react-awesome-que
 import { omit } from 'lodash';
 import { RuleOperator } from '@/apis';
 
-const jsonLogicForBetween = (field, _op, values) => {
-  const valFrom = values[0];
-  const valTo = values[1];
-  if (field.var.endsWith('time') && valFrom > valTo) {
-    return {
-      '!': {
-        '<=': [valTo, field, valFrom],
-      },
-    };
-  }
-  return {
-    '<=': [valFrom, field, valTo],
-  };
-};
-
-const validateBetweenValues = (values) => {
-  //For time
-  if (
-    typeof values[0] === 'string' &&
-    typeof values[1] === 'string' &&
-    isTimeValue(values[0]) &&
-    isTimeValue(values[1])
-  )
-    return null;
-
-  if (values[0] != undefined && values[1] != undefined) {
-    return values[0] <= values[1] ? null : 'Invalid range';
-  }
-  return null;
-};
-
-const isTimeValue = (value: string) => {
-  return value.split(':').length === 3;
-};
-
 const BUILT_IN_OPERATORS = omit(BasicConfig.operators, [
   'starts_with',
   'ends_with',
@@ -45,7 +10,7 @@ const BUILT_IN_OPERATORS = omit(BasicConfig.operators, [
   'not_like',
   'multi_select_contains',
   'multi_select_not_contains',
-]);
+]) as CoreOperators<Config>;
 
 export const JSON_LOGIC_OPERATORS: CoreOperators<Config> = {
   ...BUILT_IN_OPERATORS,
@@ -56,20 +21,6 @@ export const JSON_LOGIC_OPERATORS: CoreOperators<Config> = {
   select_not_any_in: {
     ...BasicConfig.operators.select_not_any_in,
     valueTypes: ['multiselect', 'text'],
-  },
-  between: {
-    ...BasicConfig.operators.between,
-    // TODO: jsonLogic needs to be a string
-    jsonLogic: jsonLogicForBetween,
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    validateValues: validateBetweenValues,
-  },
-  not_between: {
-    ...BasicConfig.operators.not_between,
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    validateValues: validateBetweenValues,
   },
 };
 
