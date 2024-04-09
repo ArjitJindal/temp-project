28fb69752e6c65a99ead6e34f16cb26d55fb2292
Tim Coulson <tim@flagright.com>
2024-04-09 08:23:16 +0200

    Update currency rates

--- databricks/src/scripts/currency_rates_backfill.py
+++ databricks/src/scripts/currency_rates_backfill.py
@@ -47,4 +47,4 @@ def fetch_currency_data(start_date, end_date, access_key, base="USD"):
 
 # Example usage
 API_KEY = "some-api-key"
-fetch_currency_data("2024-03-12", "2024-04-04", API_KEY)
+fetch_currency_data("2024-04-05", "2024-04-09", API_KEY)
