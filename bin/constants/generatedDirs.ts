ArjitJindal/temp-project

commit a534810a9afcd963443c09cf43864e0390060405

Nuke public device data and sanctions API and stoplight usage (#3740)
* Sync openapi-public-management-original.yaml to flagright-api repo

* Nuke public sanctions API and public device data API

Remove the following public APIs
- openapi-public-device-data-original.yaml
- openapi-public-sanctions-original.yaml

And remove the related code.

* Don't rely on stoplight schema to build phytoplankton

We don't fetch the schema from remote anymore. Instead, we always get the schema
from tarpon directly.

* Update syncing openapi schemas

For public management API, it references models in the public API. We need to do
openapi:prepare step first and we sync from `dist/openapi` instead.

* Don't publish public management API to stoplight

This will retire stoplight usage entirely.

phytoplankton-console/build/openapi-generate.js
@@ -3,6 +3,17 @@
 /* eslint-disable */
 const execSync = require('child_process').execSync;
 const fs = require('fs');
+const path = require('path');
+
+const TARPON_PATH = path.resolve(__dirname, '..', '..', 'tarpon');
+const SOURCE_OPENAPI_INTERNAL_PATH = path.resolve(
+  TARPON_PATH,
+  'dist',
+  'openapi',
+  'internal',
+  'openapi-internal-original.yaml',
+);
+const OUTPUT_OPENAPI_INTERNAL_PATH = path.resolve(__dirname, '..', 'config', 'openapi.yaml');
 
 function removeBadImports(paths) {
   for (const path of paths) {
@@ -16,32 +27,6 @@ function removeBadImports(paths) {
     fs.writeFileSync(path, newText);
   }
 }
-
-function exec(command) {
-  execSync(command, { env: process.env });
-}
-
-exec('rm -rf src/apis');
-
-exec(
-  'yarn exec openapi-generator-cli generate -- -i config/openapi.yaml -g typescript -o src/apis --additional-properties=modelPropertyNaming=original --template-dir build/openapi_generate_templates/overrides',
-);
-
-exec('mkdir -p /tmp/flagright/phytoplankton || true');
-
-exec(
-  'yarn exec openapi-generator-cli generate -- -i config/openapi.yaml -g typescript -o /tmp/flagright/phytoplankton --additional-properties=modelPropertyNaming=original --template-dir build/openapi_generate_templates/custom',
-);
-
-exec('mv /tmp/flagright/phytoplankton/models src/apis/models-custom');
-
-exec(`if [ "$(uname)" = "Darwin" ]; then
-sed -i '' "s/\* as URLParse/URLParse/g" src/apis/http/http.ts
-sed -i '' "s/private url: URLParse/private url: URLParse<Record<string, string | undefined>>/g" src/apis/http/http.ts
-elif [ "$(expr substr $(uname -s) 1 5)" = "Linux" ]; then
-sed -i "s/\* as URLParse/URLParse/g" src/apis/http/http.ts
-sed -i "s/private url: URLParse/private url: URLParse<Record<string, string | undefined>>/g" src/apis/http/http.ts
-fi`);
 
 function replaceUserSavedPaymentDetails(paths) {
   for (const path of paths) {
@@ -84,23 +69,66 @@ function replaceSimulationGetResponse(paths) {
   }
 }
 
-const pathsToReplace = [
-  'src/apis/models/Business.ts',
-  'src/apis/models/InternalBusinessUser.ts',
-  'src/apis/models/InternalUser.ts',
-  'src/apis/models/InternalBusinessUser.ts',
-  'src/apis/models/BusinessOptional.ts',
-  'src/apis/models/BusinessWithRulesResult.ts',
-  'src/apis/models/BusinessResponse.ts',
-];
+function exec(command, cwd) {
+  execSync(command, { env: process.env, cwd });
+}
 
-removeBadImports(['src/apis/models/QuestionVariable.ts']);
+async function prepare() {
+  exec('yarn openapi:prepare', TARPON_PATH);
+  fs.copyFileSync(SOURCE_OPENAPI_INTERNAL_PATH, OUTPUT_OPENAPI_INTERNAL_PATH);
+}
 
-replaceUserSavedPaymentDetails(pathsToReplace);
+async function generate() {
+  exec('rm -rf src/apis');
 
-replaceSimulationGetResponse([
-  'src/apis/apis/DefaultApi.ts',
-  'src/apis/models/SimulationGetResponse.ts',
-]);
+  exec(
+    'yarn exec openapi-generator-cli generate -- -i config/openapi.yaml -g typescript -o src/apis --additional-properties=modelPropertyNaming=original --template-dir build/openapi_generate_templates/overrides',
+  );
 
-exec('npx prettier --write src/apis');
+  exec('mkdir -p /tmp/flagright/phytoplankton || true');
+
+  exec(
+    'yarn exec openapi-generator-cli generate -- -i config/openapi.yaml -g typescript -o /tmp/flagright/phytoplankton --additional-properties=modelPropertyNaming=original --template-dir build/openapi_generate_templates/custom',
+  );
+
+  exec('mv /tmp/flagright/phytoplankton/models src/apis/models-custom');
+
+  exec(`if [ "$(uname)" = "Darwin" ]; then
+sed -i '' "s/\* as URLParse/URLParse/g" src/apis/http/http.ts
+sed -i '' "s/private url: URLParse/private url: URLParse<Record<string, string | undefined>>/g" src/apis/http/http.ts
+elif [ "$(expr substr $(uname -s) 1 5)" = "Linux" ]; then
+sed -i "s/\* as URLParse/URLParse/g" src/apis/http/http.ts
+sed -i "s/private url: URLParse/private url: URLParse<Record<string, string | undefined>>/g" src/apis/http/http.ts
+fi`);
+
+  const pathsToReplace = [
+    'src/apis/models/Business.ts',
+    'src/apis/models/InternalBusinessUser.ts',
+    'src/apis/models/InternalUser.ts',
+    'src/apis/models/InternalBusinessUser.ts',
+    'src/apis/models/BusinessOptional.ts',
+    'src/apis/models/BusinessWithRulesResult.ts',
+    'src/apis/models/BusinessResponse.ts',
+  ];
+
+  removeBadImports(['src/apis/models/QuestionVariable.ts']);
+
+  replaceUserSavedPaymentDetails(pathsToReplace);
+
+  replaceSimulationGetResponse([
+    'src/apis/apis/DefaultApi.ts',
+    'src/apis/models/SimulationGetResponse.ts',
+  ]);
+
+  exec('npx prettier --write src/apis');
+}
+
+async function main() {
+  await prepare();
+  await generate();
+}
+
+main().catch((e) => {
+  console.error(e);
+  process.exit(1);
+});

tarpon/scripts/openapi/openapi_prepare.js
@@ -22,39 +22,16 @@ async function prepareSchemas(OUTPUT_DIR) {
       'openapi',
       'public-management'
     )
-    const publicDeviceDataDir = path.resolve(
-      PROJECT_DIR,
-      'lib',
-      'openapi',
-      'public-device-data'
-    )
-    const publicSanctionsDir = path.resolve(
-      PROJECT_DIR,
-      'lib',
-      'openapi',
-      'public-sanctions'
-    )
-
     const internalDirOutput = path.resolve(OUTPUT_DIR, 'internal')
     const publicDirOutput = path.resolve(OUTPUT_DIR, 'public')
     const publicManagementDirOutput = path.resolve(
       OUTPUT_DIR,
       'public-management'
     )
-    const publicDeviceDataDirOutput = path.resolve(
-      OUTPUT_DIR,
-      'public-device-data'
-    )
-    const publicSanctionsDirOutput = path.resolve(
-      OUTPUT_DIR,
-      'public-sanctions'
-    )
 
     await fs.ensureDir(internalDirOutput)
     await fs.ensureDir(publicDirOutput)
     await fs.ensureDir(publicManagementDirOutput)
-    await fs.ensureDir(publicDeviceDataDirOutput)
-    await fs.ensureDir(publicSanctionsDirOutput)
 
     const publicSchemaFile = path.resolve(
       publicDir,
@@ -64,28 +41,12 @@ async function prepareSchemas(OUTPUT_DIR) {
       publicManagementDir,
       'openapi-public-management-original.yaml'
     )
-    const publicDeviceSchemaFile = path.resolve(
-      publicDeviceDataDir,
-      'openapi-public-device-data-original.yaml'
-    )
     const publicSchemaText = (await fs.readFile(publicSchemaFile)).toString()
     const publicManagementSchemaText = (
       await fs.readFile(publicManagementSchemaFile)
     ).toString()
     const publicManagementSchemaYaml = parse(publicManagementSchemaText)
     const publicSchemaYaml = parse(publicSchemaText)
-    const publicDeviceSchemaText = (
-      await fs.readFile(publicDeviceSchemaFile)
-    ).toString()
-    const publicDeviceSchemaYaml = parse(publicDeviceSchemaText)
-    const publicSantionsSchemaFile = path.resolve(
-      publicSanctionsDir,
-      'openapi-public-sanctions-original.yaml'
-    )
-    const publicSanctionsSchemaText = (
-      await fs.readFile(publicSantionsSchemaFile)
-    ).toString()
-
     const internalSchemaFile = path.resolve(
       internalDir,
       'openapi-internal-original.yaml'
@@ -108,7 +69,6 @@ async function prepareSchemas(OUTPUT_DIR) {
       internalSchemaYaml.components.schemas = {
         ...internalSchemaYaml.components.schemas,
         ...publicSchemaYaml.components.schemas,
-        ...publicDeviceSchemaYaml.components.schemas,
         ..._.pick(publicManagementSchemaYaml.components.schemas, [
           'ActionReason',
         ]),
@@ -141,27 +101,6 @@ async function prepareSchemas(OUTPUT_DIR) {
           'openapi-public-management-original.yaml'
         ),
         stringify(publicManagementSchemaYaml)
-      )
-    }
-    {
-      await fs.copy(publicDeviceDataDir, publicDeviceDataDirOutput)
-    }
-    {
-      await fs.copy(publicSanctionsDir, publicSanctionsDirOutput)
-      const publicSanctionsSchemaYaml = await localizeRefs(
-        parse(publicSanctionsSchemaText)
-      )
-      publicSanctionsSchemaYaml.components.schemas = {
-        ...publicSanctionsSchemaYaml.components.schemas,
-        ..._.pick(publicSchemaYaml.components.schemas, ['CountryCode']),
-      }
-      await fs.copy(publicSanctionsDir, publicSanctionsDirOutput)
-      await fs.writeFile(
-        path.resolve(
-          publicSanctionsDirOutput,
-          'openapi-public-sanctions-original.yaml'
-        ),
-        stringify(publicSanctionsSchemaYaml)
       )
     }
   } catch (err) {

.github/workflows/test_tarpon.yml
@@ -51,6 +51,8 @@ jobs:
         working-directory: "lib"
       - run: yarn install --immutable
         working-directory: "tarpon"
+      - run: yarn run openapi:prepare
+        working-directory: "tarpon"
       - run: yarn run build:local
         working-directory: "tarpon"
       - run: yarn run tsc:ci
@@ -110,6 +112,13 @@ jobs:
       #   run: |
       #     npm run test:coverage:comment "${{ github.token }}"
       #   working-directory: "tarpon"
+      - name: Sync OpenAPI schemas
+        if: github.ref == 'refs/heads/main'
+        uses: BetaHuhn/repo-file-sync-action@v1
+        with:
+          GH_PAT: ${{ secrets.FLAGRIGHT_API_SYNC_PAT }}
+          ORIGINAL_MESSAGE: true
+          COMMIT_BODY: patch
       - name: Post to a Slack channel on failure
         id: slack
         uses: slackapi/slack-github-action@v1.25.0

commit 37ef9036308ecab66b34a5526d3601769e1614a6

Fix deployment issues caused by a534810a (#3758)

bin/constants/generatedDirs.ts
@@ -7,10 +7,6 @@ const GENERATED_DIRS = [
   'src/@types/openapi-public-custom',
   'src/@types/openapi-public-management',
   'src/@types/openapi-public-management-custom',
-  'src/@types/openapi-public-device-data',
-  'src/@types/openapi-public-device-data-custom',
-  'src/@types/openapi-public-sanctions',
-  'src/@types/openapi-public-sanctions-custom',
   '.gen',
 ]
 

phytoplankton-console/build/openapi-generate.js
@@ -74,7 +74,9 @@ function exec(command, cwd) {
 }
 
 async function prepare() {
-  exec('yarn openapi:prepare', TARPON_PATH);
+  // Install the required dependencies for running openapi:prepare
+  exec('npm install -g js-yaml prettier', TARPON_PATH);
+  exec('npm run openapi:prepare', TARPON_PATH);
   fs.copyFileSync(SOURCE_OPENAPI_INTERNAL_PATH, OUTPUT_OPENAPI_INTERNAL_PATH);
 }
 

tarpon/scripts/openapi/openapi_prepare.js
@@ -3,7 +3,6 @@ const fs = require('fs-extra')
 const path = require('path')
 const mkdirp = require('mkdirp')
 const _ = require('lodash')
-require('dotenv').config()
 
 const {
   PROJECT_DIR,

.github/workflows/test_tarpon.yml
@@ -51,7 +51,7 @@ jobs:
         working-directory: "lib"
       - run: yarn install --immutable
         working-directory: "tarpon"
-      - run: yarn run openapi:prepare
+      - run: yarn run openapi:format && yarn run openapi:prepare
         working-directory: "tarpon"
       - run: yarn run build:local
         working-directory: "tarpon"