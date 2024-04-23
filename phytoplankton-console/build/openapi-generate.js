a5654217d7eee9eeb02c2f2c2708eb8b8ec8607d
Chia-Lun Wu <chia@flagright.com>
2024-04-23 18:21:38 +0200

    Fix 37ef903

--- phytoplankton-console/build/openapi-generate.js
+++ phytoplankton-console/build/openapi-generate.js
@@ -74,9 +74,7 @@ function exec(command, cwd) {
 }
 
 async function prepare() {
-  // Install the required dependencies for running openapi:prepare
-  exec('npm install -g js-yaml prettier', TARPON_PATH);
-  exec('npm run openapi:prepare', TARPON_PATH);
+  exec('yarn && yarn openapi:prepare', TARPON_PATH);
   fs.copyFileSync(SOURCE_OPENAPI_INTERNAL_PATH, OUTPUT_OPENAPI_INTERNAL_PATH);
 }
 
