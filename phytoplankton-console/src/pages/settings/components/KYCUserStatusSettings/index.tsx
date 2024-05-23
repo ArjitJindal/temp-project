4166aa177e62f77bde6efccb67e0eeecd8718928
Aman Dugar <55328714+amandugar@users.noreply.github.com>
2024-05-24 01:46:06 +0530

    Disable kyc and user status lock in settings if not Admin (#3970)
    * Disable kyc lock if not Admin
    
    * Fix

--- phytoplankton-console/src/pages/settings/components/KYCUserStatusSettings/index.tsx
+++ phytoplankton-console/src/pages/settings/components/KYCUserStatusSettings/index.tsx
@@ -4,9 +4,12 @@ import {
   useUpdateTenantSettings,
 } from '@/components/AppWrapper/Providers/SettingsProvider';
 import Toggle from '@/components/library/Toggle';
+import { isAtLeastAdmin, useAuth0User } from '@/utils/user-utils';
+import Tooltip from '@/components/library/Tooltip';
 
 export const KYCUserStatusSettings = () => {
   const settings = useSettings();
+  const user = useAuth0User();
 
   const mutateTenantSettings = useUpdateTenantSettings();
   const handleDisable = () => {
@@ -16,16 +19,20 @@ export const KYCUserStatusSettings = () => {
   const handleEnable = () => {
     mutateTenantSettings.mutate({ kycUserStatusLock: true });
   };
+
   return (
     <SettingsCard
       title="KYC/user status lock"
       description="When enabled, prevents editing of 'KYC status' and 'User status' fields on user details page."
     >
-      <Toggle
-        value={settings.kycUserStatusLock}
-        onChange={settings.kycUserStatusLock ? handleDisable : handleEnable}
-        loading={mutateTenantSettings.isLoading}
-      />
+      <Tooltip title={isAtLeastAdmin(user) ? 'User must be at least an admin' : ''}>
+        <Toggle
+          value={settings.kycUserStatusLock}
+          onChange={settings.kycUserStatusLock ? handleDisable : handleEnable}
+          loading={mutateTenantSettings.isLoading}
+          disabled={isAtLeastAdmin(user)}
+        />
+      </Tooltip>
     </SettingsCard>
   );
 };
