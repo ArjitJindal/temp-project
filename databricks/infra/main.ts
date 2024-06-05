d2ac25cea8d64e2ca67a693e0c3cbf277822ad5f
Tim Coulson <tim@flagright.com>
2024-06-05 09:26:37 +0200

    Increase worker size for backfill and refresh

--- databricks/infra/main.ts
+++ databricks/infra/main.ts
@@ -46,8 +46,7 @@ const jobs = [
     name: 'refresh',
     description: 'Rebuild derived tables from kinesis and mongo data.',
     continuous: false,
-    compute: 'G.1X',
-    // compute: "G.8X",
+    compute: 'G.8X',
     numWorkers: 2,
   },
   {
@@ -55,8 +54,7 @@ const jobs = [
     description:
       'Reset everything by clearing all tables and backfilling from mongo.',
     continuous: false,
-    compute: 'G.1X',
-    // compute: "G.8X",
+    compute: 'G.8X',
     numWorkers: 2,
   },
   {
