eaa85e6a8eeb2855f3e7502055fee81605be7c8c
Tim Coulson <tim@flagright.com>
2024-09-17 11:57:28 +0200

    Fix whitelist migration (#4751)
    * Fix whitelist migration
    
    * Fix whitelist migration

--- tarpon/scripts/migrations/pre-deployment/2024.09.17T18.27.39.migrate-whitelist.ts
+++ tarpon/scripts/migrations/pre-deployment/2024.09.17T18.27.39.migrate-whitelist.ts
@@ -1,42 +1,23 @@
 import { migrateAllTenants } from '../utils/tenant'
 import { Tenant } from '@/services/accounts'
 import { getMongoDbClient } from '@/utils/mongodb-utils'
-import {
-  SANCTIONS_HITS_COLLECTION,
-  SANCTIONS_WHITELIST_ENTITIES_COLLECTION,
-} from '@/utils/mongodb-definitions'
+import { SANCTIONS_WHITELIST_ENTITIES_COLLECTION } from '@/utils/mongodb-definitions'
 import { SanctionsWhitelistEntity } from '@/@types/openapi-internal/SanctionsWhitelistEntity'
-import { SanctionsHit } from '@/@types/openapi-internal/SanctionsHit'
 import { complyAdvantageDocToEntity } from '@/services/sanctions/providers/comply-advantage-provider'
+import { ComplyAdvantageSearchHitDoc } from '@/@types/openapi-internal/ComplyAdvantageSearchHitDoc'
 
 async function migrateTenant(tenant: Tenant) {
   const db = (await getMongoDbClient()).db()
-  const sanctionsHitCollection = db.collection<SanctionsHit>(
-    SANCTIONS_HITS_COLLECTION(tenant.id)
-  )
+
   const sanctionsWhiteListCollection = db.collection<SanctionsWhitelistEntity>(
     SANCTIONS_WHITELIST_ENTITIES_COLLECTION(tenant.id)
   )
-
-  for await (const sanctionsHit of sanctionsHitCollection.find({})) {
-    if (sanctionsHit.caEntity) {
+  for await (const sanctionsWhitelist of sanctionsWhiteListCollection.find({
+    'sanctionsEntity.doc': { $exists: true },
+  })) {
+    if (sanctionsWhitelist.sanctionsEntity) {
       const entity = complyAdvantageDocToEntity({
-        doc: sanctionsHit.caEntity,
-        match_types_details: sanctionsHit.caMatchTypesDetails,
-        match_types: sanctionsHit.caMatchTypes,
-      })
-      await sanctionsHitCollection.updateOne(
-        { _id: sanctionsHit._id },
-        { $set: { entity: entity } }
-      )
-    }
-  }
-  for await (const sanctionsWhitelist of sanctionsWhiteListCollection.find(
-    {}
-  )) {
-    if (sanctionsWhitelist.caEntity) {
-      const entity = complyAdvantageDocToEntity({
-        doc: sanctionsWhitelist.caEntity,
+        doc: sanctionsWhitelist.sanctionsEntity as ComplyAdvantageSearchHitDoc,
       })
       await sanctionsWhiteListCollection.updateOne(
         { _id: sanctionsWhitelist._id },
