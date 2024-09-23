e9a65f9d127ca09645dec928402b58951bde8c59
Madhu G Nadig <madhug.nadig@gmail.com>
2024-09-24 01:00:05 +0200

    Update provisioned capacity of sandbox lambda

--- lib/config/config-sandbox-asia-1.ts
+++ lib/config/config-sandbox-asia-1.ts
@@ -8,26 +8,20 @@ export const config: Config = {
   ...sandboxbaseConfig,
   region: 'asia-1',
   env: { account, region },
-  budgets: {
-    CLOUDWATCH: 200,
-    DYNAMODB: 10,
-    LAMBDA: 250,
-    S3: 20,
-    SQS: 10,
-    EC2: 200,
-    GUARDDUTY: 30,
-    KINESIS: 40,
-    SECRETS_MANAGER: 15,
-    VPC: 20,
-    EMR: 100,
-    GLUE: 30,
-  },
   application: {
     ...sandboxbaseConfig.application,
     MONGO_ATLAS_CLUSTER: 'Sandbox-SIN',
     BETTERUPTIME_HOOK_URL:
       'https://uptime.betterstack.com/api/v1/aws-cloudwatch/webhook/8kuHsa8jtTLABtyTSv4Fu77d',
     MONGO_EVENT_TRIGGER_RULE_ID: '66ed4f296781e98cf16e7dad',
+  },
+  resource: {
+    ...sandboxbaseConfig.resource,
+    TRANSACTION_LAMBDA: {
+      MAX_PROVISIONED_CONCURRENCY: 20,
+      MIN_PROVISIONED_CONCURRENCY: 2,
+      MEMORY_SIZE: 1024,
+    },
   },
   clickhouse: {
     privateEndpoint: {
