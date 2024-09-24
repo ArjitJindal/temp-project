b6e30be6998ea744a7a58a52abc376a9f56db965
Chia-Lun Wu <chia@flagright.com>
2024-09-24 12:09:29 +0200

    Fix QA deployment error
    Related to fad581ae.

--- lib/utils/env.ts
+++ lib/utils/env.ts
@@ -1,4 +1,8 @@
 export function stageAndRegion() {
   const [stage, region = 'eu-1'] = process.env.ENV?.split(':') || []
+  // NOTE: QA env is 'dev:user'
+  if (stage === 'dev') {
+    return [stage, 'eu-1']
+  }
   return [stage, region]
 }
