48b6d9972feeaba91a8e5dffa539bd97c09e8b96
Aman Dugar <55328714+amandugar@users.noreply.github.com>
2024-09-27 16:19:15 +0530

    Use Context Error Fix (#4840)

--- phytoplankton-console/src/components/AccountMessage/index.tsx
+++ phytoplankton-console/src/components/AccountMessage/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import { P } from '../ui/Typography';
 import { COLORS_V2_ALERT_CRITICAL } from '../ui/colors';
-import Button from '../library/Button';
+import { BaseButton } from '../library/Button';
 import s from './index.module.less';
 import { getBranding } from '@/utils/branding';
 import AlertIcon from '@/components/ui/icons/Remix/system/alert-fill.react.svg';
@@ -29,13 +29,13 @@ export const AccountMessage: React.FC<AccountMessageProps> = ({ title, message }
         <a href={`mailto:${branding.supportEmail}`}>{branding.supportEmail}</a> if you have any
         questions.
       </P>
-      <Button
+      <BaseButton
         onClick={() => logout({ returnTo: window.location.origin })}
         icon={<LogOutIcon />}
         className={s.button}
       >
         Log out
-      </Button>
+      </BaseButton>
     </div>
   );
 };
