a15d47eb86f405258832aa67d1cfea1f3dde8e2c
Kshitij Ayush <76985867+kshitij-404@users.noreply.github.com>
2024-10-18 01:09:33 +0530

    Fix spacing for transaction tags (#4911)
    * fix: transaction tags display spacing
    
    * fix: transaction tags display spacing
    
    * update: remove customContent and use css with className to fix spacing as per the new row-column template
    
    * update: new logic for tag row-column template
    
    * update: add columnTemplate prop to replace className based styling

--- phytoplankton-console/src/components/ui/EntityPropertiesCard/index.tsx
+++ phytoplankton-console/src/components/ui/EntityPropertiesCard/index.tsx
@@ -11,26 +11,34 @@ interface Props {
   items?: Item[];
   columns?: number;
   children?: React.ReactNode;
+  columnTemplate?: string;
 }
 
 export default function EntityPropertiesCard(props: Props) {
-  const { title, extraControls, items, columns = 1, children } = props;
+  const { title, extraControls, items, columns = 1, children, columnTemplate } = props;
   return (
     <div className={s.root}>
       <div className={s.header}>
         <div className={s.title}>{title}</div>
         {extraControls}
       </div>
-      {items && (
-        <div className={s.items} style={{ gridTemplateColumns: `repeat(${columns * 2}, auto)` }}>
-          {items.map(({ label, value }) => (
-            <>
-              <div className={s.itemLabel}>{label}</div>
-              <div className={s.itemValue}>{value ?? '-'}</div>
-            </>
-          ))}
-        </div>
-      )}
+      {
+        <>
+          {items && (
+            <div
+              className={s.items}
+              style={{ gridTemplateColumns: columnTemplate || `repeat(${columns * 2}, auto)` }}
+            >
+              {items.map(({ label, value }) => (
+                <>
+                  <div className={s.itemLabel}>{label}</div>
+                  <div className={s.itemValue}>{value ?? '-'}</div>
+                </>
+              ))}
+            </div>
+          )}
+        </>
+      }
       {children}
     </div>
   );
--- phytoplankton-console/src/pages/transactions-item/TransactionTags/index.tsx
+++ phytoplankton-console/src/pages/transactions-item/TransactionTags/index.tsx
@@ -2,13 +2,28 @@ import React from 'react';
 import { InternalTransaction, Tag as ApiTag } from '@/apis';
 import EntityPropertiesCard from '@/components/ui/EntityPropertiesCard';
 import EntityInfoGrid from '@/components/ui/EntityInfoGrid';
-
 interface Props {
   transaction: InternalTransaction;
 }
 
 export default function TransactionTags(props: Props) {
   const { transaction } = props;
+
+  const getColumns = (tagsCount: number) => {
+    if (tagsCount < 10) {
+      return 1;
+    }
+    if (tagsCount < 30) {
+      return 2;
+    }
+    return 3;
+  };
+
+  const getColumnTemplate = (tagsCount: number) => {
+    const columns = getColumns(tagsCount);
+    return Array(columns).fill('min-content auto').join(' ');
+  };
+
   return (
     <EntityInfoGrid.Root>
       <EntityInfoGrid.Cell>
@@ -17,7 +32,7 @@ export default function TransactionTags(props: Props) {
           items={
             transaction.tags?.map((tag: ApiTag) => ({ label: tag.key, value: tag.value })) ?? []
           }
-          columns={2}
+          columnTemplate={getColumnTemplate(transaction.tags?.length ?? 0)}
         />
       </EntityInfoGrid.Cell>
     </EntityInfoGrid.Root>
