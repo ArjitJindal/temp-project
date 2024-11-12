9835be72a930a115e2372a8459a18227dc27911d
Kshitij Ayush <76985867+kshitij-404@users.noreply.github.com>
2024-11-12 21:11:44 +0530

    Add new model in AI library & change names (#5282)
    feat: add ml model unexpected transactional behaviour to demo data
    
    Co-authored-by: Aman Dugar <55328714+amandugar@users.noreply.github.com>

--- tarpon/src/core/seed/samplers/ml-models.ts
+++ tarpon/src/core/seed/samplers/ml-models.ts
@@ -4,34 +4,46 @@ export const getMlModelsSample = (): RuleMLModel[] => {
   return [
     {
       id: 'ML-001',
-      name: 'ML Model - Fraud (Generic)',
-      description: 'Model to detect overall fraud across all payment methods',
+      name: 'Fraud (Generic)',
+      description:
+        'Detects fraud with a generic model across all payment methods and jurisdictions with an umbrella approach.',
       modelType: 'EXPLAINABLE',
       checksFor: ['Fraud'],
       enabled: true,
     },
     {
       id: 'ML-002',
-      name: 'ML Model - Fraud credit card',
-      description: 'Model to detect overall fraud of the credit card',
+      name: 'Fraud credit card',
+      description: 'Focuses on credit card fraud risks specifically.',
       modelType: 'EXPLAINABLE',
       checksFor: ['Credit card fraud'],
       enabled: true,
     },
     {
       id: 'ML-003',
-      name: 'ML Model - Money laundering',
-      description: 'Model to detect money laundering',
+      name: 'Money laundering',
+      description:
+        'Checks for money laundering-related risks, specifically structuring and money mules.',
       modelType: 'EXPLAINABLE',
       checksFor: ['Money laundering'],
       enabled: true,
     },
     {
       id: 'ML-004',
-      name: 'ML Model - Bank transfer',
-      description: 'Model to detect overall fraud in Bank transfer',
+      name: 'Bank transfer',
+      description:
+        'Detects fraud on bank transfers specifically focusing on potential social engineering, account takeover, or authorized push payment fraud.',
       modelType: 'NON_EXPLAINABLE',
       checksFor: ['Fraud', 'Bank Fraud'],
+      enabled: true,
+    },
+    {
+      id: 'ML-005',
+      name: 'Unexpected transactional behavior',
+      description:
+        'Detects behavior change of a user based on transaction attributes such as IP address, amount, time of transaction, location of the transaction, a new beneficiary account and so forth.',
+      modelType: 'EXPLAINABLE',
+      checksFor: ['Fraud', 'Behavioral Change'],
       enabled: true,
     },
   ]
