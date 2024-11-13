3db89577068c702ef45e9d11163570e5650c7bb4
Nikolai Mavrenkov <koluch@koluch.ru>
2024-11-13 18:16:17 +0100

    #FR-5569: Make case management filters persisting (#5277)
    * Implement saving and filter params in local storage
    
    * Implement saving and filter params in local storage

--- phytoplankton-console/src/pages/case-management/index.tsx
+++ phytoplankton-console/src/pages/case-management/index.tsx
@@ -1,5 +1,6 @@
-import React, { useCallback, useEffect, useState } from 'react';
-import { useNavigate } from 'react-router';
+import React, { useCallback, useEffect } from 'react';
+import { useLocation, useNavigate } from 'react-router';
+import { useLocalStorageState } from 'ahooks';
 import CaseTableWrapper from './CaseTableWrapper';
 import AlertTable from './AlertTable';
 import s from './index.module.less';
@@ -12,8 +13,8 @@ import { TableSearchParams } from '@/pages/case-management/types';
 import { makeUrl, parseQueryString } from '@/utils/routing';
 import { queryAdapter } from '@/pages/case-management/helpers';
 import { AllParams } from '@/components/library/Table/types';
-import { DEFAULT_PAGE_SIZE, DEFAULT_PARAMS_STATE } from '@/components/library/Table/consts';
-import { useDeepEqualEffect } from '@/utils/hooks';
+import { DEFAULT_PARAMS_STATE } from '@/components/library/Table/consts';
+import { useDeepEqualMemo, useIsChanged } from '@/utils/hooks';
 import ScopeSelector, {
   ScopeSelectorValue,
 } from '@/pages/case-management/components/ScopeSelector';
@@ -28,6 +29,10 @@ import Tooltip from '@/components/library/Tooltip';
 import { getBranding } from '@/utils/branding';
 import { DerivedStatus } from '@/apis';
 
+type NavigationState = {
+  isInitialised: boolean;
+} | null;
+
 export default function CaseManagementPage() {
   const i18n = useI18n();
   useCloseSidebarByDefault();
@@ -38,24 +43,21 @@ export default function CaseManagementPage() {
     showCases: qaMode ? 'QA_UNCHECKED_ALERTS' : 'ALL',
     ...parseQueryString(location.search),
   });
-  const [params, setParams] = useState<AllParams<TableSearchParams>>({
-    ...DEFAULT_PARAMS_STATE,
-    ...parsedParams,
-    caseStatus: null,
-    alertStatus: null,
-  });
+  const [savedParams, setSavedParams] =
+    useLocalStorageState<AllParams<TableSearchParams>>('CASE_MANAGEMENT_FILTERS');
 
-  useEffect(() => {
-    if (qaMode) {
-      setParams((params) => ({ ...params, showCases: 'QA_UNCHECKED_ALERTS' }));
-    } else {
-      setParams((params) => ({ ...params, showCases: 'ALL' }));
-    }
-  }, [qaMode]);
-
-  useEffect(() => {
-    setParams((params) => ({ ...params, alertStatus: null, caseStatus: null }));
-  }, [parsedParams.showCases]);
+  const params = useDeepEqualMemo(() => {
+    return {
+      ...DEFAULT_PARAMS_STATE,
+      ...parsedParams,
+      caseStatus: !parsedParams.caseStatus
+        ? getDefaultStatus(parsedParams.showCases)
+        : parsedParams.caseStatus,
+      alertStatus: !parsedParams.alertStatus
+        ? getDefaultStatus(parsedParams.showCases)
+        : parsedParams.alertStatus,
+    };
+  }, [qaMode, parsedParams, savedParams]);
 
   const pushParamsToNavigation = useCallback(
     (params: TableSearchParams) => {
@@ -68,43 +70,48 @@ export default function CaseManagementPage() {
       if (params.showCases === 'MY_ALERTS' || params.showCases === 'MY') {
         params.assignedTo = undefined;
       }
+      const navigationState: NavigationState = {
+        isInitialised: true,
+      };
       navigate(makeUrl('/case-management/cases', {}, queryAdapter.serializer(params)), {
         replace: true,
+        state: navigationState,
       });
     },
     [navigate],
   );
 
-  const handleChangeParams = (newParams: AllParams<TableSearchParams>) => {
-    pushParamsToNavigation(newParams);
-  };
+  const handleChangeParams = useCallback(
+    (newParams: AllParams<TableSearchParams>) => {
+      pushParamsToNavigation(newParams);
+      setSavedParams(newParams);
+    },
+    [pushParamsToNavigation, setSavedParams],
+  );
 
-  const getDefaultStatus = (param?: ScopeSelectorValue): DerivedStatus[] => {
-    if (param?.includes('QA')) {
-      return ['CLOSED'];
+  const locationState = useLocation();
+  const isInitialised = (locationState.state as NavigationState)?.isInitialised || false;
+  const isQaChanged = useIsChanged(qaMode);
+  useEffect(() => {
+    if (!isInitialised && savedParams != null) {
+      pushParamsToNavigation(savedParams);
+    } else if (isQaChanged) {
+      handleChangeParams({
+        ...params,
+        showCases: qaMode ? 'QA_UNCHECKED_ALERTS' : 'ALL',
+      });
     }
-
-    return ['OPEN'];
-  };
+  }, [
+    params,
+    qaMode,
+    isQaChanged,
+    savedParams,
+    isInitialised,
+    handleChangeParams,
+    pushParamsToNavigation,
+  ]);
 
   const settings = useSettings();
-  useDeepEqualEffect(() => {
-    setParams((prevState: AllParams<TableSearchParams>) => ({
-      ...prevState,
-      ...parsedParams,
-      caseStatus:
-        prevState.caseStatus === null && !parsedParams.caseStatus
-          ? getDefaultStatus(parsedParams.showCases)
-          : parsedParams.caseStatus,
-      alertStatus:
-        prevState.alertStatus === null && !parsedParams.alertStatus
-          ? getDefaultStatus(parsedParams.showCases)
-          : parsedParams.alertStatus,
-      page: parsedParams.page ?? 1,
-      sort: parsedParams.sort ?? [],
-      pageSize: parsedParams.pageSize ?? DEFAULT_PAGE_SIZE,
-    }));
-  }, [parsedParams]);
 
   const normalModeItems: Item<ScopeSelectorValue>[] = [
     { value: 'ALL', label: 'All cases' },
@@ -249,3 +256,14 @@ function getTable(
       );
   }
 }
+
+/*
+  Helpers
+ */
+const getDefaultStatus = (param?: ScopeSelectorValue): DerivedStatus[] => {
+  if (param?.includes('QA')) {
+    return ['CLOSED'];
+  }
+
+  return ['OPEN'];
+};
