147b9706dbe1b6f1aaaaf10fb166761c040ff5f5
Aman Dugar <55328714+amandugar@users.noreply.github.com>
2024-11-18 15:59:42 +0800

    Remove risk-levels:write requirement for user risk level changes (#5362)
    * Remove risk-levels:write requirement for user risk level changes
    
    * Fix

---
+++ tarpon/scripts/migrations/post-deployment/2024.11.18T02.50.01.add-risk-assignment-permission.ts
@@ -0,0 +1,26 @@
+import { migrateAllTenants } from '../utils/tenant'
+import { RoleService } from '@/services/roles'
+import { Tenant } from '@/services/accounts'
+
+async function migrateTenant(tenant: Tenant, auth0Domain: string) {
+  const rolesService = new RoleService({
+    auth0Domain,
+  })
+
+  const roles = await rolesService.getTenantRoles(tenant.id)
+  for (const role of roles) {
+    if (role.permissions.includes('risk-scoring:risk-levels:write')) {
+      await rolesService.updateRolePermissions(role.id, [
+        ...role.permissions,
+        'users:user-manual-risk-levels:write',
+      ])
+    }
+  }
+}
+
+export const up = async () => {
+  await migrateAllTenants(migrateTenant)
+}
+export const down = async () => {
+  // skip
+}
