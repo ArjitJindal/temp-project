e05584753dfe597e6fea2cb40beb876d7a388029
Tim Coulson <tim@flagright.com>
2024-12-06 16:25:13 +0000

    Check for rule stage being ONGOING

--- tarpon/src/services/rules-engine/utils/user-rule-utils.ts
+++ tarpon/src/services/rules-engine/utils/user-rule-utils.ts
@@ -1,6 +1,7 @@
 import { User } from '@/@types/openapi-public/User'
 import { Business } from '@/@types/openapi-public/Business'
 import { RuleInstance } from '@/@types/openapi-internal/RuleInstance'
+import { RuleStage } from '@/@types/openapi-internal/RuleStage'
 
 export function isConsumerUser(user: User | Business): user is User {
   return !isBusinessUser(user)
@@ -19,12 +20,18 @@ export function isOngoingUserRuleInstance(
     return true
   }
 
+  const checkForOngoing = (parameters: {
+    ongoingScreening?: boolean
+    ruleStages?: RuleStage[]
+  }) =>
+    Boolean(
+      parameters?.ongoingScreening || parameters.ruleStages?.includes('ONGOING')
+    )
+
   if (isRiskLevelsEnabled && ruleInstance.riskLevelParameters) {
     return Boolean(
-      Object.values(ruleInstance.riskLevelParameters).find(
-        (parameters) => parameters?.ongoingScreening
-      )
+      Object.values(ruleInstance.riskLevelParameters).find(checkForOngoing)
     )
   }
-  return Boolean(ruleInstance.parameters?.ongoingScreening)
+  return checkForOngoing(ruleInstance.parameters)
 }
