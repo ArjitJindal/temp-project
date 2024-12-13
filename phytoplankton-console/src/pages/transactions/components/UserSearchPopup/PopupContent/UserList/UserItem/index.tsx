ArjitJindal/temp-project

commit d1f2843a4afa5b336dddd96f6c88bf742685e30a

Pick only required fields in Transactions and Users API (#5460)
* remove comment

* Added Only Select Required Fields

* Added Only Select Required Fields

* Added Only Select Required Fields

* Added Only Select Required Fields

* Push

* Fix

* Fix

* Fix

* Cleanup

* Fix Transactions Query

---------

Co-authored-by: Aman Dugar <amandugar@Amans-MacBook-Air.local>

phytoplankton-console/src/pages/transactions/components/UserSearchPopup/PopupContent/UserList/UserItem/index.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import { List } from 'antd';
 import cn from 'clsx';
 import pluralize from 'pluralize';
-import { User } from '../../../types';
 import s from './style.module.less';
 import { getUserName } from '@/utils/api/users';
 import RiskLevelTag from '@/components/library/Tag/RiskLevelTag';
+import { AllUsersTableItem } from '@/apis';
 
 interface Props {
   isActive: boolean;
-  user: User;
+  user: AllUsersTableItem;
   onClick: () => void;
 }
 

tarpon/src/services/users/repositories/user-repository.ts
@@ -89,6 +89,7 @@ import { AverageArsScore } from '@/@types/openapi-internal/AverageArsScore'
 import { PersonAttachment } from '@/@types/openapi-internal/PersonAttachment'
 import { filterOutInternalRules } from '@/services/rules-engine/pnb-custom-logic'
 import { batchGet } from '@/utils/dynamodb'
+import { AllUsersTableItem } from '@/@types/openapi-internal/AllUsersTableItem'
 
 @traceable
 export class UserRepository {
@@ -149,6 +150,7 @@ export class UserRepository {
     params: OptionalPagination<
       DefaultApiGetAllUsersListRequest & { filterUserIds?: string[] }
     >,
+    mapper: (user: InternalUser) => AllUsersTableItem,
     userType?: UserType
   ): Promise<AllUsersListResponse> {
     const db = this.mongoDb.db()
@@ -220,6 +222,7 @@ export class UserRepository {
     const casesCount = await casesCollection
       .aggregate(casesCountPipeline)
       .toArray()
+
     result = {
       ...result,
       items: params.includeCasesCount
@@ -235,14 +238,14 @@ export class UserRepository {
         : result.items,
     }
 
-    if (isPulseEnabled) {
-      result.items = await insertRiskScores(
-        result.items,
-        riskClassificationValues
-      )
-    }
+    const updatedItems = result.items.map((user) =>
+      mapper(user as InternalUser)
+    )
 
-    return result as AllUsersListResponse
+    return {
+      ...result,
+      items: updatedItems,
+    } as AllUsersListResponse
   }
 
   public async getMongoAllUsers(
@@ -1405,7 +1408,8 @@ export class UserRepository {
 
   public async getRuleInstancesTransactionUsersHit(
     ruleInstanceId: string,
-    params: DefaultApiGetRuleInstancesTransactionUsersHitRequest
+    params: DefaultApiGetRuleInstancesTransactionUsersHitRequest,
+    mapper: (user: InternalUser) => AllUsersTableItem
   ): Promise<AllUsersListResponse> {
     const db = this.mongoDb.db()
     const collection = db.collection<InternalTransaction>(
@@ -1498,10 +1502,13 @@ export class UserRepository {
 
     const data = await result.next()
 
-    return this.getMongoUsersCursorsPaginate({
-      ...params,
-      filterUserIds: uniq(data?.userIds.flat()),
-    })
+    return this.getMongoUsersCursorsPaginate(
+      {
+        ...params,
+        filterUserIds: uniq(data?.userIds.flat()),
+      },
+      mapper
+    )
   }
 
   public async getRuleInstanceStats(

phytoplankton-console/src/components/library/Table/standardDataTypes/index.tsx
@@ -24,7 +24,6 @@ import {
   CurrencyCode,
   InternalBusinessUser,
   InternalConsumerUser,
-  KYCStatusDetails,
   RuleAction,
   RuleNature,
   Tag as ApiTag,
@@ -37,6 +36,7 @@ import {
   Alert,
   AlertsQaSampling,
   PepRank,
+  KYCStatus,
 } from '@/apis';
 import { getUserLink, getUserName } from '@/utils/api/users';
 import TransactionTypeDisplay from '@/components/library/TransactionTypeDisplay';
@@ -727,11 +727,11 @@ export const ASSIGNMENTS: ColumnDataType<Assignment[]> = {
   stringify: (value) => `${value?.map((x) => x.assigneeUserId).join(',') ?? ''}`,
 };
 
-export const USER_KYC_STATUS_TAG: ColumnDataType<KYCStatusDetails> = {
-  render: (kycStatusDetails) => {
-    return kycStatusDetails ? <UserKycStatusTag kycStatusDetails={kycStatusDetails} /> : <></>;
+export const USER_KYC_STATUS_TAG: ColumnDataType<KYCStatus> = {
+  render: (kycStatus) => {
+    return kycStatus ? <UserKycStatusTag kycStatusDetails={{ status: kycStatus }} /> : <></>;
   },
-  stringify: (kycStatusDetails) => kycStatusDetails?.status ?? '',
+  stringify: (kycStatus) => kycStatus ?? '',
 };
 
 export const USER_STATE_TAG: ColumnDataType<UserState> = {

phytoplankton-console/src/pages/case-management-item/CaseDetails/CaseTransactionsCard/index.tsx
@@ -28,11 +28,7 @@ export const CaseTransactionsCard = (props: Props) => {
   const { caseTransactionsCount, caseType, caseId, user } = props;
   const [tableParams, setTableParams] = useState<TransactionsTableParams>(DEFAULT_PARAMS_STATE);
   const queryResults = useQuery(
-    TRANSACTIONS_LIST({
-      ...tableParams,
-      filterCaseId: caseId,
-      type: 'case-transactions',
-    }),
+    TRANSACTIONS_LIST({ ...tableParams, filterCaseId: caseId, type: 'case-transactions' }),
     () =>
       api.getCaseTransactions({
         ...tableParams,

phytoplankton-console/src/pages/case-management-item/CaseDetails/InsightsCard/TransactionsList/index.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Params } from '../TransactionsSelector';
 import { FIXED_API_PARAMS } from '..';
 import TransactionsTable, {
+  transactionParamsToRequest,
   TransactionsTableParams,
 } from '@/pages/transactions/components/TransactionsTable';
 import { useApi } from '@/api';
@@ -31,22 +32,17 @@ export default function TransactionsList(props: Props) {
 
   const api = useApi();
   const queryResult = useCursorQuery(
-    TRANSACTIONS_LIST({
-      ...tableParams,
-      ...selectorParams,
-      userId,
-    }),
+    TRANSACTIONS_LIST({ ...tableParams, ...selectorParams, userId }),
 
     async ({ from }) => {
       return await api.getTransactionsList({
         ...FIXED_API_PARAMS,
-        ...tableParams,
+        ...transactionParamsToRequest(tableParams),
         pageSize: tableParams.pageSize,
-        start: from,
+        start: from || tableParams.from,
         filterUserId: userId,
         filterStatus: selectorParams.selectedRuleActions,
         filterTransactionState: selectorParams.selectedTransactionStates,
-        includeEvents: true,
         includeUsers: true,
       });
     },

phytoplankton-console/src/pages/case-management/PaymentApprovalTable/index.tsx
@@ -29,7 +29,7 @@ export default function PaymentApprovalsTable(props: Props) {
     return await api.getTransactionsList({
       ...transactionParamsToRequest(params),
       filterStatus,
-      start: from,
+      start: from || params.from,
     });
   });
 
@@ -63,10 +63,7 @@ export default function PaymentApprovalsTable(props: Props) {
       paginationBorder
       selectionInfo={
         selectedIds.length
-          ? {
-              entityCount: selectedIds.length,
-              entityName: 'transactions',
-            }
+          ? { entityCount: selectedIds.length, entityName: 'transactions' }
           : undefined
       }
       selectionActions={[

phytoplankton-console/src/pages/lists/NewListDrawer/NewValueInput/index.tsx
@@ -5,11 +5,9 @@ import { DefaultOptionType } from 'antd/es/select';
 import { COUNTRIES, COUNTRY_ALIASES } from '@flagright/lib/constants';
 import { Metadata } from '../../helpers';
 import s from './index.module.less';
-import { ListSubtype, TransactionsUniquesField } from '@/apis';
+import { AllUsersTableItem, ListSubtype, TransactionsUniquesField } from '@/apis';
 import Button from '@/components/library/Button';
 import UserSearchPopup from '@/pages/transactions/components/UserSearchPopup';
-import { User } from '@/pages/transactions/components/UserSearchPopup/types';
-import { getUserName } from '@/utils/api/users';
 import { useApi } from '@/api';
 import { useQuery } from '@/utils/queries/hooks';
 import { QueryResult } from '@/utils/queries/types';
@@ -54,16 +52,14 @@ function UserIdInput(props: Omit<Props, 'listSubtype'>) {
   });
 
   const handleChooseUser = useCallback(
-    (user: User) => {
+    (user: AllUsersTableItem) => {
       setNewUserData((state) => ({
         ...state,
         userId: user.userId,
-        userFullName: getUserName(user),
+        userFullName: user.name ?? '',
       }));
       onChange?.([user.userId]);
-      onChangeMeta?.({
-        userFullName: getUserName(user),
-      });
+      onChangeMeta?.({ userFullName: user.name ?? '' });
     },
     [onChangeMeta, onChange],
   );

phytoplankton-console/src/pages/transactions/components/TransactionsTable/DisplayCheckedTransactions/index.tsx
@@ -40,7 +40,7 @@ const DisplayCheckedTransactions = (props: Props) => {
       const hitDirections = props.alert.ruleHitMeta?.hitDirections;
       return await api.getTransactionsList({
         ...transactionParamsToRequest(params),
-        start: from,
+        start: from || params.from,
         ...(hitDirections?.length === 1
           ? {
               filterDestinationUserId: hitDirections?.includes('DESTINATION')
@@ -48,10 +48,7 @@ const DisplayCheckedTransactions = (props: Props) => {
                 : undefined,
               filterOriginUserId: hitDirections?.includes('ORIGIN') ? caseUserId : undefined,
             }
-          : {
-              filterUserId: caseUserId,
-            }),
-
+          : { filterUserId: caseUserId }),
         filterRuleInstancesExecuted: [alert.ruleInstanceId],
       });
     },

commit eb6d295e001a69c3a10f2be1b18afce27c5e3f72

Fix user search (#5584)
If firstName, middleName, lastName has space, the quick search doesn't
work. Fix by matching the entire query string against firstName, middleName, lastName
if quick search is used.
Also fix a regression from d1f2843.

phytoplankton-console/src/pages/transactions/components/UserSearchPopup/PopupContent/UserList/UserItem/index.tsx
@@ -1,9 +1,7 @@
-import React from 'react';
 import { List } from 'antd';
 import cn from 'clsx';
 import pluralize from 'pluralize';
 import s from './style.module.less';
-import { getUserName } from '@/utils/api/users';
 import RiskLevelTag from '@/components/library/Tag/RiskLevelTag';
 import { AllUsersTableItem } from '@/apis';
 
@@ -28,7 +26,7 @@ export default function UserItem(props: Props) {
     >
       <List.Item.Meta
         className={s.meta}
-        title={<span className={s.userName}>{getUserName(user)}</span>}
+        title={<span className={s.userName}>{user.name}</span>}
         description={<span className={s.id}>{user.userId}</span>}
       />
       {user.riskLevel && <RiskLevelTag level={user.riskLevel} />}

tarpon/src/services/users/repositories/user-repository.ts
@@ -326,6 +326,7 @@ export class UserRepository {
       const filterNameConditions: Filter<
         InternalBusinessUser | InternalConsumerUser
       >[] = []
+      // Check if each word in the query has a match in the user's name
       for (const part of params.filterName.split(/\s+/)) {
         const regexFilter = useQuickSearch
           ? prefixRegexMatchFilter(part)
@@ -351,8 +352,30 @@ export class UserRepository {
           ],
         })
       }
-
       filterConditions.push({ $and: filterNameConditions })
+
+      if (useQuickSearch) {
+        // As quick search searchs by prefix, if a user's name part contains space, we need to match it
+        filterConditions.push({
+          $or: [
+            {
+              'userDetails.name.firstName': prefixRegexMatchFilter(
+                params.filterName
+              ),
+            },
+            {
+              'userDetails.name.middleName': prefixRegexMatchFilter(
+                params.filterName
+              ),
+            },
+            {
+              'userDetails.name.lastName': prefixRegexMatchFilter(
+                params.filterName
+              ),
+            },
+          ],
+        })
+      }
     }
 
     if (params.filterUserIds != null) {