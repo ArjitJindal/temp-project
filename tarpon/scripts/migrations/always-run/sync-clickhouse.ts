2c20e8af5d63395fc727fa3abf4c2ac4f8723c17
Aman Dugar <55328714+amandugar@users.noreply.github.com>
2024-12-19 17:12:06 +0530

    Retry Clickhouse Migrations in Case Of Timeout Error (#5626)
    Retry Clickhouse Migrations in Case Of Error

--- tarpon/scripts/migrations/always-run/sync-clickhouse.ts
+++ tarpon/scripts/migrations/always-run/sync-clickhouse.ts
@@ -1,3 +1,4 @@
+import { backOff } from 'exponential-backoff'
 import { migrateAllTenants } from '../utils/tenant'
 import {
   createTenantDatabase,
@@ -7,7 +8,26 @@ import {
 export async function syncClickhouseTables() {
   if (isClickhouseEnabledInRegion()) {
     await migrateAllTenants(async (tenant) => {
-      await createTenantDatabase(tenant.id)
+      // retry 3 times
+      await backOff(() => createTenantDatabase(tenant.id), {
+        retry(e: Error, attemptNumber: number) {
+          if (attemptNumber > 3) {
+            return false
+          }
+
+          if (
+            e.message.toLowerCase().includes('timeout') ||
+            e.message.toLowerCase().includes('zookeeper')
+          ) {
+            return true
+          }
+          return false
+        },
+        numOfAttempts: 3,
+        maxDelay: 10000,
+        timeMultiple: 2,
+        jitter: 'full',
+      })
     })
   }
 }
