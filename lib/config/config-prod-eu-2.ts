e15238552210647f415f9a82e8b64ef4485e4aa7
Aman Dugar <55328714+amandugar@users.noreply.github.com>
2024-12-24 13:10:01 +0530

    Increase Transactions Memory View Lambda EU-2 (#5651)

--- lib/config/config-prod-eu-2.ts
+++ lib/config/config-prod-eu-2.ts
@@ -68,6 +68,7 @@ export const config: Config = {
     },
     TRANSACTIONS_VIEW_LAMBDA: {
       PROVISIONED_CONCURRENCY: 2,
+      MEMORY_SIZE: 1024,
     },
     USERS_VIEW_LAMBDA: {
       PROVISIONED_CONCURRENCY: 2,
