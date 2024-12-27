41ea2f745848635e8710de5c893c0c7fd5e87b38
Arun Kushwaha <arunkushwaha89765@gmail.com>
2024-12-27 13:05:49 +0530

    FIX: fixed demo data creation issue. (#5678)
    * FIX: fixed demo data creation issue.
    
    * FIX: updated logi for handling error

--- tarpon/src/core/seed/index.ts
+++ tarpon/src/core/seed/index.ts
@@ -1,26 +1,36 @@
+import { logger } from '../logger'
 import { seedDynamo } from './dynamodb'
 import { seedMongo } from './mongo'
 import { fetchAndSetAccounts } from './account-setup'
 import { getDynamoDbClient } from '@/utils/dynamodb'
 import { getMongoDbClient } from '@/utils/mongodb-utils'
-import { ClickHouseTables } from '@/utils/clickhouse/definition'
 import {
-  createTenantDatabase,
   getClickhouseClient,
+  createTenantDatabase,
 } from '@/utils/clickhouse/utils'
+import { ClickHouseTables } from '@/utils/clickhouse/definition'
 
 export async function seedDemoData(tenantId: string) {
   const dynamo = getDynamoDbClient()
   const mongoDb = await getMongoDbClient()
   const clickhouseClient = await getClickhouseClient(tenantId)
 
-  await Promise.all(
-    ClickHouseTables.map(async (table) => {
+  const promises = ClickHouseTables.map(async (table) => {
+    try {
       await clickhouseClient.exec({
         query: `DELETE FROM ${table.table} WHERE 1=1`,
       })
-    })
-  )
+    } catch (error) {
+      // error code 60 is returned when the table does not exist
+      if (error instanceof Error && 'code' in error && error.code === 60) {
+        logger.warn(`Table ${table.table} does not exist`)
+      } else {
+        logger.warn(`Failed to delete from table ${table.table}: ${error}`)
+        throw error
+      }
+    }
+  })
+  await Promise.all(promises)
 
   await fetchAndSetAccounts(tenantId, mongoDb)
   await createTenantDatabase(tenantId)
