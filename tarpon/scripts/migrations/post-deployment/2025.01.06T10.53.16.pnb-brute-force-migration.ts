177b1d1fdb6d0f413ad2426f5f56cc70cbcb6bf2
Arun Kushwaha <arunkushwaha89765@gmail.com>
2025-01-06 16:55:09 +0530

    MIGRATION: enabled bruteForceAccountBlocking for pnb (#5728)
    * MIGRATION: enabled bruteForceAccouningBlocking for pnb
    
    * BUG-fix

---
+++ tarpon/scripts/migrations/post-deployment/2025.01.06T10.53.16.pnb-brute-force-migration.ts
@@ -0,0 +1,28 @@
+import { StackConstants } from '@lib/constants'
+import { UpdateCommand, UpdateCommandInput } from '@aws-sdk/lib-dynamodb'
+import { migrateAllTenants } from '../utils/tenant'
+import { getDynamoDbClient } from '@/utils/dynamodb'
+import { Tenant } from '@/services/accounts'
+import { DynamoDbKeys } from '@/core/dynamodb/dynamodb-keys'
+
+async function migrateTenant(tenant: Tenant) {
+  if (tenant.id === 'pnb') {
+    const dynamoDb = getDynamoDbClient()
+    const updateItemInput: UpdateCommandInput = {
+      TableName: StackConstants.TARPON_DYNAMODB_TABLE_NAME(tenant.id),
+      Key: DynamoDbKeys.TENANT_SETTINGS(tenant.id),
+      UpdateExpression: 'SET bruteForceAccountBlockingEnabled = :value',
+      ExpressionAttributeValues: {
+        ':value': true,
+      },
+    }
+    await dynamoDb.send(new UpdateCommand(updateItemInput))
+  }
+}
+
+export const up = async () => {
+  await migrateAllTenants(migrateTenant)
+}
+export const down = async () => {
+  // skip
+}
