43ba5f2ede111eb81c0726d2dea55580f4be45c0
Kumar Amit Sinha <kumar@flagright.com>
2025-01-15 14:41:14 +0530

    Syncing of data with mongo for b4b payments (#5789)
    * migration to clickhouse for b4b
    
    * review suggestions

---
+++ tarpon/scripts/migrations/post-deployment/2025.01.10T08.57.40.migrate-b4b-to-clickhouse.ts
@@ -0,0 +1,50 @@
+import { backOff } from 'exponential-backoff'
+import { syncClickhouseTableWithMongo } from '../utils/clickhouse'
+import { ClickHouseTables } from '@/utils/clickhouse/definition'
+import { getMongoDbClient } from '@/utils/mongodb-utils'
+import { logger } from '@/core/logger'
+
+export const up = async () => {
+  logger.info('Starting MongoDB to Clickhouse sync migration')
+  const mongoClient = await getMongoDbClient()
+
+  const tenantId = '0789ad73b8'
+  // Sync each table with retry logic
+  for (const table of ClickHouseTables) {
+    try {
+      logger.info(
+        `Starting sync for table ${table.table} - Tenant: ${tenantId}`
+      )
+
+      await backOff(
+        () => syncClickhouseTableWithMongo(mongoClient, tenantId, table),
+        {
+          numOfAttempts: 3,
+          maxDelay: 10000,
+          timeMultiple: 2,
+          jitter: 'full',
+          retry(e: Error, attemptNumber: number) {
+            logger.error(
+              `Failed attempt ${attemptNumber} for table ${table.table}:`,
+              e
+            )
+            return attemptNumber <= 3
+          },
+        }
+      )
+
+      logger.info(
+        `Completed sync for table ${table.table} - Tenant: ${tenantId}`
+      )
+    } catch (error) {
+      logger.error(
+        `Failed to sync table ${table.table} for tenant ${tenantId}:`,
+        error
+      )
+      continue
+    }
+  }
+  logger.info('Completed MongoDB to Clickhouse sync migration')
+}
+
+export const down = async () => {}
