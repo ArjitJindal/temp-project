2089bf457d924130196cdcb0045dfaa591855f1b
Kumar Amit Sinha <kumar@flagright.com>
2025-01-24 14:38:43 +0530

    Wrap AssigneesDropdown into an ErrorBoundary (#5891)
    * Wrap AssigneesDropdown into an ErrorBoundary
    
    * suggested changes

--- phytoplankton-console/src/pages/case-management/components/AssigneesDropdown/index.tsx
+++ phytoplankton-console/src/pages/case-management/components/AssigneesDropdown/index.tsx
@@ -8,6 +8,7 @@ import { useHasPermissions, useSortedUsers } from '@/utils/user-utils';
 import { Account, Assignment, Permission } from '@/apis';
 import AccountTag from '@/components/AccountTag';
 import ArrowDropDownFill from '@/components/ui/icons/Remix/system/arrow-drop-down-fill.react.svg';
+import ErrorBoundary from '@/components/utils/ErrorBoundary';
 
 interface Props {
   editing: boolean;
@@ -20,7 +21,7 @@ interface Props {
   requiredPermissions?: Permission[];
 }
 
-export const AssigneesDropdown: React.FC<Props> = ({
+const AssigneesDropdownContent: React.FC<Props> = ({
   assignments,
   editing,
   onChange,
@@ -41,75 +42,81 @@ export const AssigneesDropdown: React.FC<Props> = ({
     return users.filter((user) => customFilter(user));
   }, [users, customFilter]);
   return editing && canEditAssignees ? (
-    <>
-      <Select<string[]>
-        open={maxAssignees && assignments.length >= maxAssignees ? false : undefined}
-        className={
-          cn(s.select, fixSelectorHeight ? s.fixSelectorHeight : '') +
-          (assignments.length === 0 ? ' unassigned ' : '')
+    <Select<string[]>
+      open={maxAssignees && assignments.length >= maxAssignees ? false : undefined}
+      className={
+        cn(s.select, fixSelectorHeight ? s.fixSelectorHeight : '') +
+        (assignments.length === 0 ? ' unassigned ' : '')
+      }
+      mode={'multiple'}
+      allowClear
+      filterOption={(input, option) => {
+        if (!input || !option) {
+          return false;
         }
-        mode={'multiple'}
-        allowClear
-        filterOption={(input, option) => {
-          if (!input || !option) {
-            return false;
-          }
 
-          const accountId = option.value;
-          const selectedUser = users.find((user) => user.id === accountId);
+        const accountId = option.value;
+        const selectedUser = users.find((user) => user.id === accountId);
 
-          if (!selectedUser) {
-            return false;
-          }
-
-          return selectedUser.name.includes(input) || selectedUser.email.includes(input);
-        }}
-        style={{ width: '100%' }}
-        disabled={loadingUsers}
-        placeholder={
-          loadingUsers ? (
-            <>
-              <LoadingOutlined /> Loading...
-            </>
-          ) : (
-            placeholder ?? 'Unassigned'
-          )
+        if (!selectedUser) {
+          return false;
         }
-        onChange={onChange}
-        value={
-          loadingUsers
-            ? []
-            : assignments
-                .filter((assignment) => assignment.assigneeUserId !== undefined)
-                .map((assignment) => assignment.assigneeUserId)
-        }
-        suffixIcon={<ArrowDropDownFill />}
-      >
-        {filteredUsers.map((user, index) => (
-          <Select.Option key={user.id}>
-            <div className={s.item}>
-              <Avatar
-                size="small"
-                className={s.avatar}
-                style={{
-                  color: colorSchema[index % 4].text,
-                  backgroundColor: colorSchema[index % 4].background,
-                }}
-              >
-                {' '}
-                {user.email.toUpperCase().charAt(0)}
-              </Avatar>
-              <span>{user.name}</span>
-            </div>
-          </Select.Option>
-        ))}
-      </Select>
-    </>
+
+        return selectedUser.name.includes(input) || selectedUser.email.includes(input);
+      }}
+      style={{ width: '100%' }}
+      disabled={loadingUsers}
+      placeholder={
+        loadingUsers ? (
+          <>
+            <LoadingOutlined /> Loading...
+          </>
+        ) : (
+          placeholder ?? 'Unassigned'
+        )
+      }
+      onChange={onChange}
+      value={
+        loadingUsers
+          ? []
+          : assignments
+              .filter((assignment) => assignment.assigneeUserId !== undefined)
+              .map((assignment) => assignment.assigneeUserId)
+      }
+      suffixIcon={<ArrowDropDownFill />}
+    >
+      {filteredUsers.map((user, index) => (
+        <Select.Option key={user.id}>
+          <div className={s.item}>
+            <Avatar
+              size="small"
+              className={s.avatar}
+              style={{
+                color: colorSchema[index % 4].text,
+                backgroundColor: colorSchema[index % 4].background,
+              }}
+            >
+              {' '}
+              {user.email.toUpperCase().charAt(0)}
+            </Avatar>
+            <span>{user.name}</span>
+          </div>
+        </Select.Option>
+      ))}
+    </Select>
   ) : (
-    <>
+    <div className={s.assigneesList}>
       {assignments?.map((assignment) => (
         <AccountTag key={assignment.assigneeUserId} accountId={assignment.assigneeUserId} />
       ))}
-    </>
+    </div>
+  );
+};
+
+export const AssigneesDropdown: React.FC<Props> = (props) => {
+  return (
+    <ErrorBoundary>
+      <AssigneesDropdownContent {...props} />
+    </ErrorBoundary>
   );
 };
