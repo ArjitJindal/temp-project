b9088fbe3c4d3d7cb5120201934a2a507e81cabd
Samarjeet Mohite <80679439+Smr0303@users.noreply.github.com>
2025-01-31 15:10:28 +0530

    [UPDATE]: Added attachements to nango schema (#6002)

--- nango-integrations/freshdesk/syncs/freshdesk_tickets.ts
+++ nango-integrations/freshdesk/syncs/freshdesk_tickets.ts
@@ -4,7 +4,6 @@ import type {
   ProxyConfiguration,
 } from '../../models'
 
-// TODO: Include Attachments
 export interface FreshdeskApiTicket {
   cc_emails: string[]
   company_id: number
@@ -46,6 +45,7 @@ export interface FreshdeskApiTicket {
   description: string | null
   description_text: string | null
   conversations: Conversation[] | null
+  attachments: Attachments[] | null
 }
 
 export interface Conversation {
@@ -55,6 +55,15 @@ export interface Conversation {
   to_email: string | null
   created_at: number | null
   updated_at: number | null
+}
+
+export interface Attachments {
+  content_type: string
+  size: number
+  updated_at: string
+  name: string
+  id: number
+  attachment_url: string
 }
 
 export default async function fetchData(nango: NangoSync): Promise<void> {
@@ -117,6 +126,15 @@ export default async function fetchData(nango: NangoSync): Promise<void> {
                 conversation.updated_at !== null
                   ? new Date(conversation.updated_at).valueOf()
                   : null,
+            })) || [],
+          attachments:
+            ticketData.attachments?.map((attachment) => ({
+              content_type: attachment.content_type,
+              size: attachment.size,
+              updated_at: attachment.updated_at,
+              name: attachment.name,
+              id: attachment.id,
+              attachment_url: attachment.attachment_url,
             })) || [],
         }
 
