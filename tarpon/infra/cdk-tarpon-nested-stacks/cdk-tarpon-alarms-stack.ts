e72ca76025bbe57da76763457e5aec00d56b50f7
Jayant Patil <72199264+patiljayant@users.noreply.github.com>
2025-02-05 18:42:36 +0530

    Increase dynamo write capacity alram (#6080)
    Increase dynamo alram

--- tarpon/infra/cdk-tarpon-nested-stacks/cdk-tarpon-alarms-stack.ts
+++ tarpon/infra/cdk-tarpon-nested-stacks/cdk-tarpon-alarms-stack.ts
@@ -207,7 +207,7 @@ export class CdkTarponAlarmsStack extends cdk.NestedStack {
                 period: Duration.minutes(1),
               }
             : {
-                threshold: 50,
+                threshold: 100,
                 statistic: 'Average',
                 period: Duration.minutes(5),
               }
