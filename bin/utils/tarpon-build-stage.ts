9dc4014d86df1860b8d3c6a1aca58daafdf47c34
Aman Dugar <55328714+amandugar@users.noreply.github.com>
2025-03-28 12:29:09 +0530

    Update Orca Pipeline to Increase memory (#6527)

--- bin/utils/tarpon-build-stage.ts
+++ bin/utils/tarpon-build-stage.ts
@@ -27,6 +27,7 @@ export const buildTarpon = (scope: Construct, role: iam.IRole) => {
         build: {
           commands: [
             ...installTerraform,
+            'NODE_OPTIONS="--max_old_space_size=8192"',
             'yarn build',
             ...getSentryReleaseSpec(true).commands,
           ],
