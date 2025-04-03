665fb8da51a2e91c50659fd8db1732e603ba913b
Kumar Amit Sinha <kumar@flagright.com>
2025-04-03 18:49:50 +0530

    Revert "Rule Id added with rule Instance for PNB" (#6563)
    Revert "Rule Id added with rule Instance for PNB (#6561)"
    
    This reverts commit 1f1f6a9d49879acf2d777967d8e6ab7a0f3f5f4e.

--- tarpon/src/services/rules-engine/pnb-custom-logic.ts
+++ tarpon/src/services/rules-engine/pnb-custom-logic.ts
@@ -18,7 +18,6 @@ import { InternalUser } from '@/@types/openapi-internal/InternalUser'
 export const PNB_INTERNAL_RULES: RuleInstance[] = [
   {
     id: 'pnb-internal-trigger-incomplete-risk-levels',
-    ruleId: 'pnb-internal-trigger-incomplete-risk-levels',
     type: 'USER',
     ruleNameAlias: 'PNB - Trigger to return incomplete risk levels',
     ruleDescriptionAlias: 'Trigger to return incomplete risk levels',
@@ -286,7 +285,6 @@ export const PNB_INTERNAL_RULES: RuleInstance[] = [
   },
   {
     id: 'pnb-internal-trigger-incomplete-risk-levels-origin-user',
-    ruleId: 'pnb-internal-trigger-incomplete-risk-levels-origin-user',
     type: 'TRANSACTION',
     ruleNameAlias: 'PNB - Trigger to return incomplete risk levels origin user',
     ruleDescriptionAlias:
@@ -638,7 +636,6 @@ export const PNB_INTERNAL_RULES: RuleInstance[] = [
   },
   {
     id: 'pnb-internal-trigger-incomplete-risk-levels-destination-user',
-    ruleId: 'pnb-internal-trigger-incomplete-risk-levels-destination-user',
     type: 'TRANSACTION',
     ruleNameAlias:
       'PNB - Trigger to return incomplete risk levels destination user',
@@ -990,7 +987,6 @@ export const PNB_INTERNAL_RULES: RuleInstance[] = [
   },
   {
     id: 'pnb-internal-trigger-complete-risk-levels-origin-user',
-    ruleId: 'pnb-internal-trigger-complete-risk-levels-origin-user',
     type: 'TRANSACTION',
     ruleNameAlias: 'PNB - Trigger to return complete risk levels origin user',
     ruleDescriptionAlias: 'Trigger to return complete risk levels origin user',
@@ -1207,7 +1203,6 @@ export const PNB_INTERNAL_RULES: RuleInstance[] = [
   },
   {
     id: 'pnb-internal-trigger-complete-risk-levels-destination-user',
-    ruleId: 'pnb-internal-trigger-complete-risk-levels-destination-user',
     type: 'TRANSACTION',
     ruleNameAlias:
       'PNB - Trigger to return complete risk levels destination user',
