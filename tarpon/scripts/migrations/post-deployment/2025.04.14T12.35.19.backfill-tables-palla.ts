e85f0d2817141c521110d479aff72770a25ad6e3
Sohan Bandary <73984453+sohan2410@users.noreply.github.com>
2025-04-16 15:25:44 +0530

    Backfill ClickHouse data for Palla (#6663)
    * Backfill ClickHouse data for Palla
    
    * added region check
    
    * Update tarpon/scripts/migrations/post-deployment/2025.04.14T12.35.19.backfill-tables-palla.ts

---
+++ tarpon/scripts/migrations/post-deployment/2025.04.14T12.35.19.backfill-tables-palla.ts
@@ -0,0 +1,33 @@
+import { stageAndRegion } from '@flagright/lib/utils/env'
+import { sendBatchJobCommand } from '@/services/batch-jobs/batch-job'
+import { ClickhouseTableNames } from '@/utils/clickhouse/definition'
+import { envIsNot } from '@/utils/env'
+export const up = async () => {
+  const [_, region] = stageAndRegion()
+  if (envIsNot('prod') || region !== 'us-1') {
+    return
+  }
+  await sendBatchJobCommand({
+    type: 'CLICKHOUSE_DATA_BACKFILL',
+    tenantId: 'e5eb4e1664',
+    parameters: {
+      referenceId: '0',
+      tableNames: [
+        ClickhouseTableNames.Transactions,
+        ClickhouseTableNames.Users,
+        ClickhouseTableNames.TransactionEvents,
+        ClickhouseTableNames.UserEvents,
+        ClickhouseTableNames.Cases,
+        ClickhouseTableNames.Reports,
+        ClickhouseTableNames.KrsScore,
+        ClickhouseTableNames.DrsScore,
+        ClickhouseTableNames.ArsScore,
+        ClickhouseTableNames.CrmRecords,
+        ClickhouseTableNames.CrmUserRecordLink,
+      ],
+    },
+  })
+}
+export const down = async () => {
+  // skip
+}
