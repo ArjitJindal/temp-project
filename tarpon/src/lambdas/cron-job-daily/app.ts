108ee4f4a5b7cf18b780d85630a976653d63c597
Aman Dugar <55328714+amandugar@users.noreply.github.com>
2025-04-21 14:32:08 +0530

    Build Slack Alerts Based on Transaction Deviation of Transactions (#6737)
    * Build Slack Alerts Based on Transaction Deviation of Transactions
    
    * Build Slack Alerts Based on Transaction Deviation of Transactions

--- tarpon/src/lambdas/cron-job-daily/app.ts
+++ tarpon/src/lambdas/cron-job-daily/app.ts
@@ -5,6 +5,7 @@ import { isQaEnv } from '@flagright/lib/qa'
 import { WebClient } from '@slack/web-api'
 import axios from 'axios'
 import { DynamoDBClient } from '@aws-sdk/client-dynamodb'
+import slackify from 'slackify-markdown'
 import { sendCaseCreatedAlert } from '../slack-app/app'
 import { lambdaConsumer } from '@/core/middlewares/lambda-consumer-middlewares'
 import { TenantInfo, TenantService } from '@/services/tenants'
@@ -22,7 +23,10 @@ import {
   getTenantSpecificProviders,
   isSanctionsDataFetchTenantSpecific,
 } from '@/services/sanctions/utils'
-import { TRIAGE_QUEUE_TICKETS_COLLECTION } from '@/utils/mongodb-definitions'
+import {
+  TRANSACTIONS_COLLECTION,
+  TRIAGE_QUEUE_TICKETS_COLLECTION,
+} from '@/utils/mongodb-definitions'
 import { getMongoDbClient } from '@/utils/mongodb-utils'
 import { TriageQueueTicket } from '@/@types/triage'
 import { getSecret } from '@/utils/secrets-manager'
@@ -34,6 +38,12 @@ import {
 } from '@/utils/slack'
 import { BatchJob } from '@/@types/batch-job'
 import { FLAGRIGHT_TENANT_ID } from '@/core/constants'
+import {
+  executeClickhouseQuery,
+  isClickhouseEnabledInRegion,
+} from '@/utils/clickhouse/utils'
+import { isDemoTenant } from '@/utils/tenant'
+import { InternalTransaction } from '@/@types/openapi-internal/InternalTransaction'
 
 export const cronJobDailyHandler = lambdaConsumer()(async () => {
   const dynamoDb = getDynamoDbClient()
@@ -194,6 +204,28 @@ export const cronJobDailyHandler = lambdaConsumer()(async () => {
     })
   )
 
+  try {
+    if (!envIs('prod')) {
+      return
+    }
+
+    const diffPercentageMap = new Map<
+      string,
+      { monthly?: number; daily?: number }
+    >()
+    for (const tenant of tenantInfos) {
+      await transactionsDeviationAlert(tenant.tenant.id, diffPercentageMap)
+    }
+    if (diffPercentageMap.size > 0) {
+      await sendTransactionsDeviationAlert(diffPercentageMap, tenantInfos)
+    }
+  } catch (e) {
+    logger.error(
+      `Failed to check transactions deviation: ${(e as Error)?.message}`,
+      e
+    )
+  }
+
   if (envIs('dev')) {
     await sendBatchJobCommand({
       type: 'QA_CLEANUP',
@@ -345,4 +377,168 @@ export async function updateOncallUsers() {
       })
     }
   }
+}
+
+const getClickhouseTransactionsCount = async (
+  tenantId: string,
+  timestamps: { start: number; end: number }
+) => {
+  const transactions = await executeClickhouseQuery<{ count: number }>(
+    tenantId,
+    `SELECT count() as count FROM transactions
+    WHERE timestamp >= ${timestamps.start} AND timestamp >= ${timestamps.end}
+  `
+  )
+  return transactions[0].count
+}
+
+const getMongoTransactionsCount = async (
+  collection: any,
+  start: number,
+  end: number
+) => {
+  return collection.countDocuments({
+    createdAt: { $gte: start, $lte: end },
+  })
+}
+
+const calculatePercentage = (
+  count1: number,
+  count2: number,
+  threshold: number
+) => {
+  if (count1 < count2 * threshold) {
+    return (count1 / count2) * 100
+  }
+  return undefined
+}
+
+export async function transactionsDeviationAlert(
+  tenantId: string,
+  diffPercentageMap: Map<string, { monthly?: number; daily?: number }>
+) {
+  if (isDemoTenant(tenantId)) {
+    return
+  }
+  let dailyPercentage: number | undefined
+  let monthlyPercentage: number | undefined
+
+  const now = dayjs().valueOf()
+  const oneDayAgo = dayjs().subtract(1, 'day').valueOf()
+  const twoDaysAgo = dayjs().subtract(2, 'day').valueOf()
+  const oneMonthAgo = dayjs().subtract(1, 'month').valueOf()
+  const twoMonthsAgo = dayjs().subtract(2, 'month').valueOf()
+
+  if (isClickhouseEnabledInRegion()) {
+    const monthCount1 = await getClickhouseTransactionsCount(tenantId, {
+      start: twoMonthsAgo,
+      end: oneMonthAgo,
+    })
+    const monthCount2 = await getClickhouseTransactionsCount(tenantId, {
+      start: oneMonthAgo,
+      end: now,
+    })
+    monthlyPercentage = calculatePercentage(monthCount1, monthCount2, 0.8)
+
+    const dayCount1 = await getClickhouseTransactionsCount(tenantId, {
+      start: twoDaysAgo,
+      end: oneDayAgo,
+    })
+    const dayCount2 = await getClickhouseTransactionsCount(tenantId, {
+      start: oneDayAgo,
+      end: now,
+    })
+    dailyPercentage = calculatePercentage(dayCount1, dayCount2, 0.7)
+  } else {
+    const mongoDb = await getMongoDbClient()
+    const transactionsCollection = mongoDb
+      .db()
+      .collection<InternalTransaction>(TRANSACTIONS_COLLECTION(tenantId))
+
+    const monthCount1 = await getMongoTransactionsCount(
+      transactionsCollection,
+      twoMonthsAgo,
+      now
+    )
+    const monthCount2 = await getMongoTransactionsCount(
+      transactionsCollection,
+      oneMonthAgo,
+      now
+    )
+    monthlyPercentage = calculatePercentage(monthCount1, monthCount2, 0.8)
+
+    const dayCount1 = await getMongoTransactionsCount(
+      transactionsCollection,
+      twoDaysAgo,
+      oneDayAgo
+    )
+    const dayCount2 = await getMongoTransactionsCount(
+      transactionsCollection,
+      oneDayAgo,
+      now
+    )
+    dailyPercentage = calculatePercentage(dayCount1, dayCount2, 0.7)
+  }
+
+  if (monthlyPercentage || dailyPercentage) {
+    diffPercentageMap.set(tenantId, {
+      ...(monthlyPercentage && { monthly: monthlyPercentage }),
+      ...(dailyPercentage && { daily: dailyPercentage }),
+    })
+  }
+}
+
+async function sendTransactionsDeviationAlert(
+  diffPercentageMap: Map<string, { monthly?: number; daily?: number }>,
+  tenants: TenantInfo[]
+) {
+  const slack = await getSecret<{ token: string }>('slackCreds')
+  const slackClient = new WebClient(slack.token)
+
+  const slackMessage = Array.from(diffPercentageMap.entries())
+    .filter(([tenantId]) => {
+      const tenant = tenants.find((tenant) => tenant.tenant.id === tenantId)
+      return !tenant?.tenant.name.toLowerCase().includes('flagright')
+    })
+    .map(([tenantId]) => {
+      const tenant = tenants.find((tenant) => tenant.tenant.id === tenantId)
+      return `Tenant ${tenantId}
+        ${tenant?.tenant.name} (${tenantId})
+        ${
+          diffPercentageMap.get(tenantId)?.monthly
+            ? `${diffPercentageMap.get(tenantId)?.monthly}% monthly`
+            : ''
+        } ${
+        diffPercentageMap.get(tenantId)?.daily
+          ? `${diffPercentageMap.get(tenantId)?.daily}% daily`
+          : ''
+      } deviation`
+    })
+    .join('\n')
+
+  if (diffPercentageMap.size === 0) {
+    return
+  }
+
+  await slackClient.chat.postMessage({
+    channel: INCIDENTS_BUGS_CHANNEL_ID,
+    blocks: [
+      {
+        type: 'header',
+        text: { type: 'plain_text', text: 'Transactions Deviation Alert' },
+      },
+      { type: 'divider' },
+      {
+        type: 'section',
+        text: {
+          type: 'mrkdwn',
+          text: `Hey <!subteam^${CUSTOMER_ON_CALL_GROUP_ID}> we have detected a deviation in the transactions for the following tenants:`,
+        },
+      },
+      {
+        type: 'section',
+        text: { type: 'mrkdwn', text: slackify(slackMessage) },
+      },
+    ],
+  })
 }
