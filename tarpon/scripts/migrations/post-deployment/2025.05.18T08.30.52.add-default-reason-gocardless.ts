945c22b7c1b1b280a368164cb248233f703250e6
Arun Kushwaha <arunkushwaha89765@gmail.com>
2025-05-19 12:10:13 +0530

    Migration for adding default reason for gocardlesspoc (#6939)

---
+++ tarpon/scripts/migrations/post-deployment/2025.05.18T08.30.52.add-default-reason-gocardless.ts
@@ -0,0 +1,21 @@
+import { migrateAllTenants } from '../utils/tenant'
+import { getMongoDbClient } from '@/utils/mongodb-utils'
+import { Tenant } from '@/services/accounts/repository'
+import { ReasonsService } from '@/services/tenants/reasons-service'
+import { logger } from '@/core/logger'
+
+async function migrateTenant(tenant: Tenant) {
+  if (tenant.id === '46fe6f381c') {
+    const mongoDb = await getMongoDbClient()
+    const reasonsService = new ReasonsService(tenant.id, mongoDb)
+    await reasonsService.initialiseDefaultReasons()
+    logger.info(`Stored default reasons for tenant ${tenant.name} `)
+  }
+}
+
+export const up = async () => {
+  await migrateAllTenants(migrateTenant)
+}
+export const down = async () => {
+  // skip
+}
