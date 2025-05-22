17d367666f13206985af65d314db02ea7eb99a59
Aman Dugar <55328714+amandugar@users.noreply.github.com>
2025-05-22 18:24:19 +0530

    Fix Dev Deploy OpenApi Augment (#6991)
    Fix Dev Deploy
    
    Co-authored-by: Stefano Balzan <stefano@flagright.com>

--- tarpon/lib/openapi/openapi-internal-augmentor.ts
+++ tarpon/lib/openapi/openapi-internal-augmentor.ts
@@ -326,6 +326,7 @@ export const ConsoleApiPathToLambda: any = {
     StackConstants.CONSOLE_API_WORKFLOWS_CASE_FUNCTION_NAME,
   '/workflows/{workflowType}/{workflowId}/{version}':
     StackConstants.CONSOLE_API_WORKFLOWS_CASE_FUNCTION_NAME,
+  '/flat-files/format': StackConstants.CONSOLE_API_FLAT_FILES_FUNCTION_NAME,
   // TODO: rename CONSOLE_API_WORKFLOWS_CASE_FUNCTION_NAME to make it generic
 }
 
