3752f5530cc747655cbd3d1426de1a3d4298b712
Arun Kushwaha <arunkushwaha89765@gmail.com>
2025-08-13 10:08:06 +0530

    FIX: refetch creds before cdktf deploy (#7645)
    * FIX: refetch creds before cdktf deploy
    
    * Throwed error if cred fetch failed

--- tarpon/scripts/deploy-cdktf.ts
+++ tarpon/scripts/deploy-cdktf.ts
@@ -1,5 +1,6 @@
 import { exit } from 'process'
 import { execSync } from 'child_process'
+import { AssumeRoleCommand, STS } from '@aws-sdk/client-sts'
 
 const main = async () => {
   const env = process.argv[2]
@@ -15,8 +16,35 @@ Example:
 Defaulting to 'dev' stage and 'eu-1' region...
           `)
   }
-
   const selectedEnv = env || 'dev:eu-1'
+  const [_, region] = selectedEnv.split(':')
+
+  try {
+    console.info(
+      `Refreshing AWS credentials for cdKTF deployment in ${selectedEnv}`
+    )
+    const sts = new STS({
+      region: process.env.AWS_REGION,
+    })
+
+    const assumeRoleCommand = new AssumeRoleCommand({
+      RoleArn: process.env.ASSUME_ROLE_ARN as string,
+      RoleSessionName: `deploy-${region}`,
+    })
+
+    const assumeRoleResult = await sts.send(assumeRoleCommand)
+
+    process.env.AWS_ACCESS_KEY_ID = assumeRoleResult.Credentials?.AccessKeyId
+    process.env.AWS_SECRET_ACCESS_KEY =
+      assumeRoleResult.Credentials?.SecretAccessKey
+    process.env.AWS_SESSION_TOKEN = assumeRoleResult.Credentials?.SessionToken
+    console.info('Refreshed AWS credentials')
+  } catch (e) {
+    console.error('Failed to refresh AWS credentials')
+    console.error(e)
+    throw e
+  }
+
   const command = `ENV='${selectedEnv}' ASSUME_ROLE_ARN='' cdktf deploy --quiet --auto-approve`
 
   execSync(command, { stdio: 'inherit' })
