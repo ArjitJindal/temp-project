749ba9e1030e60dd00f034ef89269f246005b49b
Jayant Patil <72199264+patiljayant@users.noreply.github.com>
2025-09-04 18:58:57 +0530

    Add migration for comply settings removal and disable R-16 (#7845)

---
+++ tarpon/scripts/migrations/post-deployment/2025.09.04T12.38.09.remove-comply-advantage-settings.ts
@@ -0,0 +1,47 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+// @ts-nocheck
+import { migrateAllTenants } from '../utils/tenant'
+import { getDynamoDbClient } from '@/utils/dynamodb'
+import { Tenant } from '@/services/accounts/repository'
+import { TenantRepository } from '@/services/tenants/repositories/tenant-repository'
+import { getMongoDbClient } from '@/utils/mongodb-utils'
+
+async function migrateTenant(tenant: Tenant) {
+  const dynamoDb = getDynamoDbClient()
+  const mongoDb = await getMongoDbClient()
+  const tenantRepository = new TenantRepository(tenant.id, {
+    dynamoDb,
+    mongoDb,
+  })
+  const { sanctions } = await tenantRepository.getTenantSettings(['sanctions'])
+  if (!sanctions) {
+    return
+  }
+  if (sanctions?.marketType) {
+    delete sanctions.marketType
+  }
+  if (sanctions.customSearchProfileId) {
+    delete sanctions.customSearchProfileId
+  }
+  if (sanctions.customInitialSearchProfileId) {
+    delete sanctions.customInitialSearchProfileId
+  }
+  if (sanctions?.providerScreeningTypes) {
+    sanctions.providerScreeningTypes = sanctions.providerScreeningTypes?.filter(
+      (type) => {
+        if (type.provider === 'comply-advantage') {
+          return false
+        }
+        return true
+      }
+    )
+  }
+  await tenantRepository.createOrUpdateTenantSettings({ sanctions })
+}
+
+export const up = async () => {
+  await migrateAllTenants(migrateTenant)
+}
+export const down = async () => {
+  // skip
+}
---
+++ tarpon/scripts/migrations/post-deployment/2025.09.04T12.54.30.disable-deleted-r-16-rule.ts
@@ -0,0 +1,31 @@
+import { migrateAllTenants } from '../utils/tenant'
+import { getDynamoDbClient } from '@/utils/dynamodb'
+import { Tenant } from '@/services/accounts/repository'
+import { RuleInstanceRepository } from '@/services/rules-engine/repositories/rule-instance-repository'
+
+async function migrateTenant(tenant: Tenant) {
+  const dynamoDb = getDynamoDbClient()
+  const rulesRepository = new RuleInstanceRepository(tenant.id, {
+    dynamoDb,
+  })
+  const activeRuleInstances = await rulesRepository.getActiveRuleInstances()
+  const deletedRuleInstances = activeRuleInstances.filter(
+    (ruleInstance) => ruleInstance.ruleId === 'R-16'
+  )
+  for (const ruleInstance of deletedRuleInstances) {
+    await rulesRepository.createOrUpdateRuleInstance(
+      {
+        ...ruleInstance,
+        status: 'INACTIVE',
+      },
+      Date.now()
+    )
+  }
+}
+
+export const up = async () => {
+  await migrateAllTenants(migrateTenant)
+}
+export const down = async () => {
+  // skip
+}
