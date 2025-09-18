6f94dd29cceaaeba2402a7fc5a295613814b6ea2
Vedansh Mishra <139506945+Vedansh00Mishra@users.noreply.github.com>
2025-09-18 15:13:02 +0530

    Added whitelist examples in demo data (#7927)
    Make whitelists demo data

--- tarpon/src/core/seed/data/lists.ts
+++ tarpon/src/core/seed/data/lists.ts
@@ -12,31 +12,54 @@ type ListTypeObject = {
 }
 
 const data: () => ListTypeObject[] = memoize(() => {
-  return (
-    [
-      'USER_ID',
-      'CARD_FINGERPRINT_NUMBER',
-      'IBAN_NUMBER',
-      'BANK_ACCOUNT_NUMBER',
-      'ACH_ACCOUNT_NUMBER',
-      'SWIFT_ACCOUNT_NUMBER',
-      'BIC',
-      'BANK_SWIFT_CODE',
-      'UPI_IDENTIFYING_NUMBER',
-      'IP_ADDRESS',
-    ] as const
-  ).map((subtype, i) => {
+  const blacklistSubtypes = [
+    'USER_ID',
+    'CARD_FINGERPRINT_NUMBER',
+    'IBAN_NUMBER',
+    'BANK_ACCOUNT_NUMBER',
+    'ACH_ACCOUNT_NUMBER',
+    'SWIFT_ACCOUNT_NUMBER',
+    'BIC',
+    'BANK_SWIFT_CODE',
+    'UPI_IDENTIFYING_NUMBER',
+    'IP_ADDRESS',
+  ] as const
+
+  const whitelistSubtypes = [
+    'USER_ID',
+    'CARD_FINGERPRINT_NUMBER',
+    'IBAN_NUMBER',
+    'BANK_ACCOUNT_NUMBER',
+    'IP_ADDRESS',
+  ] as const
+
+  const blacklists = blacklistSubtypes.map((subtype, i) => {
     return {
       listId: generateChecksum(i).substring(0, 10),
-      listType: 'BLACKLIST',
+      listType: 'BLACKLIST' as const,
       subtype,
       data: {
         metadata: {
-          name: `"${subtype}" list`,
+          name: `"${subtype}" blacklist`,
         },
       },
     }
   })
+
+  const whitelists = whitelistSubtypes.map((subtype, i) => {
+    return {
+      listId: generateChecksum(i + 100).substring(0, 10), // Use different range to avoid ID conflicts
+      listType: 'WHITELIST' as const,
+      subtype,
+      data: {
+        metadata: {
+          name: `"${subtype}" whitelist`,
+        },
+      },
+    }
+  })
+
+  return [...blacklists, ...whitelists]
 })
 
 export { data }
