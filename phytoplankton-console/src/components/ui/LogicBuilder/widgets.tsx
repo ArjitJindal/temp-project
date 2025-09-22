8b8d1e51ace299b32458ee2152a443bac94983ad
ArjitJindal <49954613+ArjitJindal@users.noreply.github.com>
2025-09-22 19:50:28 +0530

    Fix: Tag values can't be inputted (#7931)
    * Fix: update value handling in customTextWidget to reset on force update trigger
    
    * Refactor: simplify value reset logic in customTextWidget on force update trigger
    
    * Fix: Update dynamic value handling in custom text widget
    
    * Refactor logic to determine current value based on tag key value
    * Ensure value is set to undefined when key value is not present

--- phytoplankton-console/src/components/ui/LogicBuilder/widgets.tsx
+++ phytoplankton-console/src/components/ui/LogicBuilder/widgets.tsx
@@ -327,8 +327,12 @@ const customTextWidget: CoreWidgets['text'] = {
         if (entityId) {
           return (
             <DynamicValueFieldWrapper entityId={entityId}>
-              {(dynamicKeyFilter, forceUpdateTrigger) => {
-                const currentValue = forceUpdateTrigger > 0 ? undefined : props.value;
+              {(dynamicKeyFilter) => {
+                const keyValue = getTagKeyValue(entityId);
+                const currentValue = !keyValue ? undefined : props.value;
+                if (!keyValue) {
+                  props.setValue(undefined);
+                }
 
                 if (isArrayType) {
                   return (
