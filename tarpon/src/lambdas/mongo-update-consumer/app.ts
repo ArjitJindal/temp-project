22de19b034a799d0079eadec5245f059efe84064
Kavish <56078568+kavish6@users.noreply.github.com>
2025-09-22 20:58:00 +0530

    Handle duplicate error and create new sanctionDetailsId (#8013)
    handle duplicate error and create new sanctionDetailsId

--- tarpon/src/lambdas/mongo-update-consumer/app.ts
+++ tarpon/src/lambdas/mongo-update-consumer/app.ts
@@ -1,11 +1,13 @@
 import { SQSEvent } from 'aws-lambda'
-import { Document } from 'mongodb'
+import { Document, ModifyResult } from 'mongodb'
 import pMap from 'p-map'
 import { lambdaConsumer } from '@/core/middlewares/lambda-consumer-middlewares'
 import { getMongoDbClient } from '@/utils/mongodb-utils'
 import { MongoUpdateMessage } from '@/@types/mongo'
 import { sendMessageToMongoConsumer } from '@/utils/clickhouse/utils'
 import { logger } from '@/core/logger'
+import { CounterRepository } from '@/services/counter/repository'
+import { getDynamoDbClient } from '@/utils/dynamodb'
 
 export const mongoUpdateConsumerHandler = lambdaConsumer()(
   async (event: SQSEvent) => {
@@ -20,21 +22,56 @@ export const mongoUpdateConsumerHandler = lambdaConsumer()(
 export const executeMongoUpdate = async (events: MongoUpdateMessage[]) => {
   const mongoClient = await getMongoDbClient()
   const db = mongoClient.db()
-
   await pMap(
     events,
     async (event) => {
       const collection = db.collection<Document>(event.collectionName)
-      const data = await collection.findOneAndUpdate(
-        event.filter,
-        event.updateMessage,
-        {
-          upsert: event.upsert || false,
-          ...(event.arrayFilters ? { arrayFilters: event.arrayFilters } : {}),
-          returnDocument: 'after',
+      const idDetails = event.updateMessage.$setOnInsert
+      let screeningId = idDetails?.screeningId
+      const tenantId = event.collectionName.split('-')[0]
+      let data: ModifyResult<Document> | undefined = undefined
+      try {
+        data = await collection.findOneAndUpdate(
+          event.filter,
+          event.updateMessage,
+          {
+            upsert: event.upsert || false,
+            ...(event.arrayFilters ? { arrayFilters: event.arrayFilters } : {}),
+            returnDocument: 'after',
+          }
+        )
+      } catch (e) {
+        if (
+          (e as any).code === 11000 && //  Duplicate key error from mongoDB
+          screeningId
+        ) {
+          const dynamoDb = getDynamoDbClient()
+          const counterRepository = new CounterRepository(tenantId, {
+            mongoDb: mongoClient,
+            dynamoDb,
+          })
+          screeningId = await counterRepository.getNextCounterAndUpdate(
+            'ScreeningDetails'
+          )
+          data = await collection.findOneAndUpdate(
+            event.filter,
+            {
+              ...event.updateMessage,
+              $setOnInsert: {
+                ...event.updateMessage.$setOnInsert,
+                screeningId: `S-${screeningId}`,
+              },
+            },
+            {
+              upsert: event.upsert || false,
+              ...(event.arrayFilters
+                ? { arrayFilters: event.arrayFilters }
+                : {}),
+              returnDocument: 'after',
+            }
+          )
         }
-      )
-
+      }
       if (!data?.value?._id) {
         logger.warn(
           `Mongo update consumer: No _id found for event ${event.collectionName}`,
