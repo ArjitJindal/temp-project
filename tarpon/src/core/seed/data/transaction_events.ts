2b042bc0d4c875e9806d14720a30c531fe0a065d
Vedansh Mishra <139506945+Vedansh00Mishra@users.noreply.github.com>
2025-10-01 10:03:31 +0530

    Make last transaction state to suspended for payment approvals demo data (#8081)
    make last transaction state to suspended for payment approvals demo data

--- tarpon/src/core/seed/data/transaction_events.ts
+++ tarpon/src/core/seed/data/transaction_events.ts
@@ -55,7 +55,9 @@ const data: () => TransactionEventWithRulesResult[] = memoize(() => {
         },
       },
       {
-        transactionState: t.transactionState || 'REFUNDED',
+        transactionState:
+          t.transactionState ||
+          (t.status === 'SUSPEND' ? 'SUSPENDED' : 'REFUNDED'),
         timestamp: t.timestamp + 3600000 + 3600000,
         transactionId: t.transactionId,
         eventId: rng.r(2).randomGuid(),
