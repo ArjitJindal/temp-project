c102e0ad40c6c29b4e8097575e0c7815ec1f8d42
Divya Agrawal <121372068+divyaagrawal261@users.noreply.github.com>
2025-10-01 14:58:10 +0530

    Fix: beforeTimestamp and afterTimestamp now have right values if they are not manually set (#7798)
    * Fix: beforeTimestamp and afterTimestamp now have right values if they are not manually set
    
    The default value of afterTimestamp when createdAt filter is not selected is 0. If a user selects FROM as 'any', afterTimestamp will be 0 now
    
    The default value of beforeTimestamp when createdAt filter is not selected is MAX_SAFE_INTEGER. If a user selects TO as 'any', beforeTimestamp will be MAX_SAFE_INTEGER now
    
    * Fix: Lint Errors

--- phytoplankton-console/src/pages/case-management/CaseTableWrapper.tsx
+++ phytoplankton-console/src/pages/case-management/CaseTableWrapper.tsx
@@ -72,15 +72,22 @@ export default function CaseTableWrapper(props: {
 
       const [sortField, sortOrder] = sort[0] ?? [];
 
+      const afterTimestamp =
+        createdTimestamp && createdTimestamp[0] !== undefined && createdTimestamp[0] !== null
+          ? dayjs(createdTimestamp[0]).valueOf()
+          : 0;
+      const beforeTimestamp =
+        createdTimestamp && createdTimestamp[1] !== undefined && createdTimestamp[1] !== null
+          ? dayjs(createdTimestamp[1]).valueOf()
+          : Number.MAX_SAFE_INTEGER;
+
       const response = await api.getCaseList({
         page,
         pageSize,
         view,
         ...paginationParams,
-        afterTimestamp: createdTimestamp ? dayjs(createdTimestamp[0]).valueOf() : 0,
-        beforeTimestamp: createdTimestamp
-          ? dayjs(createdTimestamp[1]).valueOf()
-          : Number.MAX_SAFE_INTEGER,
+        afterTimestamp,
+        beforeTimestamp,
         filterId: caseId,
         filterRulesHit: rulesHitFilter,
         filterRulesExecuted: rulesExecutedFilter,
