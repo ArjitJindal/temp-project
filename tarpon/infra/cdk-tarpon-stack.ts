fe4909bcb25c70a0194b1599e562625961151190
Kumar Amit Sinha <kumar@flagright.com>
2025-10-07 21:09:38 +0530

    feat: DynamoDB VPC for fargate (#8146)
    Enhance ECS Batch Job Configuration in CDK Stack
    
    - Added conditional logic to set security groups and subnet selection based on the LAMBDA_VPC_ENABLED configuration.
    - Updated EcsRunTask properties to include security groups and subnet settings for improved network configuration.

--- tarpon/infra/cdk-tarpon-stack.ts
+++ tarpon/infra/cdk-tarpon-stack.ts
@@ -1254,6 +1254,14 @@ export class CdkTarponStack extends cdk.Stack {
         StackConstants.FARGATE_BATCH_JOB_CLUSTER_NAME,
         { vpc }
       )
+      const ecsSecurityGroups = this.config.resource.LAMBDA_VPC_ENABLED
+        ? [securityGroup, clickhouseSecurityGroup].filter(
+            (sg): sg is SecurityGroup => Boolean(sg)
+          )
+        : undefined
+      const ecsSubnetSelection = this.config.resource.LAMBDA_VPC_ENABLED
+        ? { subnetType: SubnetType.PRIVATE_WITH_EGRESS }
+        : undefined
       ecsBatchJobTask = new EcsRunTask(
         this,
         getResourceNameForTarpon('BatchJobFargateRunner'),
@@ -1263,6 +1271,12 @@ export class CdkTarponStack extends cdk.Stack {
           launchTarget: new EcsFargateLaunchTarget({
             platformVersion: FargatePlatformVersion.LATEST,
           }),
+          assignPublicIp: false,
+          securityGroups:
+            ecsSecurityGroups && ecsSecurityGroups.length > 0
+              ? ecsSecurityGroups
+              : undefined,
+          subnets: ecsSubnetSelection,
           inputPath: `$.Payload.${BATCH_JOB_PAYLOAD_RESULT_KEY}`,
           integrationPattern: IntegrationPattern.RUN_JOB,
           containerOverrides: [
