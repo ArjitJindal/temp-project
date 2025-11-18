346b8d53dcb21c27c04794fb6b99eef31a870373
Alit Pani <alit@flagright.com>
2025-11-18 12:16:30 +0530

    Update Fargate batch job configuration to disable by default (#8547)
    Co-authored-by: Alit Pani <alitpani@Alits-MacBook-Pro.local>

--- tarpon/infra/cdk-tarpon-stack.ts
+++ tarpon/infra/cdk-tarpon-stack.ts
@@ -171,7 +171,7 @@ const CONSUMER_SQS_VISIBILITY_TIMEOUT = Duration.seconds(
 // SQS max receive count cannot go above 1000
 const MAX_SQS_RECEIVE_COUNT = 1000
 const isDevUserStack = isQaEnv()
-const enableFargateBatchJob = true
+const enableFargateBatchJob = false
 const FEATURE = 'feature'
 
 const FEATURES = {
